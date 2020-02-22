23f63a86f5a9d760989845648480e4831b64579e
Tom Nielsen <tanielsen@gmail.com>
2020-02-22 10:47:47 +0000

    express server

---
+++ packages/saltcorn/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+  })
---
+++ packages/saltcorn/types.js
@@ -0,0 +1,24 @@
+class Attribute {
+    constructor(o) {
+
+    }
+}
+
+class Type {
+    name = ""
+    attributes = {}
+    
+}
+
+class String extends Type {
+    name="String"
+}
+
+class Integer extends Type {
+    name="Integer"
+    attributes = [
+        { name: "max",
+          type: "Integer",
+          required: false }
+    ]
+}
