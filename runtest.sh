2c7c3e0f0706f38962538ae974f2a2e47ae67645
Tom Nielsen <tanielsen@gmail.com>
2020-03-24 16:38:54 +0000

    tests organise

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
+
 createdb saltcorn_test
+trap "dropdb saltcorn_test" EXIT
 PGDATABASE=saltcorn_test node packages/saltcorn/db/reset_schema.js
 PGDATABASE=saltcorn_test node packages/saltcorn/db/fixtures.js
-(cd packages/saltcorn/ && PGDATABASE=saltcorn_test npm run test)
-dropdb saltcorn_test
+PGDATABASE=saltcorn_test lerna run test
+
