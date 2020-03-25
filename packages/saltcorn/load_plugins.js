2320806afc2e38de8be803b8274c1caf6200f77c
Tom Nielsen <tanielsen@gmail.com>
2020-03-25 14:08:28 +0000

    clean

--- packages/saltcorn/load_plugins.js
+++ packages/saltcorn/load_plugins.js
@@ -1,22 +1,22 @@
 const db = require("saltcorn-data/db");
 
 const loadAsync = async () => {
-    const plugins = await db.select("plugins")
-    plugins.forEach(plugin=>{
-        if(plugin.source ==="npm") {
-            require(plugin.location).register();
-        }
-    })
-}
+  const plugins = await db.select("plugins");
+  plugins.forEach(plugin => {
+    if (plugin.source === "npm") {
+      require(plugin.location).register();
+    }
+  });
+};
 
-const load=()=>{
-    loadAsync().then(
-        () => {         
-        },
-        err => {
-          console.error(err);
-          process.exit(1);
-        })
-}
+const load = () => {
+  loadAsync().then(
+    () => {},
+    err => {
+      console.error(err);
+      process.exit(1);
+    }
+  );
+};
 
-module.exports = load
+module.exports = load;
