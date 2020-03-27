5b3bf912df4e214afdf888b6949011ea629f9fab
Tom Nielsen <tanielsen@gmail.com>
2020-03-27 15:40:35 +0000

    fix imports

--- packages/saltcorn-markup/index.js
+++ packages/saltcorn-markup/index.js
@@ -1,24 +1,6 @@
 const renderForm = require("./form");
 const mkTable = require("./table");
-const { a, td, tr, th, text } = require("./tags");
-
-const mkTable = (hdrs, vs) => {
-  var s =
-    '<div class="table-responsive"><table class="table table-sm"><thead><tr>';
-  hdrs.forEach(hdr => {
-    s += th(text(hdr.label));
-  });
-  s += "</tr></thead><tbody>";
-  (vs || []).forEach(v => {
-    const tds = hdrs.map(hdr =>
-      td(typeof hdr.key === "string" ? text(v[hdr.key]) : hdr.key(v))
-    );
-
-    s += tr(tds);
-  });
-  s += "</tbody></table></div>";
-  return s;
-};
+const { a, text } = require("./tags");
 
 const link = (href, s) => a({ href: text(href) }, text(s));
 
--- packages/saltcorn-markup/table.js
+++ packages/saltcorn-markup/table.js
@@ -1,21 +1,21 @@
-const { a, td, tr, th, text } = require("./tags");
+const { a, td, tr, th, text, div, table, thead, tbody } = require("./tags");
 
-const mkTable = (hdrs, vs) => {
-  var s =
-    '<div class="table-responsive"><table class="table table-sm"><thead><tr>';
-  hdrs.forEach(hdr => {
-    s += th(text(hdr.label));
-  });
-  s += "</tr></thead><tbody>";
-  (vs || []).forEach(v => {
-    const tds = hdrs.map(hdr =>
-      td(typeof hdr.key === "string" ? text(v[hdr.key]) : hdr.key(v))
-    );
-
-    s += tr(tds);
-  });
-  s += "</tbody></table></div>";
-  return s;
-};
+const mkTable = (hdrs, vs, opts) =>
+  div(
+    { class: "table-responsive" },
+    table(
+      { class: "table table-sm" },
+      thead(tr(hdrs.map(hdr => th(text(hdr.label))))),
+      tbody(
+        (vs || []).map(v =>
+          tr(
+            hdrs.map(hdr =>
+              td(typeof hdr.key === "string" ? text(v[hdr.key]) : hdr.key(v))
+            )
+          )
+        )
+      )
+    )
+  );
 
 module.exports = mkTable;
