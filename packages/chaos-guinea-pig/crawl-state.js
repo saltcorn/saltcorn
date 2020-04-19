18fb83244527d9e929e813bb0c2f30dbbea53136
Tom Nielsen <tanielsen@gmail.com>
2020-04-19 20:35:02 +0100

    rm ext res checks

--- packages/chaos-guinea-pig/crawl-state.js
+++ packages/chaos-guinea-pig/crawl-state.js
@@ -1,32 +1,5 @@
-const superagent = require("superagent");
-const axios  = require("axios");
-
 class CrawlState {
-  constructor() {
-    this.external_resources = [];
-  }
-
-  async check_external_resource(url, type) {
-    if (this.external_resources.includes(url)) return;
-    //console.log("checking", type, url);
-    const lres = await axios.head(url,  {
-        validateStatus: function (status) {
-          return status < 400; // Reject only if the status code is greater than or equal to 500
-        }
-      });
-    //if (lres.status == 200) {
-      this.external_resources.push(url);
-    /*} else {
-      throw new Error(`Got response ${lres.status} to ${type} request ${url}`);
-    }*/
-  }
-
-  async check_link(link) {
-    await this.check_external_resource(link.attribs.href, "link");
-  }
-  async check_script_src(link) {
-    await this.check_external_resource(link.attribs.src, "script");
-  }
+  constructor() {}
 }
 
 module.exports = CrawlState;
--- packages/chaos-guinea-pig/index.js
+++ packages/chaos-guinea-pig/index.js
@@ -26,23 +26,16 @@ const step = async (app, url, state, steps_left) => {
   //console.log(res.text);
 
   const $ = cheerio.load(res.text);
-  for (const link of $("link").toArray()) {
-    await state.check_link(link);
-  }
-  for (const scr of $("script[src]").toArray()) {
-    await state.check_script_src(scr);
-  }
-  var local_links = []
+
+  var local_links = [];
   for (const a of $("a[href]").toArray()) {
-    const url=new URL(a.attribs.href, "http://my.local/")
-    if(url.origin==='http://my.local') {
-        local_links.push(a.attribs.href)
-    } else {
-        await state.check_external_resource(a.attribs.href, 'a_href');
+    const url = new URL(a.attribs.href, "http://my.local/");
+    if (url.origin === "http://my.local") {
+      local_links.push(a.attribs.href);
     }
   }
-  if(steps_left && local_links.length>0) {
-    return await step(app, oneOf(local_links), state, steps_left-1)
+  if (steps_left && local_links.length > 0) {
+    return await step(app, oneOf(local_links), state, steps_left - 1);
   } else return;
 };
 
