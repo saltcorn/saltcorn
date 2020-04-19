7435d36cbf7af5bd847df490812f551c913af8bd
Tom Nielsen <tanielsen@gmail.com>
2020-04-19 22:17:31 +0100

    somewhat working

--- packages/chaos-guinea-pig/index.js
+++ packages/chaos-guinea-pig/index.js
@@ -14,29 +14,92 @@ const oneOf = vs => vs[Math.floor(Math.random() * vs.length)];
 const run = async (app, options = {}) => {
   const startAt = options.startAt || "/";
   const state = new CrawlState();
-  await step(app, startAt, state, options.steps || 10);
+  await get_link(app, startAt, state, options.steps || 10);
 };
 
-const step = async (app, url, state, steps_left) => {
+const isLocalURL = url =>
+  url &&
+  !url.startsWith("javascript:") &&
+  new URL(url, "http://my.local/").origin === "http://my.local";
+
+const get_link = async (app, url, state, steps_left) => {
   console.log("Checking page", url);
   const res = await request(app)
     .get(url)
     .set("Accept", "text/html");
   expect(toSucceed);
   //console.log(res.text);
+  await process(res, app, url, state, steps_left);
+};
+
+const genRandom = input => {
+  return "foo";
+};
+
+const submit_form = async (app, form, url, state, steps_left) => {
+  const action = form.attr("action");
+  const method = (form.attr("method") || "post").toLowerCase();
+  //console.log({ form });
+
+  console.log("submitting form at", action);
+  const inputs = form.find("input").toArray();
+  //console.log({ inputs });
+  if (method === "post") {
+    var req = request(app).post(action);
+    for (const input of inputs) {
+      const oldreq = req;
+      req = oldreq.send(`${input.attribs.name}=${genRandom(input)}`);
+    }
+    const res = await req.expect(toSucceed);
+    await process(res, app, url, state, steps_left);
+  } else if (method === "get") {
+    var url = action + "?";
+    for (const input of inputs) {
+      url += `${input.attribs.name}=${genRandom(input)}`;
+    }
+    const res = await request(app)
+      .get(action)
+      .expect(toSucceed);
+    await process(res, app, url, state, steps_left);
+  }
+};
+const rndElem = selection => {
+  var random = Math.floor(Math.random() * selection.length);
+  return selection.eq(random);
+};
+const process = async (res, app, url, state, steps_left) => {
+  if (res.status === 302) {
+    console.log("redirect to", res.headers.location);
+    const nextres = await request(app).get(res.headers.location);
+    return await process(nextres, app, url, state, steps_left);
+  }
 
   const $ = cheerio.load(res.text);
 
-  var local_links = [];
-  for (const a of $("a[href]").toArray()) {
-    const url = new URL(a.attribs.href, "http://my.local/");
-    if (url.origin === "http://my.local") {
-      local_links.push(a.attribs.href);
-    }
+  var local_links = $("a[href]").filter((i, e) => isLocalURL(e.attribs.href));
+  var forms = $("form");
+  if (steps_left) {
+    if (forms.length > 0 && local_links.length > 0)
+      if (Math.random() < 0.2)
+        return await get_link(
+          app,
+          rndElem(local_links).attr("href"),
+          state,
+          steps_left - 1
+        );
+      else return await submit_form(app, rndElem(forms), state, steps_left - 1);
+
+    if (forms.length > 0)
+      return await submit_form(app, rndElem(forms), state, steps_left - 1);
+
+    if (local_links.length > 0)
+      return await get_link(
+        app,
+        rndElem(local_links).attr("href"),
+        state,
+        steps_left - 1
+      );
   }
-  if (steps_left && local_links.length > 0) {
-    return await step(app, oneOf(local_links), state, steps_left - 1);
-  } else return;
 };
 
 module.exports = run;
--- packages/saltcorn/index.js
+++ packages/saltcorn/index.js
@@ -5,6 +5,6 @@ const port = 3000;
 //https://blog.cloudboost.io/waiting-for-db-connections-before-app-listen-in-node-f568af8b9ec9
 app.on("ready", function() {
   app.listen(port, () => {
-    console.log(`App running on port ${port}.`);
+    console.log(`Saltcorn listening on port ${port}.`);
   });
 });
