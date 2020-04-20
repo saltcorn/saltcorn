6d2704fdd6595f138e5744f03b393b8627027852
Tom Nielsen <tanielsen@gmail.com>
2020-04-20 11:32:47 +0100

    run init tool

---
+++ packages/saltcorn-cli/src/commands/hello.js
@@ -0,0 +1,20 @@
+const {Command, flags} = require('@oclif/command')
+
+class HelloCommand extends Command {
+  async run() {
+    const {flags} = this.parse(HelloCommand)
+    const name = flags.name || 'world'
+    this.log(`hello ${name} from ./src/commands/hello.js`)
+  }
+}
+
+HelloCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`
+
+HelloCommand.flags = {
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = HelloCommand
