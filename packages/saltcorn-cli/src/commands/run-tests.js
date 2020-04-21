191ffb8dbfa75e3c1fb5099ef3a6cdfada13e8ad
Tom Nielsen <tanielsen@gmail.com>
2020-04-21 12:04:04 +0100

    Runtest cli to run test on windows (#42)
    * changeConnection
    
    * run test cmd
    
    * run tests
    
    * rename cmd
    
    * rename bin in pkg.json
    
    * rename cmd
    
    * run lerna on windows

---
+++ packages/saltcorn-cli/src/commands/run-tests.js
@@ -0,0 +1,26 @@
+const { Command, flags } = require("@oclif/command");
+const fixtures = require("saltcorn/fixtures");
+const reset = require("saltcorn-data/db/reset_schema");
+const db = require("saltcorn-data/db");
+const { spawnSync } = require("child_process");
+
+class RunTestsCommand extends Command {
+  async run() {
+    await db.changeConnection({ database: "saltcorn_test" });
+    await reset();
+    await fixtures();
+    const env = { ...process.env, PGDATABASE: "saltcorn_test" };
+    const lerna = process.platform === 'win32' ? 'lerna.cmd' : 'lerna'
+    const res = spawnSync(lerna, ["run", "test"], { stdio: "inherit", env });
+    this.exit(res.status);
+  }
+}
+
+RunTestsCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`;
+
+RunTestsCommand.flags = {};
+
+module.exports = RunTestsCommand;
