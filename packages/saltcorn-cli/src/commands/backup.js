3ecea9d5d40e21c89975b0ff053d8b45a31f4cd7
Tom Nielsen <tanielsen@gmail.com>
2020-04-21 20:30:10 +0100

    backup cmd

---
+++ packages/saltcorn-cli/src/commands/backup.js
@@ -0,0 +1,34 @@
+const { Command, flags } = require("@oclif/command");
+const { execSync } = require("child_process");
+const dateFormat = require('dateformat');
+const os=require('os')
+
+const env = process.env
+var day=dateFormat(new Date(), "yyyymmdd");
+const pgdb =env.PGDATABASE || "saltcorn"
+var default_filenm = `${day}-backup-${pgdb}-${os.hostname}.sqlc`
+
+class BackupCommand extends Command {
+  async run() {
+    const { flags } = this.parse(BackupCommand);
+
+      
+      const pguser=env.PGUSER
+      const outfnm=flags.output || default_filenm
+    execSync(`pg_dump ${pgdb} -U ${pguser} -h localhost -F c >${outfnm}`);
+  }
+}
+
+BackupCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`;
+
+BackupCommand.flags = {
+    output: flags.string({ char: "o", description: "output filename",
+    default: default_filenm
+})
+
+};
+
+module.exports = BackupCommand;
