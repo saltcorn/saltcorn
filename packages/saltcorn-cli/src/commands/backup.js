fbb6c16e3808e5e79f861d71ad99c57d6479c514
Tom Nielsen <tanielsen@gmail.com>
2020-04-21 20:31:30 +0100

    prettier

--- packages/saltcorn-cli/src/commands/backup.js
+++ packages/saltcorn-cli/src/commands/backup.js
@@ -1,20 +1,20 @@
 const { Command, flags } = require("@oclif/command");
 const { execSync } = require("child_process");
-const dateFormat = require('dateformat');
-const os=require('os')
+const dateFormat = require("dateformat");
+const os = require("os");
 
-const env = process.env
-var day=dateFormat(new Date(), "yyyymmdd");
-const pgdb =env.PGDATABASE || "saltcorn"
-var default_filenm = `${day}-backup-${pgdb}-${os.hostname}.sqlc`
+const env = process.env;
+var day = dateFormat(new Date(), "yyyymmdd");
+const pgdb = env.PGDATABASE || "saltcorn";
+var default_filenm = `${day}-backup-${pgdb}-${os.hostname}.sqlc`;
 
 class BackupCommand extends Command {
   async run() {
     const { flags } = this.parse(BackupCommand);
 
-      
-      const pguser=env.PGUSER
-      const outfnm=flags.output || default_filenm
+    const pguser = env.PGUSER;
+    const outfnm = flags.output || default_filenm;
+
     execSync(`pg_dump ${pgdb} -U ${pguser} -h localhost -F c >${outfnm}`);
   }
 }
@@ -25,10 +25,11 @@ Extra documentation goes here
 `;
 
 BackupCommand.flags = {
-    output: flags.string({ char: "o", description: "output filename",
+  output: flags.string({
+    char: "o",
+    description: "output filename",
     default: default_filenm
-})
-
+  })
 };
 
 module.exports = BackupCommand;
