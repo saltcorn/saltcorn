e8a44be4768b03b1eaa9c48cb6de26b31f3f0317
Tom Nielsen <tanielsen@gmail.com>
2020-04-23 14:20:33 +0100

    run 1 repo, coverage

--- packages/saltcorn-cli/src/commands/run-tests.js
+++ packages/saltcorn-cli/src/commands/run-tests.js
@@ -5,14 +5,28 @@ const db = require("saltcorn-data/db");
 const { spawnSync } = require("child_process");
 
 class RunTestsCommand extends Command {
+  static args = [
+    { name: "package", description: "which package to run tests for" }
+  ];
   async run() {
+    const { args, flags } = this.parse(RunTestsCommand);
     await db.changeConnection({ database: "saltcorn_test" });
     await reset();
     await fixtures();
     const env = { ...process.env, PGDATABASE: "saltcorn_test" };
-    const lerna = process.platform === "win32" ? "lerna.cmd" : "lerna";
-    const res = spawnSync(lerna, ["run", "test"], { stdio: "inherit", env });
-    this.exit(res.status);
+    if (args.package) {
+      const cwd = "packages/" + args.package;
+      const res = spawnSync(
+        "npm",
+        ["run", "test", ...(flags.coverage && ["--", "--coverage"])],
+        { stdio: "inherit", env, cwd }
+      );
+      this.exit(res.status);
+    } else {
+      const lerna = process.platform === "win32" ? "lerna.cmd" : "lerna";
+      const res = spawnSync(lerna, ["run", "test"], { stdio: "inherit", env });
+      this.exit(res.status);
+    }
   }
 }
 
@@ -21,6 +35,8 @@ RunTestsCommand.description = `Describe the command here
 Extra documentation goes here
 `;
 
-RunTestsCommand.flags = {};
+RunTestsCommand.flags = {
+  coverage: flags.boolean({ char: "c", description: "Coverage" })
+};
 
 module.exports = RunTestsCommand;
