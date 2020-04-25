3bcff4a9b9f3a26d9ebc6efc9b88bc90da2e2272
Tom Nielsen <tanielsen@gmail.com>
2020-04-25 16:11:54 +0100

    Base plugin reorganise (#53)
    * move base plugin into -data
    
    * move tests to -data
    
    * prettier

---
+++ packages/saltcorn-data/base-plugin/base.test.js
@@ -0,0 +1,11 @@
+const { auto_test_plugin } = require("../plugin-testing");
+const plugin = require(".");
+const db = require("../db");
+
+afterAll(db.close);
+
+describe("base plugin", () => {
+  it("passes auto test", async () => {
+    await auto_test_plugin(plugin);
+  });
+});
