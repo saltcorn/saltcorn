522775d3f5d13c61ef8f59af7e4f4f611520d7fb
Tom Nielsen <tanielsen@gmail.com>
2020-04-29 08:25:22 +0100

    update jquery

--- packages/saltcorn-sbadmin2/wrap.js
+++ packages/saltcorn-sbadmin2/wrap.js
@@ -68,8 +68,7 @@ const wrap = ({ title, menu, alerts, body, headers }) => `<!doctype html>
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.0.7/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core plugin JavaScript-->
