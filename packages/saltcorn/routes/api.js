73f1564a03b9cff3efca8d61a3b24293766e50a9
Tom Nielsen <tanielsen@gmail.com>
2020-05-03 10:19:04 +0100

    basic api

--- packages/saltcorn/routes/api.js
+++ packages/saltcorn/routes/api.js
@@ -9,3 +9,16 @@ const load_plugins = require("../load_plugins");
 
 const router = new Router();
 module.exports = router;
+
+const noId=r=>{
+    const {id, ...rest} = r
+    return rest
+}
+
+router.get("/:tableName/", async (req, res) => {
+    const { tableName } = req.params;
+    const table = await Table.findOne({ name: tableName });
+    const rows = await table.getRows()
+    res.json(rows.map(noId))
+})
+  
