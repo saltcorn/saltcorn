2cad598b4ef3c2d1b0acfc93138038928d69a371
Tom Nielsen <tanielsen@gmail.com>
2020-05-03 11:06:33 +0100

    import table

--- packages/saltcorn/routes/api.js
+++ packages/saltcorn/routes/api.js
@@ -3,7 +3,7 @@ const db = require("saltcorn-data/db");
 const { isAdmin } = require("./utils.js");
 const { mkTable, renderForm, link, post_btn } = require("saltcorn-markup");
 const State = require("saltcorn-data/db/state");
-const Form = require("saltcorn-data/models/form");
+const Table = require("saltcorn-data/models/table");
 const Field = require("saltcorn-data/models/field");
 const load_plugins = require("../load_plugins");
 
