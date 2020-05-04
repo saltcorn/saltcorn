d711f9f8f221c6aa0f72df6b592f417281d18b24
Tom Nielsen <tanielsen@gmail.com>
2020-05-04 23:02:23 +0100

    prettier

--- packages/saltcorn-data/models/pack.js
+++ packages/saltcorn-data/models/pack.js
@@ -4,63 +4,62 @@ const Field = require("./field");
 const Plugin = require("./plugin");
 
 const table_pack = async name => {
-    const table = await Table.findOne({ name });
-    const fields = await table.getFields();
-    const strip_ids = o=>{
-        delete o.id
-        delete o.table_id
-        return o
-    }
-    return {
-      name: table.name,
-      expose_api_read: table.expose_api_read,
-      expose_api_write: table.expose_api_write,
-      min_role_read: table.min_role_read,
-      min_role_write: table.min_role_write,
-      fields: fields.map(f => strip_ids(f.toJson))
-    };
+  const table = await Table.findOne({ name });
+  const fields = await table.getFields();
+  const strip_ids = o => {
+    delete o.id;
+    delete o.table_id;
+    return o;
   };
-  const view_pack = async name => {
-    const view = await View.findOne({ name });
-    const table = await Table.findOne({ id: view.table_id });
-  
-    return {
-      name: view.name,
-      viewtemplate: view.viewtemplate,
-      configuration: view.configuration,
-      is_public: view.is_public,
-      on_root_page: view.on_root_page,
-      on_menu: view.on_menu,
-      table: table.name
-    };
+  return {
+    name: table.name,
+    expose_api_read: table.expose_api_read,
+    expose_api_write: table.expose_api_write,
+    min_role_read: table.min_role_read,
+    min_role_write: table.min_role_write,
+    fields: fields.map(f => strip_ids(f.toJson))
   };
-  
-  const plugin_pack = async name => {
-    const plugin = await Plugin.findOne({ name });
-  
-    return {
-      name: plugin.name,
-      source: plugin.source,
-      location: plugin.location
-    };
-  };
+};
+const view_pack = async name => {
+  const view = await View.findOne({ name });
+  const table = await Table.findOne({ id: view.table_id });
 
+  return {
+    name: view.name,
+    viewtemplate: view.viewtemplate,
+    configuration: view.configuration,
+    is_public: view.is_public,
+    on_root_page: view.on_root_page,
+    on_menu: view.on_menu,
+    table: table.name
+  };
+};
 
-const install_pack=async(pack)=>{
-    for (const tableSpec of pack.tables) {
-        const table = await Table.create(tableSpec.name, tableSpec);
-        for (const field of tableSpec.fields)
-          await Field.create({ table, ...field });
-      }
-      for (const viewSpec of pack.views) {
-        const { table, ...viewNoTable } = viewSpec;
-        const vtable = await Table.findOne({ name: table });
-        await View.create({ ...viewNoTable, table_id:vtable.id });
-      }
-      const existingPlugins=await Plugin.find({});
-      for (const plugin of pack.plugins) {
-        if(!existingPlugins.some(ep=>ep.name===plugin.name))
-          await Plugin.upsert(plugin);
-      }
-}
-module.exports = {table_pack, view_pack, plugin_pack,install_pack}
+const plugin_pack = async name => {
+  const plugin = await Plugin.findOne({ name });
+
+  return {
+    name: plugin.name,
+    source: plugin.source,
+    location: plugin.location
+  };
+};
+
+const install_pack = async pack => {
+  for (const tableSpec of pack.tables) {
+    const table = await Table.create(tableSpec.name, tableSpec);
+    for (const field of tableSpec.fields)
+      await Field.create({ table, ...field });
+  }
+  for (const viewSpec of pack.views) {
+    const { table, ...viewNoTable } = viewSpec;
+    const vtable = await Table.findOne({ name: table });
+    await View.create({ ...viewNoTable, table_id: vtable.id });
+  }
+  const existingPlugins = await Plugin.find({});
+  for (const plugin of pack.plugins) {
+    if (!existingPlugins.some(ep => ep.name === plugin.name))
+      await Plugin.upsert(plugin);
+  }
+};
+module.exports = { table_pack, view_pack, plugin_pack, install_pack };
