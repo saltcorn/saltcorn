043bb7b5a8c4826a51df8e52171fa6c592950286
Tom Nielsen <tanielsen@gmail.com>
2020-05-05 17:45:34 +0100

    config with test

---
+++ packages/saltcorn-data/models/config.js
@@ -0,0 +1,24 @@
+const db = require("../db");
+const { contract, is } = require("contractis");
+
+const getConfig = async (key, def)=>{
+    const cfg = await db.selectMaybeOne("_sc_config", {key});
+    if(cfg)
+        return cfg.value
+    else return def
+}
+
+const getAllConfig = async ()=>{
+    const cfgs = await db.select("_sc_config");
+    var cfg = {}
+    cfgs.forEach(({key, value}) => {
+        cfg[key]=value
+    });
+    return cfg
+}
+
+const setConfig = async (key, value)=>{
+    await db.query(`insert into _sc_config(key, value) values($1, $2) 
+                    on conflict (key) do update set value = $2`, [key, value]);
+}
+module.exports= {getConfig, getAllConfig, setConfig}
---
+++ packages/saltcorn-data/tests/config.test.js
@@ -0,0 +1,28 @@
+const Table = require("saltcorn-data/models/table");
+const Field = require("saltcorn-data/models/field");
+const db = require("saltcorn-data/db");
+const State = require("../db/state");
+State.registerPlugin(require("../base-plugin"));
+const {getConfig, getAllConfig, setConfig} = require("../models/config");
+afterAll(db.close);
+
+describe("Config", () => {
+  it("should get default", async () => {
+    const d = await getConfig("noval", 5)
+    expect(d).toBe(5)
+  });
+  it("should set value", async () => {
+    await setConfig("cfg1", 8)
+    const d = await getConfig("cfg1", 5)
+    expect(d).toBe(8)
+    await setConfig("cfg1", 9)
+    const x = await getConfig("cfg1", 5)
+    expect(x).toBe(9)
+
+  });
+  it("should get all value", async () => {
+    const d = await getAllConfig()
+    expect(d.cfg1).toBe(9)
+
+  });
+})
