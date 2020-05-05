ecd5c5e73061d62a1d325893d7250f72823d50ec
Tom Nielsen <tanielsen@gmail.com>
2020-05-05 19:24:01 +0100

    error message on install

--- packages/saltcorn/routes/plugins.js
+++ packages/saltcorn/routes/plugins.js
@@ -5,9 +5,7 @@ const State = require("saltcorn-data/db/state");
 const Form = require("saltcorn-data/models/form");
 const Field = require("saltcorn-data/models/field");
 const Plugin = require("saltcorn-data/models/plugin");
-const {
-  fetch_available_packs
-} = require("saltcorn-data/models/pack");
+const { fetch_available_packs } = require("saltcorn-data/models/pack");
 const { getConfig, setConfig } = require("saltcorn-data/models/config");
 
 const load_plugins = require("../load_plugins");
@@ -42,7 +40,7 @@ router.get("/", isAdmin, async (req, res) => {
   const rows = await Plugin.find({});
   const instore = await Plugin.store_plugins_available();
   const packs_available = await fetch_available_packs();
-  const packs_installed = await getConfig("installed_packs",[])
+  const packs_installed = await getConfig("installed_packs", []);
   res.sendWrap(
     "Plugins",
     h5("Installed"),
@@ -70,11 +68,9 @@ router.get("/", isAdmin, async (req, res) => {
         {
           label: "Install",
           key: r =>
-          packs_installed.includes(r.name) ? "Installed"
-            : post_btn(
-              `/plugins/install/${encodeURIComponent()}`,
-              "Install"
-            )
+            packs_installed.includes(r.name)
+              ? "Installed"
+              : post_btn(`/plugins/install/${encodeURIComponent()}`, "Install")
         }
       ],
       instore
