b1ae4ac383737b5ea122d8b940249fcab7f35776
Tom Nielsen <tanielsen@gmail.com>
2020-05-08 22:43:17 +0100

    serve print address

--- packages/saltcorn/serve.js
+++ packages/saltcorn/serve.js
@@ -4,6 +4,6 @@ module.exports = async portArg => {
   const port = portArg || 3000;
   const app = await getApp();
   app.listen(port, () => {
-    console.log(`Saltcorn listening on port ${port}.`);
+    console.log(`Saltcorn listening on http://127.0.0.1:${port}/`);
   });
 };
