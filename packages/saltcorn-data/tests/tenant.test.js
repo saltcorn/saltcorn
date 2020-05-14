73b6cc7d6ed98a34099f42c9581d1886ce8cbf6f
Tom Nielsen <tanielsen@gmail.com>
2020-05-14 21:33:32 +0100

    tests

--- packages/saltcorn-data/tests/tenant.test.js
+++ packages/saltcorn-data/tests/tenant.test.js
@@ -1,28 +1,21 @@
 const db = require("saltcorn-data/db");
 const { getState } = require("../db/state");
 getState().registerPlugin(require("../base-plugin"));
-const { getAllTenants, createTenant } = require("../models/tenant");
+const { createTenant } = require("../models/tenant");
+
 afterAll(db.close);
 
-describe("Tenant", () => {
-  it("can create a new tenant", done => {
-    db.tenantNamespace.run(() => {
-      createTenant({
-        subdomain: "test1",
-        email: "foo@bar.com",
-        password: "secret"
-      })
-        .then(v => {
-          expect(5).toBe(5);
-          console.log(v);
+beforeAll(async () => {
+  await db.query(`drop schema if exists test10 CASCADE `);
+});
 
-          //db.query(`drop schema test1;`).then(() => );
-        })
-        .catch(err => {
-          console.log(err);
-          done();
-        })
-        .finally(() => done());
+describe("Tenant", () => {
+  it("can create a new tenant", async () => {
+    db.enable_multi_tenant();
+    await createTenant({
+      subdomain: "test10",
+      email: "foo@bar.com",
+      password: "secret"
     });
   });
 });
--- packages/saltcorn/routes/tenant.test.js
+++ packages/saltcorn/routes/tenant.test.js
@@ -14,19 +14,19 @@ const {
 afterAll(db.close);
 
 beforeAll(async () => {
-  //db.query(`drop schema if exists test2`);
+  db.query(`drop schema if exists test2 cascade`);
 });
 
 describe("tenant routes", () => {
   it("creates tenant", async () => {
     db.enable_multi_tenant();
-    /*const app = await getApp();
+    const app = await getApp();
     await request(app)
       .post("/tenant/create")
       .send("subdomain=test2")
       .send("email=foo@bar.com")
       .send("password=secret")
-      .expect(toInclude("Success"));*/
+      .expect(toInclude("Success"));
     expect(1).toBe(1);
   });
 });
