7c418117bf796d1a104e7f5565ed313703e6f83e
Tom Nielsen <tanielsen@gmail.com>
2020-05-20 22:15:45 +0100

    clean

--- packages/saltcorn-builder/src/index.js
+++ packages/saltcorn-builder/src/index.js
@@ -3,8 +3,7 @@ import Builder from "./components/Builder";
 import ReactDOM from "react-dom";
 
 function renderBuilder(id) {
-  const wrapper = document.getElementById(id);
-  wrapper ? ReactDOM.render(<Builder />, wrapper) : false;
+  ReactDOM.render(<Builder />, document.getElementById(id));
 }
 
 export { renderBuilder };
