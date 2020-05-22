8863e01c68b3ae5db869c3a51805dca9e00b66e1
Tom Nielsen <tanielsen@gmail.com>
2020-05-22 08:44:30 +0100

    comment console

--- packages/saltcorn-data/base-plugin/viewtemplates/show.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/show.js
@@ -66,7 +66,7 @@ const get_state_fields = () => [
 const initial_config = initial_config_all_fields(false);
 
 const run = async (table_id, viewname, { columns, layout }, { id }) => {
-  console.log(columns);
+  //console.log(columns);
   if (typeof id === "undefined") return "No record selected";
   const tbl = await Table.findOne({ id: table_id });
   const fields = await Field.find({ table_id: tbl.id });
