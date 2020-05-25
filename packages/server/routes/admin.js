b5df0b1613c39c1e7851dc73dbe612cf11ae0827
Tom Nielsen <tanielsen@gmail.com>
2020-05-25 10:15:13 +0100

    plugin notify to restart

--- packages/server/routes/admin.js
+++ packages/server/routes/admin.js
@@ -2,15 +2,21 @@ const Router = require("express-promise-router");
 
 const { setTenant, isAdmin } = require("./utils.js");
 const Table = require("@saltcorn/data/models/table");
-const {  post_btn } = require("@saltcorn/markup");
-const {  div } = require("@saltcorn/markup/tags");
+const { post_btn } = require("@saltcorn/markup");
+const { div } = require("@saltcorn/markup/tags");
 
 const router = new Router();
 module.exports = router;
 
 router.get("/", setTenant, isAdmin, async (req, res) => {
-    res.sendWrap(`Admin`, div("Restart server. Try reloading the page after a few seconds after pressing this button.", post_btn('/admin/restart', 'Restart')));
-})
+  res.sendWrap(
+    `Admin`,
+    div(
+      "Restart server. Try reloading the page after a few seconds after pressing this button.",
+      post_btn("/admin/restart", "Restart")
+    )
+  );
+});
 
 router.post("/restart", setTenant, isAdmin, async (req, res) => {
   process.exit(0);
