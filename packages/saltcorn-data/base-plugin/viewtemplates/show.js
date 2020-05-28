809ffecfc7440d6f3df3e3e20d5ff55097a8f518
Tom Nielsen <tanielsen@gmail.com>
2020-05-28 22:04:34 +0100

    show: accept any state

--- packages/saltcorn-data/base-plugin/viewtemplates/show.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/show.js
@@ -58,21 +58,21 @@ const get_state_fields = () => [
 
 const initial_config = initial_config_all_fields(false);
 
-const run = async (table_id, viewname, { columns, layout }, { id }) => {
+const run = async (table_id, viewname, { columns, layout }, state) => {
   //console.log(columns);
   //console.log(layout);
-  if (typeof id === "undefined") return "No record selected";
   const tbl = await Table.findOne({ id: table_id });
   const fields = await Field.find({ table_id: tbl.id });
   const { joinFields, aggregations } = picked_fields_to_query(columns);
-  const [row] = await tbl.getJoinedRows({
-    where: { id },
+  const rows = await tbl.getJoinedRows({
+    where: state,
     joinFields,
     aggregations,
     limit: 1
   });
+  if (rows.length!==1) return "No record selected";
   //const tfields = get_viewable_fields(viewname, tbl, fields, columns, true);
-  return render(row, fields, layout, viewname, tbl);
+  return render(rows[0], fields, layout, viewname, tbl);
 };
 
 const runMany = async (
