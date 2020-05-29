9ed444a98784c21928b255e373cd938a329faacb
Tom Nielsen <tanielsen@gmail.com>
2020-05-29 11:36:45 +0100

    pp

--- packages/server/load_plugins.js
+++ packages/server/load_plugins.js
@@ -15,20 +15,20 @@ const loadPlugin = async plugin => {
 };
 
 const requirePlugin = async (plugin, force) => {
-  const installed_plugins=(await manager.list()).map(p=>p.name)
+  const installed_plugins = (await manager.list()).map(p => p.name);
   if (
     ["@saltcorn/base-plugin", "@saltcorn/sbadmin2"].includes(plugin.location)
   ) {
     return require(plugin.location);
   } else if (plugin.source === "npm") {
-    if(!force && !installed_plugins.includes(plugin.location))
+    if (!force && !installed_plugins.includes(plugin.location))
       await manager.install(plugin.location, plugin.version);
     return manager.require(plugin.location);
   } else if (plugin.source === "local") {
     await manager.installFromPath(plugin.location, { force: true });
     return manager.require(plugin.name);
   } else if (plugin.source === "github") {
-    if(!force && !installed_plugins.includes(plugin.location))
+    if (!force && !installed_plugins.includes(plugin.location))
       await manager.installFromGithub(plugin.location, { force: true });
     return manager.require(plugin.name);
   }
