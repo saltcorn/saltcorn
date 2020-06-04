f125c9a6600e3f02060497f98a0dd3a6f214b4a4
Tom Nielsen <tanielsen@gmail.com>
2020-06-04 20:49:07 +0100

    show image in show but no size control

--- packages/saltcorn-data/base-plugin/fileviews.js
+++ packages/saltcorn-data/base-plugin/fileviews.js
@@ -1,9 +1,13 @@
-const { text } = require("@saltcorn/markup/tags");
+const { text, img} = require("@saltcorn/markup/tags");
 const { link } = require("@saltcorn/markup");
 
 module.exports = {
   "Download link": {
     run: (file_id, file_name) =>
       link(`/files/download/${file_id}`, file_name || "Download")
+  },
+  "Show Image": {
+    run: (file_id, file_name) =>
+      img({src:`/files/download/${file_id}`})
   }
 };
--- packages/saltcorn-data/base-plugin/viewtemplates/show.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/show.js
@@ -6,6 +6,7 @@ const FieldRepeat = require("../../models/fieldrepeat");
 const { mkTable } = require("@saltcorn/markup");
 const Workflow = require("../../models/workflow");
 const { post_btn, link } = require("@saltcorn/markup");
+const { getState } = require("../../db/state");
 
 const { div, text, span } = require("@saltcorn/markup/tags");
 const {
@@ -132,7 +133,9 @@ const render = async (row, fields, layout, viewname, table, role) => {
     } else if (segment.type === "field") {
       const val = row[segment.field_name];
       const field = fields.find(fld => fld.name === segment.field_name);
-      if (segment.fieldview && field.type.fieldviews[segment.fieldview])
+      if (segment.fieldview && field.type==='File') {
+        return val ? getState().fileviews[segment.fieldview].run(val) :''
+      } else if (segment.fieldview && field.type.fieldviews[segment.fieldview])
         return wrapBlock(
           segment,
           field.type.fieldviews[segment.fieldview].run(val)
