29392d2f3863e50c034a1769b7377bcb3a5f6de6
Tom Nielsen <tanielsen@gmail.com>
2020-06-05 12:12:45 +0100

    force plugin if public schema

--- packages/server/load_plugins.js
+++ packages/server/load_plugins.js
@@ -22,7 +22,7 @@ const requirePlugin = async (plugin, force) => {
   ) {
     return { plugin_module: require(plugin.location) };
   } else if (plugin.source === "npm") {
-    if (!force && !installed_plugins.includes(plugin.location)) {
+    if (force || !installed_plugins.includes(plugin.location)) {
       const plinfo = await manager.install(plugin.location, plugin.version);
       return { plugin_module: manager.require(plugin.location), ...plinfo };
     } else {
@@ -33,7 +33,7 @@ const requirePlugin = async (plugin, force) => {
     await manager.installFromPath(plugin.location, { force: true });
     return { plugin_module: manager.require(plugin.name) };
   } else if (plugin.source === "github") {
-    if (!force && !installed_plugins.includes(plugin.location))
+    if (force || !installed_plugins.includes(plugin.location))
       await manager.installFromGithub(plugin.location, { force: true });
     return { plugin_module: manager.require(plugin.name) };
   }
