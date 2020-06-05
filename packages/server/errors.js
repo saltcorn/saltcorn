da28040cb66025bddaab99a96ca7301189e1c91b
Tom Nielsen <tanielsen@gmail.com>
2020-06-05 15:16:14 +0100

    crash model

--- packages/server/errors.js
+++ packages/server/errors.js
@@ -1,9 +1,10 @@
 const db = require("@saltcorn/data/db");
 const { pre, p } = require("@saltcorn/markup/tags");
-
+const Crash = require("@saltcorn/data/models/crash")
 
 module.exports = async function (err, req, res, next) {
     console.error(err.stack)
+    await Crash.create(err,req)
     res.status(500).sendWrap('Internal Error', pre(err.stack)+p(`A report has been logged and a team of bug-squashing squirrels 
     has been dispatched to deal with the situation.`))
   }
