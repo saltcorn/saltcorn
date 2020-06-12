63de9d822f2b98ae4aa9b7a43fdb8ee38caeead4
Tom Nielsen <tanielsen@gmail.com>
2020-06-12 13:35:10 +0100

    dont crash list on deleted fields

--- packages/saltcorn-data/base-plugin/viewtemplates/viewable_fields.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/viewable_fields.js
@@ -64,64 +64,68 @@ const get_viewable_fields = async (
   isShow,
   csrfToken
 ) =>
-  await asyncMap(columns, async column => {
-    if (column.type === "Action")
-      return {
-        label: column.action_name,
-        key: r =>
-          post_btn(
-            action_url(viewname, table, column, r),
-            column.action_name,
-            csrfToken
-          )
-      };
-    else if (column.type === "ViewLink") {
-      return await view_linker(column, fields);
-    } else if (column.type === "JoinField") {
-      const [refNm, targetNm] = column.join_field.split(".");
-      return {
-        label: text(targetNm),
-        key: text(targetNm)
-        // sortlink: `javascript:sortby('${text(targetNm)}')`
-      };
-    } else if (column.type === "Aggregation") {
-      //console.log(column)
-      const [table, fld] = column.agg_relation.split(".");
-      const targetNm = (column.stat + "_" + table + "_" + fld).toLowerCase();
+  (
+    await asyncMap(columns, async column => {
+      if (column.type === "Action")
+        return {
+          label: column.action_name,
+          key: r =>
+            post_btn(
+              action_url(viewname, table, column, r),
+              column.action_name,
+              csrfToken
+            )
+        };
+      else if (column.type === "ViewLink") {
+        return await view_linker(column, fields);
+      } else if (column.type === "JoinField") {
+        const [refNm, targetNm] = column.join_field.split(".");
+        return {
+          label: text(targetNm),
+          key: text(targetNm)
+          // sortlink: `javascript:sortby('${text(targetNm)}')`
+        };
+      } else if (column.type === "Aggregation") {
+        //console.log(column)
+        const [table, fld] = column.agg_relation.split(".");
+        const targetNm = (column.stat + "_" + table + "_" + fld).toLowerCase();
 
-      return {
-        label: text(column.stat + " " + table),
-        key: text(targetNm)
-        // sortlink: `javascript:sortby('${text(targetNm)}')`
-      };
-    } else if (column.type === "Field") {
-      const f = fields.find(fld => fld.name === column.field_name);
+        return {
+          label: text(column.stat + " " + table),
+          key: text(targetNm)
+          // sortlink: `javascript:sortby('${text(targetNm)}')`
+        };
+      } else if (column.type === "Field") {
+        const f = fields.find(fld => fld.name === column.field_name);
 
-      return {
-        label: text(f.label),
-        key:
-          column.fieldview && f.type === "File"
-            ? row =>
-                row[f.name] &&
-                getState().fileviews[column.fieldview].run(
-                  row[f.name],
-                  row[`${f.name}__filename`]
-                )
-            : column.fieldview && f.type.fieldviews[column.fieldview]
-            ? row =>
-                f.type.fieldviews[column.fieldview].run(
-                  row[f.name],
-                  row[`${f.name}__filename`]
-                )
-            : isShow
-            ? f.type.showAs
-              ? row => f.type.showAs(row[f.name])
-              : row => text(row[f.name])
-            : f.listKey,
-        sortlink: `javascript:sortby('${text(f.name)}')`
-      };
-    }
-  });
+        return (
+          f && {
+            label: text(f.label),
+            key:
+              column.fieldview && f.type === "File"
+                ? row =>
+                    row[f.name] &&
+                    getState().fileviews[column.fieldview].run(
+                      row[f.name],
+                      row[`${f.name}__filename`]
+                    )
+                : column.fieldview && f.type.fieldviews[column.fieldview]
+                ? row =>
+                    f.type.fieldviews[column.fieldview].run(
+                      row[f.name],
+                      row[`${f.name}__filename`]
+                    )
+                : isShow
+                ? f.type.showAs
+                  ? row => f.type.showAs(row[f.name])
+                  : row => text(row[f.name])
+                : f.listKey,
+            sortlink: `javascript:sortby('${text(f.name)}')`
+          }
+        );
+      }
+    })
+  ).filter(v => !!v);
 const stateToQueryString = state => {
   if (!state || Object.keys(state).length === 0) return "";
 
