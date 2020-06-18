ed19b0d14bb6fd0df0c409e37f557dfe041873b0
Tom Nielsen <tanielsen@gmail.com>
2020-06-18 14:30:06 +0100

    create tenant wip

--- packages/e2e/packs.test.js
+++ packages/e2e/packs.test.js
@@ -6,13 +6,16 @@ let browser;
 // 2
 beforeAll(async () => {
   browser = await Browser.init()
-  await browser.page.goto("http://localhost:3000/");
+  //await browser.page.goto("http://localhost:3000/");
 });
 
 test("renders body", async () => {
-    await browser.page.waitForSelector("body");
+    //await browser.page.waitForSelector("body");
+    await browser.create_tenant('sub2')
+    await browser.delete_tenant('sub2')
 })
 
-afterAll(() => {
-    browser.browser.close();
+afterAll(async () => {
+   await browser.close();
+    
   });
--- packages/e2e/utils.js
+++ packages/e2e/utils.js
@@ -1,14 +1,38 @@
 const puppeteer = require("puppeteer");
-
+const { deleteTenant } = require("@saltcorn/data/models/tenant");
+const db = require("@saltcorn/data/db");
 class Browser {
     static async init(o) {
         const b = new Browser()
         b.browser = await puppeteer.launch({
-            headless: false
+            headless: o||false
           });
         b.page = await b.browser.newPage();
         //  await page.goto("http://localhost:3000/");
         return b
+    }
+    async goto(url) {
+        //await this.page.goto(`http://${this.tenant?this.tenant+'.':'' }example.com:3000${url}`);
+        await this.page.goto(`http://localhost:3000${url}`);
+    }
+    async delete_tenant(nm) {
+        await deleteTenant(nm)
+    }
+    async create_tenant(nm) {
+        this.tenant=undefined;
+        this.goto('/tenant/create')
+        
+
+        const page=this.page;await page.waitForNavigation();
+        await page.waitForSelector('form #inputsubdomain')
+        await page.click('form #inputsubdomain')
+        await page.type('form #inputsubdomain', nm);
+        await page.click('button[type=submit]');
+        //await page.waitForNavigation();
+    }
+    async close() {
+        await this.browser.close()
+        await db.close()
     }
 }
 
