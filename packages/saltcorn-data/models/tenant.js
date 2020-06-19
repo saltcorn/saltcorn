e05ab9d2f392bdbbd5fa00621abbb51435145214
Tom Nielsen <tanielsen@gmail.com>
2020-06-19 21:05:11 +0100

    tenant contracts

--- packages/saltcorn-data/models/tenant.js
+++ packages/saltcorn-data/models/tenant.js
@@ -4,37 +4,47 @@ const { contract, is } = require("contractis");
 const { sqlsanitize } = require("../db/internal");
 const User = require("./user");
 
-const getAllTenants = async () => {
-  const tens = await db.select("_sc_tenants");
-  return tens.map(({ subdomain }) => subdomain);
-};
+const getAllTenants = contract(
+  is.fun([], is.promise(is.array(is.str))),
+  async () => {
+    const tens = await db.select("_sc_tenants");
+    return tens.map(({ subdomain }) => subdomain);
+  }
+);
 
-const createTenant = async subdomain => {
-  const saneDomain = domain_sanitize(subdomain);
-  const id = await db.insert(
-    "_sc_tenants",
-    { subdomain: saneDomain, email: "" },
-    true
-  );
-  //create schema
-  await db.query(`CREATE SCHEMA "${saneDomain}";`);
+const createTenant = contract(
+  is.fun(is.str, is.promise(is.undefined)),
+  async subdomain => {
+    const saneDomain = domain_sanitize(subdomain);
+    const id = await db.insert(
+      "_sc_tenants",
+      { subdomain: saneDomain, email: "" },
+      true
+    );
+    //create schema
+    await db.query(`CREATE SCHEMA "${saneDomain}";`);
 
-  // set continuation storage
-  //db.tenantNamespace.set("tenant", saneDomain);
-  await db.runWithTenant(saneDomain, async () => {
-    //reset schema
-    await reset(true, saneDomain);
-  });
-};
+    // set continuation storage
+    //db.tenantNamespace.set("tenant", saneDomain);
+    await db.runWithTenant(saneDomain, async () => {
+      //reset schema
+      await reset(true, saneDomain);
+    });
+  }
+);
 
-const deleteTenant = async sub => {
-  const subdomain = domain_sanitize(sub);
-  await db.query(`drop schema if exists "${subdomain}" CASCADE `);
-  await db.deleteWhere("_sc_tenants", { subdomain });
-  return null;
-};
+const deleteTenant = contract(
+  is.fun(is.str, is.promise(is.undefined)),
+  async sub => {
+    const subdomain = domain_sanitize(sub);
+    await db.query(`drop schema if exists "${subdomain}" CASCADE `);
+    await db.deleteWhere("_sc_tenants", { subdomain });
+  }
+);
 
-const domain_sanitize = s => sqlsanitize(s.replace(".", "").toLowerCase());
+const domain_sanitize = contract(is.fun(is.str, is.str), s =>
+  sqlsanitize(s.replace(".", "").toLowerCase())
+);
 
 module.exports = {
   getAllTenants,
