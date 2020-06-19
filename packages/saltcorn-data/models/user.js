7178915869061628803d63140d0a04b4b8e78a39
Tom Nielsen <tanielsen@gmail.com>
2020-06-19 21:28:30 +0100

    users

--- packages/saltcorn-data/models/user.js
+++ packages/saltcorn-data/models/user.js
@@ -1,5 +1,6 @@
 const db = require("../db");
 const bcrypt = require("bcryptjs");
+const { contract, is } = require("contractis");
 
 class User {
   constructor(o) {
@@ -7,6 +8,7 @@ class User {
     this.password = o.password;
     this.id = o.id;
     this.role_id = o.role_id || 8;
+    contract.class(this);
   }
   static async create(uo) {
     const u = new User(uo);
@@ -47,5 +49,31 @@ class User {
     return rs;
   }
 }
+
+User.contract = {
+  variables: {
+    id: is.maybe(is.posint),
+    email: is.str,
+    password: is.str,
+    role_id: is.posint
+  },
+  methods: {
+    delete: is.fun([], is.promise(is.undefined))
+  },
+  static_methods: {
+    find: is.fun(is.maybe(is.obj()), is.promise(is.array(is.class("User")))),
+    findOne: is.fun(is.obj(), is.promise(is.class("User"))),
+    nonEmpty: is.fun([], is.promise(is.bool)),
+    authenticate: is.fun(
+      is.obj({ email: is.str, password: is.str }),
+      is.promise(is.or(is.class("User"), is.eq(false)))
+    ),
+    create: is.fun(is.obj({ email: is.str }), is.promise(is.class("User"))),
+    get_roles: is.fun(
+      [],
+      is.promise(is.array(is.obj({ id: is.posint, role: is.str })))
+    )
+  }
+};
 
 module.exports = User;
