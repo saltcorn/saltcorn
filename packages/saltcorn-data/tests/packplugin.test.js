5a565d4ef89f8c953c6c81c414f8be6e4316ad75
Tom Nielsen <tanielsen@gmail.com>
2020-06-30 17:07:05 +0100

    more pack create

--- packages/saltcorn-data/tests/packplugin.test.js
+++ packages/saltcorn-data/tests/packplugin.test.js
@@ -61,4 +61,36 @@ describe("pack create", () => {
       name: "patients"
     });
   });
+  it("creates view pack", async () => {
+    const vpack = await view_pack("authorlist");
+    expect(vpack).toStrictEqual({
+      configuration: {
+        columns: [
+          { field_name: "author", state_field: "on", type: "Field" },
+          { type: "ViewLink", view: "Own:authorshow" },
+          { action_name: "Delete", type: "Action" },
+          {
+            agg_field: "name",
+            agg_relation: "patients.favbook",
+            stat: "Count",
+            type: "Aggregation"
+          }
+        ]
+      },
+      is_public: true,
+      name: "authorlist",
+      on_menu: true,
+      on_root_page: true,
+      table: "books",
+      viewtemplate: "List"
+    });
+  });
+  it("creates view pack", async () => {
+    const ppack = await plugin_pack("base");
+    expect(ppack).toStrictEqual({
+      location: "@saltcorn/base-plugin",
+      name: "base",
+      source: "npm"
+    });
+  });
 });
