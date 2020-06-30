a7819aa66de73b61b6ec8f803d660ddf605b5429
Tom Nielsen <tanielsen@gmail.com>
2020-06-30 18:14:21 +0100

    field tests

--- packages/saltcorn-data/tests/table_field.test.js
+++ packages/saltcorn-data/tests/table_field.test.js
@@ -175,6 +175,8 @@ describe("Field", () => {
     expect(fc.id > 0).toBe(true);
     const f = await Field.findOne({ id: fc.id });
     expect(f.toJson.name).toBe("Height1");
+    expect(f.listKey).toBe("Height1");
+    expect(f.presets).toBe(null);
     await f.delete();
     const fs = await Field.find({ name: "Height1" });
     expect(fs.length).toBe(0);
@@ -194,5 +196,24 @@ describe("Field", () => {
     await f.delete();
     const fs = await Field.find({ name: "Height11" });
     expect(fs.length).toBe(0);
+  });
+  it("creates file field", async () => {
+    const patients = await Table.findOne({ name: "patients" });
+    const fc = await Field.create({
+      table: patients,
+      name: "pic",
+      label: "pic",
+      type: "File",
+      required: false
+    });
+    expect(fc.reftable_name).toBe("_sc_files");
+  });
+  it("fills fkey options", async () => {
+    const f = await Field.findOne({ name: "favbook" });
+    await f.fill_fkey_options();
+    expect(f.options).toContainEqual({ label: "Leo Tolstoy", value: 2 });
+    expect(f.sql_type).toBe('int references "public"."books" (id)');
+    expect(f.is_fkey).toBe(true);
+    expect(f.sql_bare_type).toBe("int");
   });
 });
