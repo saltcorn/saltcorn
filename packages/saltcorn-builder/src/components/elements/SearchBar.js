eb80b9b9c4b1cdbb1e690984bd4386b86f5937db
Tom Nielsen <tanielsen@gmail.com>
2020-07-24 22:35:58 +0100

    fix class

--- packages/saltcorn-builder/src/components/elements/SearchBar.js
+++ packages/saltcorn-builder/src/components/elements/SearchBar.js
@@ -6,21 +6,21 @@ export const SearchBar = ({}) => {
     connectors: { connect, drag }
   } = useNode();
   return (
-    <div class="input-group" ref={dom => connect(drag(dom))}>
+    <div className="input-group" ref={dom => connect(drag(dom))}>
       <input
         type="text"
-        class="form-control bg-light"
+        className="form-control bg-light"
         placeholder="Search..."
         disabled
       />
-      <div class="input-group-append">
+      <div className="input-group-append">
         <button
-          class="btn btn-primary"
+          className="btn btn-primary"
           disabled
           type="submit"
           id="button-search-submit"
         >
-          <i class="fas fa-search"></i>
+          <i className="fas fa-search"></i>
         </button>
       </div>
     </div>
