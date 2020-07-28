c33293b4e1ee9764b65dcb159d4b237b04651510
Tom Nielsen <tanielsen@gmail.com>
2020-07-28 22:54:59 +0100

    am able to reset

--- packages/saltcorn-data/migrate.js
+++ packages/saltcorn-data/migrate.js
@@ -3,9 +3,12 @@ const path = require("path");
 const db = require("./db");
 
 const dateFormat = require("dateformat");
+const { ifError } = require("assert");
 
 const migrate = async (schema = "public") => {
   //console.log("migrating",schema )
+  const is_sqlite = db.isSQLite;
+
   const dbmigrationRows = await db.select("_sc_migrations");
   const dbmigrations = dbmigrationRows.map(r => r.migration);
   //https://stackoverflow.com/questions/5364928/node-js-require-all-files-in-a-folder
@@ -13,21 +16,25 @@ const migrate = async (schema = "public") => {
     .readdirSync(path.join(__dirname, "migrations"))
     .filter(file => file.match(/\.js$/) !== null);
 
-  const client = await db.getClient();
+  const client = is_sqlite ? db : await db.getClient();
+  if(!is_sqlite) {
   db.sql_log(`SET search_path TO "${schema}";`);
   await client.query(`SET search_path TO "${schema}";`);
+  }
   for (const file of files) {
     const name = file.replace(".js", "");
     if (!dbmigrations.includes(name)) {
       //console.log("Running migration", name);
       const contents = require(path.join(__dirname, "migrations", name));
       if (contents.sql) {
-        await client.query(contents.sql);
+        if(!contents.sql.includes("DROP COLUMN"))
+          await client.query(contents.sql);
       }
       await db.insert("_sc_migrations", { migration: name }, true);
     }
   }
-  client.release(true);
+  if(!is_sqlite)
+    client.release(true);
 };
 
 const create_blank_migration = async () => {
