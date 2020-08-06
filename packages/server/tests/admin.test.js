1e6c7029f985d13dbd4f44ef4877d7f40691d986
Tom Nielsen <tanielsen@gmail.com>
2020-08-06 18:08:27 +0100

    file admin test

--- packages/server/tests/admin.test.js
+++ packages/server/tests/admin.test.js
@@ -11,9 +11,19 @@ const {
   resetToFixtures
 } = require("../auth/testhelp");
 const db = require("@saltcorn/data/db");
+const fs = require("fs").promises;
+const File = require("@saltcorn/data/models/file");
 
 beforeAll(async () => {
   await resetToFixtures();
+  const mv = async fnm => {
+    await fs.writeFile(fnm, "nevergonnagiveyouup");
+  };
+  await File.from_req_files(
+    { mimetype: "image/png", name: "rick.png", mv, size: 245752 },
+    1,
+    4
+  );
 });
 afterAll(db.close);
 
@@ -122,6 +132,53 @@ describe("menu editor", () => {
       .expect(toInclude("BarMenu"));
   });
 });
-/*
 
-*/
+describe("files admin", () => {
+  itShouldRedirectUnauthToLogin("/files");
+  it("show files list", async () => {
+    const app = await getApp({ disableCsrf: true });
+    const loginCookie = await getAdminLoginCookie();
+    await request(app)
+      .get("/files")
+      .set("Cookie", loginCookie)
+      .expect(toInclude("Size (KiB)"));
+  });
+  it("download file", async () => {
+    const app = await getApp({ disableCsrf: true });
+    const loginCookie = await getStaffLoginCookie();
+    await request(app)
+      .get("/files/download/1")
+      .set("Cookie", loginCookie)
+      .expect(toSucceed);
+  });
+
+  it("serve file", async () => {
+    const app = await getApp({ disableCsrf: true });
+    const loginCookie = await getStaffLoginCookie();
+    await request(app)
+      .get("/files/serve/1")
+      .set("Cookie", loginCookie)
+      .expect(toSucceed);
+  });
+  it("not serve file to public", async () => {
+    const app = await getApp({ disableCsrf: true });
+    await request(app)
+      .get("/files/serve/1")
+      .expect(toRedirect("/"));
+  });
+  it("set file min role", async () => {
+    const app = await getApp({ disableCsrf: true });
+    const loginCookie = await getAdminLoginCookie();
+    await request(app)
+      .post("/files/setrole/1")
+      .set("Cookie", loginCookie)
+      .send("role=10")
+      .expect(toRedirect("/files"));
+  });
+  it("serve file to public after role change", async () => {
+    const app = await getApp({ disableCsrf: true });
+    await request(app)
+      .get("/files/serve/1")
+      .expect(toSucceed);
+  });
+});
