fface99895ba49f2ced7e85776424a5e1bf37090
Tom Nielsen <tanielsen@gmail.com>
2020-08-11 14:57:27 +0100

    dont show error on CSRF token invalid

--- packages/server/errors.js
+++ packages/server/errors.js
@@ -8,10 +8,15 @@ module.exports = async function(err, req, res, next) {
   await Crash.create(err, req);
   const devmode = getState().getConfig("development_mode", false);
 
-  res.status(500).sendWrap(
-    "Internal Error",
-    devmode ? pre(text(err.stack)) : h3("An error occurred"),
-    p(`A report has been logged and a team of bug-squashing squirrels 
+  if (err.message && err.message.includes("invalid csrf token")) {
+    req.flash("error", "Invalid form data, try again");
+    if (req.url && req.url.includes("/auth/login")) res.redirect("/auth/login");
+    else res.redirect("/");
+  } else
+    res.status(500).sendWrap(
+      "Internal Error",
+      devmode ? pre(text(err.stack)) : h3("An error occurred"),
+      p(`A report has been logged and a team of bug-squashing squirrels 
     has been dispatched to deal with the situation.`)
-  );
+    );
 };
