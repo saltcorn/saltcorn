1b8d4742cd02fd6fc5c5c007cf6fc2b6692a6f13
Tom Nielsen <tanielsen@gmail.com>
2020-08-12 13:50:38 +0100

    fix fixtue not public

--- packages/saltcorn-data/db/fixtures.js
+++ packages/saltcorn-data/db/fixtures.js
@@ -130,7 +130,7 @@ module.exports = async () => {
         { type: "JoinField", join_field: "favbook.pages" }
       ]
     },
-    min_role: 10,
+    min_role: 8,
     on_root_page: true
   });
   const readings = await Table.create("readings");
