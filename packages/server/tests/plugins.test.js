4a19f3be13dd7059ed11e097f8a7ac80cb16c24d
Tom Nielsen <tanielsen@gmail.com>
2020-08-17 21:33:58 +0100

    resolve dependencies

--- packages/server/tests/plugins.test.js
+++ packages/server/tests/plugins.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const getApp = require("../app");
 const Table = require("@saltcorn/data/models/table");
 const Plugin = require("@saltcorn/data/models/plugin");
+const { getState } = require("@saltcorn/data/db/state");
 
 const {
   getAdminLoginCookie,
@@ -106,8 +107,14 @@ describe("Plugin dependency resolution", () => {
       .send("source=npm")
       .send("location=%40saltcorn%2Fquill-editor")
       .expect(toRedirect("/plugins"));
-  })
-})
+    const quill = await Plugin.findOne({ name: "quill-editor" });
+    expect(quill.location).toBe("@saltcorn/quill-editor");
+    const html = await Plugin.findOne({ location: "@saltcorn/html" });
+    expect(html.location).toBe("@saltcorn/html");
+    const html_type = getState().types.HTML;
+    expect(!!html_type.fieldviews.Quill).toBe(true);
+  });
+});
 describe("Pack Endpoints", () => {
   it("should show get create", async () => {
     const loginCookie = await getAdminLoginCookie();
