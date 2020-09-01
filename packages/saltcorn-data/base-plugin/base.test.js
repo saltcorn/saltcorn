d92713c6468272b1789460458346262e799bd427
Tom Nielsen <tanielsen@gmail.com>
2020-09-01 22:51:08 +0100

    renderRows

--- packages/saltcorn-data/base-plugin/base.test.js
+++ packages/saltcorn-data/base-plugin/base.test.js
@@ -3,6 +3,10 @@ const plugin = require(".");
 const db = require("../db");
 
 afterAll(db.close);
+beforeAll(async () => {
+  await require("../db/reset_schema")();
+  await require("../db/fixtures")();
+});
 
 describe("base plugin", () => {
   it("passes auto test", async () => {
--- packages/saltcorn-data/base-plugin/viewtemplates/show.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/show.js
@@ -18,25 +18,26 @@ const {
   get_link_view_opts,
   picked_fields_to_query,
   initial_config_all_fields,
-  calcfldViewOptions,
+  calcfldViewOptions
 } = require("../../plugin-helper");
 const { action_url, view_linker } = require("./viewable_fields");
 const db = require("../../db");
+const { asyncMap } = require("../../utils");
 
 const configuration_workflow = () =>
   new Workflow({
     steps: [
       {
         name: "Layout",
-        builder: async (context) => {
+        builder: async context => {
           const table = await Table.findOne({ id: context.table_id });
           const fields = await table.getFields();
           const boolfields = fields.filter(
-            (f) => f.type && f.type.name === "Bool"
+            f => f.type && f.type.name === "Bool"
           );
           const actions = [
             "Delete",
-            ...boolfields.map((f) => `Toggle ${f.name}`),
+            ...boolfields.map(f => `Toggle ${f.name}`)
           ];
           const field_view_options = calcfldViewOptions(fields, false);
           const link_view_opts = await get_link_view_opts(
@@ -47,19 +48,19 @@ const configuration_workflow = () =>
           const { parent_field_list } = await table.get_parent_relations();
           const {
             child_field_list,
-            child_relations,
+            child_relations
           } = await table.get_child_relations();
           var agg_field_opts = {};
           child_relations.forEach(({ table, key_field }) => {
             agg_field_opts[
               `${table.name}.${key_field.name}`
-            ] = table.fields.map((f) => f.name);
+            ] = table.fields.map(f => f.name);
           });
           const views = await View.find_table_views_where(
             context.table_id,
             ({ state_fields, viewtemplate, viewrow }) =>
               viewrow.name !== context.viewname &&
-              state_fields.some((sf) => sf.name === "id")
+              state_fields.some(sf => sf.name === "id")
           );
 
           return {
@@ -72,18 +73,18 @@ const configuration_workflow = () =>
             agg_field_opts,
             roles,
             views,
-            mode: "show",
+            mode: "show"
           };
-        },
-      },
-    ],
+        }
+      }
+    ]
   });
 const get_state_fields = () => [
   {
     name: "id",
     type: "Integer",
-    required: true,
-  },
+    required: true
+  }
 ];
 
 const initial_config = initial_config_all_fields(false);
@@ -93,22 +94,64 @@ const run = async (table_id, viewname, { columns, layout }, state, extra) => {
   //console.log(layout);
   if (!columns || !layout) return "View not yet built";
   const tbl = await Table.findOne({ id: table_id });
-  const fields = await Field.find({ table_id: tbl.id });
+  const fields = await tbl.getFields();
+
   const { joinFields, aggregations } = picked_fields_to_query(columns, fields);
   const qstate = await stateFieldsToWhere({ fields, state, approximate: true });
   const rows = await tbl.getJoinedRows({
     where: qstate,
     joinFields,
     aggregations,
-    limit: 1,
+    limit: 1
   });
-  const role = extra.req.user ? extra.req.user.role_id : 10;
   if (rows.length !== 1) return "No record selected";
-  await eachView(layout, async (segment) => {
-    const view = await View.findOne({ name: segment.view });
-    segment.contents = await view.run(state, extra);
+
+  return (await renderRows(tbl, viewname, { columns, layout }, extra, rows))[0];
+};
+
+const renderRows = async (
+  table,
+  viewname,
+  { columns, layout },
+  extra,
+  rows
+) => {
+  //console.log(columns);
+  //console.log(layout);
+  if (!columns || !layout) return "View not yet built";
+
+  const fields = await table.getFields();
+
+  const role = extra.req.user ? extra.req.user.role_id : 10;
+  var views = {};
+  const getView = async nm => {
+    if (views[nm]) return views[nm];
+    const view = await View.findOne({ name: nm });
+    view.table = await Table.findOne({ id: view.table_id });
+    views[nm] = view;
+    return view;
+  };
+
+  return await asyncMap(rows, async row => {
+    await eachView(layout, async segment => {
+      const view = await getView(segment.view);
+
+      if (view.viewtemplateObj.renderRows) {
+        segment.contents = (
+          await view.viewtemplateObj.renderRows(
+            view.table,
+            view.name,
+            view.configuration,
+            extra,
+            [row]
+          )
+        )[0];
+      } else {
+        segment.contents = await view.run({ id: row.id }, extra);
+      }
+    });
+    return render(row, fields, layout, viewname, table, role, extra.req);
   });
-  return render(rows[0], fields, layout, viewname, tbl, role, extra.req);
 };
 
 const runMany = async (
@@ -119,7 +162,7 @@ const runMany = async (
   extra
 ) => {
   const tbl = await Table.findOne({ id: table_id });
-  const fields = await Field.find({ table_id: tbl.id });
+  const fields = await tbl.getFields();
   const { joinFields, aggregations } = picked_fields_to_query(columns, fields);
   const qstate = await stateFieldsToWhere({ fields, state });
   const rows = await tbl.getJoinedRows({
@@ -127,21 +170,25 @@ const runMany = async (
     joinFields,
     aggregations,
     ...(extra && extra.orderBy && { orderBy: extra.orderBy }),
-    ...(extra && extra.orderDesc && { orderDesc: extra.orderDesc }),
+    ...(extra && extra.orderDesc && { orderDesc: extra.orderDesc })
   });
-  const role = extra.req && extra.req.user ? extra.req.user.role_id : 10;
 
-  return rows.map((row) => ({
-    html: render(row, fields, layout, viewname, tbl, role, extra.req),
-    row,
-  }));
+  const rendered = await renderRows(
+    tbl,
+    viewname,
+    { columns, layout },
+    extra,
+    rows
+  );
+
+  return rendered.map((html, ix) => ({ html, row: rows[ix] }));
 };
 
 const render = (row, fields, layout, viewname, table, role, req) => {
   const blockDispatch = {
     field({ field_name, fieldview }) {
       const val = row[field_name];
-      const field = fields.find((fld) => fld.name === field_name);
+      const field = fields.find(fld => fld.name === field_name);
       if (fieldview && field.type === "File") {
         return val ? getState().fileviews[fieldview].run(val) : "";
       } else if (fieldview && field.type.fieldviews[fieldview])
@@ -169,7 +216,7 @@ const render = (row, fields, layout, viewname, table, role, req) => {
     view_link(view) {
       const { key } = view_linker(view, fields);
       return key(row);
-    },
+    }
   };
   return renderLayout({ blockDispatch, layout, role });
 };
@@ -180,6 +227,7 @@ module.exports = {
   configuration_workflow,
   run,
   runMany,
+  renderRows,
   initial_config,
-  display_state_form: false,
+  display_state_form: false
 };
