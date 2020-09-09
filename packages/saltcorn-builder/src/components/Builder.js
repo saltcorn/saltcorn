1122a229dcd5e51a06c5888352660e59831e0707
Tom Nielsen <tanielsen@gmail.com>
2020-09-09 12:22:35 +0100

    show elem name in settings

--- packages/saltcorn-builder/src/components/Builder.js
+++ packages/saltcorn-builder/src/components/Builder.js
@@ -33,6 +33,9 @@ const SettingsPanel = () => {
       selected = {
         id: currentNodeId,
         name: state.nodes[currentNodeId].data.name,
+        displayName:
+          state.nodes[currentNodeId].data &&
+          state.nodes[currentNodeId].data.displayName,
         settings:
           state.nodes[currentNodeId].related &&
           state.nodes[currentNodeId].related.settings,
@@ -44,13 +47,17 @@ const SettingsPanel = () => {
       selected,
     };
   });
-
   return (
     <div className="settings-panel card mt-2">
-      <div className="card-header">Settings</div>
+      <div className="card-header">
+        {selected && selected.displayName
+          ? `Settings: ${selected.displayName}`
+          : "Settings"}
+      </div>
       <div className="card-body">
         {selected ? (
           <Fragment>
+            {}
             {selected.settings && React.createElement(selected.settings)}
             {selected.isDeletable && (
               <button
