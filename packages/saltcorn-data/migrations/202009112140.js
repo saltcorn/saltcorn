e761aec38ec372dca249374950cf6d5096520625
Tom Nielsen <tanielsen@gmail.com>
2020-09-12 09:13:03 +0100

    add sqlite migration

--- packages/saltcorn-data/migrations/202009112140.js
+++ packages/saltcorn-data/migrations/202009112140.js
@@ -1,4 +1,6 @@
-const sql = `ALTER TABLE users
+const sql_pg = `ALTER TABLE users
 ADD CONSTRAINT users_unique_email UNIQUE (email);`;
 
-module.exports = { sql };
+const sql_sqlite = `create unique index users_unique_email on users(email);`;
+
+module.exports = { sql_pg, sql_sqlite };
