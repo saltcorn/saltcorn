6e047bdd2f09da51c33dcb2cbedf59bf3ce1b714
Tom Nielsen <tanielsen@gmail.com>
2020-09-19 15:36:40 +0100

    send email

--- packages/server/auth/resetpw.js
+++ packages/server/auth/resetpw.js
@@ -1,9 +1,52 @@
 const { getState } = require("@saltcorn/data/db/state");
 const { get_base_url } = require("../routes/utils");
+const nodemailer = require("nodemailer");
+
+const { port, secure } = (() => {
+  const port = getState().getConfig("smtp_port");
+  const secure = getState().getConfig("smtp_secure", port === 465);
+  return { port, secure };
+})();
+
+const transporter = nodemailer.createTransport({
+  host: getState().getConfig("smtp_host"),
+  port,
+  secure,
+  auth: {
+    user: getState().getConfig("smtp_username"),
+    pass: getState().getConfig("smtp_password"),
+  },
+});
 
 const send_reset_email = async (user, req) => {
   const link = await get_reset_link(user, req);
-  console.log(link);
+
+  let info = await transporter.sendMail({
+    from: getState().getConfig("email_from"), // sender address
+    to: user.email, // list of receivers
+    subject: "Reset password instructions", // Subject line
+    text: `Hi ${user.email},
+
+You have requested a link to change your password. You can do this through this link:
+
+${link}
+
+If you did not request this, please ignore this email.
+
+Your password will not change until you access the link above and set a new one.
+`,
+    html: `Hi ${user.email},
+    
+You have requested a link to change your password. You can do this through this link:
+
+<a href="${link}">Change my password</a>
+
+If you did not request this, please ignore this email.
+
+Your password will not change until you access the link above and set a new one.
+`,
+  });
+  console.log(link, info);
 };
 
 const get_reset_link = async (user, req) => {
