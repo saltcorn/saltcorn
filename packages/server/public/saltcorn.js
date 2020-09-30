dbe6880ae7a8ddc94f2deee11119bfa14a126ba1
Tom Nielsen <tanielsen@gmail.com>
2020-09-30 10:38:33 +0100

    create modal, ajax

--- packages/server/public/saltcorn.js
+++ packages/server/public/saltcorn.js
@@ -201,5 +201,29 @@ function press_store_button(clicked) {
 }
 
 function ajax_modal(url) {
-  console.log(url);
+  if ($("#scmodal").length === 0) {
+    $("body").append(`<div id="scmodal", class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>`);
+  }
+  $.ajax(url).done(function (res) {
+    $("#scmodal .modal-body").html(res);
+    $("#scmodal").modal();
+  });
 }
