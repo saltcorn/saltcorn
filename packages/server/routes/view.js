a0d3d81b00b2536a4b83962791b2b0b5fb473edd
Tom Nielsen <tanielsen@gmail.com>
2020-10-08 22:12:54 +0100

    rm view in two places

--- packages/server/routes/view.js
+++ packages/server/routes/view.js
@@ -29,7 +29,7 @@ router.get(
       const state_form = await view.get_state_form(state);
 
       res.sendWrap(
-        `${view.name} view`,
+        `${view.name}`,
         div(state_form ? renderForm(state_form, req.csrfToken()) : "", resp)
       );
     }
