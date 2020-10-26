b02955d2edb8ce380a4b3bdb2bd5cb0d72fa3e81
Tom Nielsen <tanielsen@gmail.com>
2020-10-26 18:04:04 +0000

    migration fix for sqlite

--- packages/saltcorn-data/migrations/202010251412.js
+++ packages/saltcorn-data/migrations/202010251412.js
@@ -1,6 +1,6 @@
 const sql = [
   `insert into _sc_tables(name,min_role_read,min_role_write,versioned) 
-       values('users',1,1,'false');`,
+       values('users',1,1,false);`,
   `insert into _sc_fields(table_id, name, label, type, attributes)
        values((select id from _sc_tables where name='users'),'email','Email','String', '{}')
   ;`,
