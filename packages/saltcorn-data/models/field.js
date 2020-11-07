8acedee3f5584e2d52f0696cfee5a529f3642972
Tom Nielsen <tanielsen@gmail.com>
2020-11-07 12:01:05 +0000

    fix autotest

--- packages/saltcorn-data/models/field.js
+++ packages/saltcorn-data/models/field.js
@@ -174,10 +174,8 @@ class Field {
       );
       if (rows.length === 1) return rows[0].id;
     } else {
-      if (this.type.contract) {
-        if (this.type.contract)
-          return this.type.contract(this.attributes).generate();
-      }
+      if (this.type && this.type.contract)
+        return this.type.contract(this.attributes).generate();
     }
   }
 
