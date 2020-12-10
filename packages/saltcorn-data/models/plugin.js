2b0057c771a0c555ca58f1576f876811948f9938
Tom Nielsen <tanielsen@gmail.com>
2020-12-10 15:44:53 +0000

    on tenants, hide auth plugins

--- packages/saltcorn-data/models/plugin.js
+++ packages/saltcorn-data/models/plugin.js
@@ -79,12 +79,17 @@ class Plugin {
       "available_plugins_fetched_at",
       false
     );
+    const isRoot = db.getTenantSchema() === db.connectObj.default_schema;
+
     if (!stored || !stored_at || is_stale(stored_at)) {
       const from_api = await Plugin.store_plugins_available_from_store();
       await getState().setConfig("available_plugins", from_api);
       await getState().setConfig("available_plugins_fetched_at", new Date());
-      return from_api;
-    } else return stored.map((p) => new Plugin(p));
+      return from_api.filter((p) => isRoot || !p.has_auth);
+    } else
+      return stored
+        .map((p) => new Plugin(p))
+        .filter((p) => isRoot || !p.has_auth);
   }
   static async store_plugins_available_from_store() {
     //console.log("fetch plugins");
