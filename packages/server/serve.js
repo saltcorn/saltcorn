026eac7fb11e9c4af138dbf58f9c6ec19695f6b2
Tom Nielsen <tanielsen@gmail.com>
2020-12-29 14:41:08 +0000

    install greenlock

--- packages/server/serve.js
+++ packages/server/serve.js
@@ -1,10 +1,27 @@
 const runScheduler = require("@saltcorn/data/models/scheduler");
+const User = require("@saltcorn/data/models/user");
+const db = require("@saltcorn/data/db");
+const path = require("path");
+
 const getApp = require("./app");
 
 module.exports = async ({ port = 3000, ...appargs } = {}) => {
   const app = await getApp(appargs);
   runScheduler();
-  app.listen(port, () => {
-    console.log(`Saltcorn listening on http://127.0.0.1:${port}/`);
-  });
+  if (port === 80) {
+    const admin_users = await User.find({ role_id: 1 }, { orderBy: "id" });
+    const file_store = db.connectObj.file_store;
+
+    require("greenlock-express")
+      .init({
+        packageRoot: __dirname,
+        configDir: path.join(file_store, "greenlock.d"),
+        maintainerEmail: admin_users[0].email,
+        cluster: false,
+      })
+      .serve(app);
+  } else
+    app.listen(port, () => {
+      console.log(`Saltcorn listening on http://127.0.0.1:${port}/`);
+    });
 };
