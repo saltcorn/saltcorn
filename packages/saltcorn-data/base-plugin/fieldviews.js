a8de955f251170261a8b2eef4c8d7cc8f6d989b4
Tom Nielsen <tanielsen@gmail.com>
2021-01-19 17:49:35 +0000

    opts

--- packages/saltcorn-data/base-plugin/fieldviews.js
+++ packages/saltcorn-data/base-plugin/fieldviews.js
@@ -1,24 +1,29 @@
+const View = require("../models/view");
+const Table = require("../models/table");
+
 const search_or_create = {
   type: "Key",
   isEdit: true,
   configFields: async (field) => {
+    const reftable = await Table.findOne({ name: field.reftable_name });
+    const views = await View.find({ table_id: reftable.id });
     return [
-      // which view to create with
       {
-        name: "allow_input",
-        label: "Allow input",
-        type: "Bool",
+        name: "viewname",
+        label: "View to create",
+        input_type: "select",
+
+        options: views.map((v) => v.name),
       },
       {
-        name: "day_only",
-        label: "Only day",
-        type: "Bool",
-        //sublabel: "Do not pick time",
+        name: "label",
+        label: "Label to create",
+        type: "String",
       },
     ];
   },
   run: (nm, v, attrs, cls, reqd, field) => {
-    return "BOO";
+    return "BOO" + JSON.stringify(attrs);
   },
 };
 module.exports = { search_or_create };
