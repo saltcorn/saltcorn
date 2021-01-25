79f3e2f577592e1fd3858808d2829d7397edf7bd
Tom Nielsen <tanielsen@gmail.com>
2021-01-25 10:49:27 +0000

    zip_restore cli

--- packages/saltcorn-cli/src/commands/restore.js
+++ packages/saltcorn-cli/src/commands/restore.js
@@ -1,18 +1,15 @@
 const { Command, flags } = require("@oclif/command");
 const { spawnSync } = require("child_process");
-
-const env = process.env;
+const path = require("path");
 
 class RestoreCommand extends Command {
-  async run() {
-    const { args } = this.parse(RestoreCommand);
+  async pg_restore(fnm) {
     const { getConnectObject } = require("@saltcorn/data/db/connect");
     const connobj = getConnectObject();
 
     const pgdb = connobj.database;
     const pguser = connobj.user;
     const pghost = connobj.host || "localhost";
-    const fnm = args.file;
     const env = { ...process.env, PGPASSWORD: connobj.password };
     const res = spawnSync(
       "pg_restore",
@@ -21,13 +18,40 @@ class RestoreCommand extends Command {
     );
     this.exit(res.status);
   }
+  async zip_restore(fnm) {
+    const { restore } = require("@saltcorn/data/models/backup");
+    const User = require("@saltcorn/data/models/user");
+    const hasUsers = await User.nonEmpty();
+    const savePlugin = (p) => {
+      return p.upsert();
+    };
+    const err = await restore(fnm, savePlugin, !hasUsers);
+    if (err) {
+      console.error(err);
+      this.exit(1);
+    } else this.exit(0);
+  }
+  async run() {
+    const { args } = this.parse(RestoreCommand);
+    switch (path.extname(args.file)) {
+      case ".sqlc":
+        this.pg_restore(args.file);
+        break;
+      case ".zip":
+        this.zip_restore(args.file);
+        break;
+      default:
+        console.error("unknown filetype: " + path.extname(args.file));
+        this.exit(1);
+    }
+  }
 }
 
 RestoreCommand.args = [
   { name: "file", required: true, description: "backup file to restore" },
 ];
 
-RestoreCommand.description = `Restore a previously backed up database from a file with pg_dump`;
+RestoreCommand.description = `Restore a previously backed up database (zip or sqlc format)`;
 
 RestoreCommand.flags = {};
 
