328d284b9702ee1ee8f3b0fad4802d68967639af
Tom Nielsen <tanielsen@gmail.com>
2021-01-25 17:06:31 +0000

    more intervals (#558)

--- packages/saltcorn-data/models/scheduler.js
+++ packages/saltcorn-data/models/scheduler.js
@@ -2,6 +2,7 @@ const Crash = require("./crash");
 const { eachTenant } = require("./tenant");
 const Trigger = require("./trigger");
 const db = require("../db");
+const { getState } = require("../db/state");
 
 const sleepUntil = (date, plusSeconds) => {
   const waitTill = new Date();
@@ -9,6 +10,28 @@ const sleepUntil = (date, plusSeconds) => {
   const now = new Date();
   const ms = waitTill.getTime() - now.getTime();
   return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const getIntervalTriggersDueNow = async (name, hours) => {
+  const state = getState();
+  const cfgField = `next_${name.toLowerCase()}_event`;
+  const now = new Date();
+  let due = state.getConfig(cfgField, false);
+  if (!due) {
+    //first run, set rnd due
+    const due_in_hrs = Math.random() * hours;
+    if (hours < 4) now.setMinutes(now.getMinutes() + due_in_hrs * 60);
+    else now.setHours(now.getHours() + due_in_hrs);
+    await state.setConfig(cfgField, now);
+    return [];
+  }
+  due = new Date(due);
+  if (due > now) return [];
+  const triggers = await Trigger.find({ when_trigger: name });
+  due.setHours(due.getHours() + hours);
+  await state.setConfig(cfgField, due);
+
+  return triggers;
 };
 
 const runScheduler = async ({
@@ -23,7 +46,16 @@ const runScheduler = async ({
       const isRoot = db.getTenantSchema() === db.connectObj.default_schema;
 
       const triggers = await Trigger.find({ when_trigger: "Often" });
-      for (const trigger of triggers) {
+      const trsHourly = await getIntervalTriggersDueNow("Hourly", 1);
+      const trsDaily = await getIntervalTriggersDueNow("Daily", 24);
+      const trsWeekly = await getIntervalTriggersDueNow("Weekly", 24 * 7);
+      const allTriggers = [
+        ...triggers,
+        ...trsHourly,
+        ...trsDaily,
+        ...trsWeekly,
+      ];
+      for (const trigger of allTriggers) {
         try {
           await trigger.runWithoutRow();
         } catch (e) {
