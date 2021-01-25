2267ddb585b8506bae0341cb2bae58303e73aca4
Tom Nielsen <tanielsen@gmail.com>
2021-01-25 10:24:57 +0000

    pg restore similar to backup

--- packages/saltcorn-cli/src/commands/restore.js
+++ packages/saltcorn-cli/src/commands/restore.js
@@ -6,15 +6,18 @@ const env = process.env;
 class RestoreCommand extends Command {
   async run() {
     const { args } = this.parse(RestoreCommand);
+    const { getConnectObject } = require("@saltcorn/data/db/connect");
+    const connobj = getConnectObject();
 
-    const pgdb = env.PGDATABASE || "saltcorn";
-    const pguser = env.PGUSER;
+    const pgdb = connobj.database;
+    const pguser = connobj.user;
+    const pghost = connobj.host || "localhost";
     const fnm = args.file;
-
+    const env = { ...process.env, PGPASSWORD: connobj.password };
     const res = spawnSync(
       "pg_restore",
-      ["-d", pgdb, "-U", pguser, "-h", "localhost", fnm],
-      { stdio: "inherit" }
+      ["-d", pgdb, "-U", pguser, "-h", pghost, fnm],
+      { stdio: "inherit", env }
     );
     this.exit(res.status);
   }
@@ -24,7 +27,7 @@ RestoreCommand.args = [
   { name: "file", required: true, description: "backup file to restore" },
 ];
 
-RestoreCommand.description = `Restore a previously backed up database from a file`;
+RestoreCommand.description = `Restore a previously backed up database from a file with pg_dump`;
 
 RestoreCommand.flags = {};
 
