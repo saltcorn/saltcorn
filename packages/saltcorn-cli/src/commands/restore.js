f6606c87a326515dbeed97e69b2606c4e90cefee
Tom Nielsen <tanielsen@gmail.com>
2021-01-25 12:12:38 +0000

    Restore zip backups from cli (#556)
    * restore: optional skip first user
    
    * pg restore similar to backup
    
    * zip_restore cli
    
    * load plugins
    
    * load all plugins
    
    * dont exit on success

--- packages/saltcorn-cli/src/commands/restore.js
+++ packages/saltcorn-cli/src/commands/restore.js
@@ -1,22 +1,49 @@
 const { Command, flags } = require("@oclif/command");
 const { spawnSync } = require("child_process");
-
-const env = process.env;
+const path = require("path");
 
 class RestoreCommand extends Command {
-  async run() {
-    const { args } = this.parse(RestoreCommand);
+  async pg_restore(fnm) {
+    const { getConnectObject } = require("@saltcorn/data/db/connect");
+    const connobj = getConnectObject();
 
-    const pgdb = env.PGDATABASE || "saltcorn";
-    const pguser = env.PGUSER;
-    const fnm = args.file;
-
+    const pgdb = connobj.database;
+    const pguser = connobj.user;
+    const pghost = connobj.host || "localhost";
+    const env = { ...process.env, PGPASSWORD: connobj.password };
     const res = spawnSync(
       "pg_restore",
-      ["-d", pgdb, "-U", pguser, "-h", "localhost", fnm],
-      { stdio: "inherit" }
+      ["-d", pgdb, "-U", pguser, "-h", pghost, fnm],
+      { stdio: "inherit", env }
     );
     this.exit(res.status);
+  }
+  async zip_restore(fnm) {
+    const { restore } = require("@saltcorn/data/models/backup");
+    const User = require("@saltcorn/data/models/user");
+    const load_plugins = require("@saltcorn/server/load_plugins");
+    await load_plugins.loadAllPlugins();
+    const hasUsers = await User.nonEmpty();
+    const savePlugin = (p) => load_plugins.loadAndSaveNewPlugin(p);
+    const err = await restore(fnm, savePlugin, !hasUsers);
+    if (err) {
+      console.error(err);
+      this.exit(1);
+    }
+  }
+  async run() {
+    const { args } = this.parse(RestoreCommand);
+    switch (path.extname(args.file)) {
+      case ".sqlc":
+        this.pg_restore(args.file);
+        break;
+      case ".zip":
+        this.zip_restore(args.file);
+        break;
+      default:
+        console.error("unknown filetype: " + path.extname(args.file));
+        this.exit(1);
+    }
   }
 }
 
@@ -24,7 +51,7 @@ RestoreCommand.args = [
   { name: "file", required: true, description: "backup file to restore" },
 ];
 
-RestoreCommand.description = `Restore a previously backed up database from a file`;
+RestoreCommand.description = `Restore a previously backed up database (zip or sqlc format)`;
 
 RestoreCommand.flags = {};
 
