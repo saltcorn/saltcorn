7bb04f5524753497490e5a36d7c9795ee36cb50e
Tom Nielsen <tanielsen@gmail.com>
2021-01-28 14:53:44 +0000

    rm log

--- packages/saltcorn-data/base-plugin/viewtemplates/edit.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/edit.js
@@ -266,7 +266,6 @@ const run = async (table_id, viewname, config, state, { res, req }) => {
   }
   traverseSync(form.layout, {
     action(segment) {
-      console.log(segment);
       if (segment.action_name === "Delete") {
         if (form.values.id) {
           segment.action_url = `/delete/${table.name}/${form.values.id}`;
