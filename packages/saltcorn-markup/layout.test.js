a8eada4c48ce5d9c6b8eb4a399442d57fdfb6d27
Tom Nielsen <tanielsen@gmail.com>
2021-01-28 15:26:42 +0000

    layout test

--- packages/saltcorn-markup/layout.test.js
+++ packages/saltcorn-markup/layout.test.js
@@ -14,4 +14,26 @@ describe("layout", () => {
     const markup = { above: [{ type: "reverseIt", theString: "foobar" }] };
     expect(render({ blockDispatch, layout: markup })).toBe("<p>raboof</p>");
   });
+  it("renders a nested layout", () => {
+    const blockDispatch = {};
+    const markup = {
+      above: [
+        {
+          besides: [
+            { type: "blank", contents: "hello" },
+            { type: "blank", contents: "world" },
+          ],
+        },
+        {
+          besides: [
+            { type: "blank", contents: "bar" },
+            { type: "blank", contents: "foo" },
+          ],
+        },
+      ],
+    };
+    expect(render({ blockDispatch, layout: markup })).toBe(
+      '<div class="row"><div class="col-6">hello</div><div class="col-6">world</div></div><div class="row"><div class="col-6">bar</div><div class="col-6">foo</div></div>'
+    );
+  });
 });
