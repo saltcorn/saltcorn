1e57ad8ee3272a43cfb39cf42c1e3d4fae0982a7
Tom Nielsen <tanielsen@gmail.com>
2021-01-29 16:06:37 +0000

    no self contained

--- infosec-scan/owasp-zap.sh
+++ infosec-scan/owasp-zap.sh
@@ -7,7 +7,21 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
-source infosec_scan_tmp/zapcli/bin/activate
-export ZAP_PATH=$PWD/infosec_scan_tmp/ZAP_2.10.0
 
- zap-cli quick-scan --self-contained --spider -r -s xss $1
+infosec_scan_tmp/ZAP_2.10.0/zap.sh \
+   -daemon -config api.disablekey=true -port 8090 &
+ZAP_PID=$!
+source infosec_scan_tmp/zapcli/bin/activate
+
+
+# wait for zap port to open
+while ! nc -z localhost 8090; do   
+  sleep 0.1 
+done
+
+zap-cli quick-scan --spider -r -s all $1
+zap-cli report -o zap.html -f html
+zap-cli shutdown
+
+sleep 1
+kill $ZAP_PID || echo ''
