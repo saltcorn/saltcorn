ea47a8d38493c60bfdb178ea00168660f9484c85
Tom Nielsen <tanielsen@gmail.com>
2021-02-07 14:39:13 +0000

    tenant backup

--- packages/saltcorn-cli/src/commands/backup.js
+++ packages/saltcorn-cli/src/commands/backup.js
@@ -15,25 +15,47 @@ class BackupCommand extends Command {
   async run() {
     const { flags } = this.parse(BackupCommand);
 
-    const pguser = connobj.user;
-    const pghost = connobj.host || "localhost";
-    const outfnm = flags.output || default_filenm;
-    const env = { ...process.env, PGPASSWORD: connobj.password };
-    execSync(`pg_dump ${pgdb} -U ${pguser} -h ${pghost} -F c >${outfnm}`, {
-      stdio: "inherit",
-      env,
-    });
-    console.log(outfnm);
+    if (flags.tenant) {
+      const { create_backup } = require("@saltcorn/data/models/backup");
+
+      const db = require("@saltcorn/data/db");
+      await db.runWithTenant(flags.tenant, async () => {
+        const fnm = await create_backup(flags.output);
+        console.log(fnm);
+      });
+    } else if (flags.zip) {
+      const { create_backup } = require("@saltcorn/data/models/backup");
+      const fnm = await create_backup(flags.output);
+      console.log(fnm);
+    } else {
+      const pguser = connobj.user;
+      const pghost = connobj.host || "localhost";
+      const outfnm = flags.output || default_filenm;
+      const env = { ...process.env, PGPASSWORD: connobj.password };
+      execSync(`pg_dump ${pgdb} -U ${pguser} -h ${pghost} -F c >${outfnm}`, {
+        stdio: "inherit",
+        env,
+      });
+      console.log(outfnm);
+    }
   }
 }
 
-BackupCommand.description = `Backup the PostgreSQL database to a file with pg_dump`;
+BackupCommand.description = `Backup the PostgreSQL database to a file with pg_dump or zip`;
 
 BackupCommand.flags = {
   output: flags.string({
     char: "o",
     description: "output filename",
     default: default_filenm,
+  }),
+  tenant: flags.string({
+    char: "t",
+    description: "tenant",
+  }),
+  zip: flags.boolean({
+    char: "z",
+    description: "zip format",
   }),
 };
 
