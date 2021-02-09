0e64ec2929cb7b8438dcaaeab8a759be1bde50b0
Tom Nielsen <tanielsen@gmail.com>
2021-02-09 21:49:15 +0000

    try catch in migration

--- packages/saltcorn-data/migrations/202102091312.js
+++ packages/saltcorn-data/migrations/202102091312.js
@@ -5,11 +5,15 @@ const js = async () => {
   const tables = await Table.find({});
   const schema = db.getTenantSchemaPrefix();
   for (const t of tables) {
-    await db.query(
-      `insert into ${schema}_sc_fields(table_id, name, label, type, attributes, required, is_unique,primary_key)
+    try {
+      await db.query(
+        `insert into ${schema}_sc_fields(table_id, name, label, type, attributes, required, is_unique,primary_key)
           values($1,'id','ID','Integer', '{}', true, true, true)`,
-      [t.id]
-    );
+        [t.id]
+      );
+    } catch (e) {
+      console.error(e);
+    }
   }
 };
 module.exports = { sql, js };
