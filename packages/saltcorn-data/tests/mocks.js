47374568741604b02818833f206294892c1121d5
Tom Nielsen <tanielsen@gmail.com>
2021-02-10 15:03:11 +0000

    uuid type in mock plugin

--- packages/saltcorn-data/tests/mocks.js
+++ packages/saltcorn-data/tests/mocks.js
@@ -54,6 +54,37 @@ const resetActionCounter = () => {
 };
 const plugin_with_routes = {
   sc_plugin_api_version: 1,
+  types: [
+    {
+      name: "UUID",
+      sql_name: "uuid",
+      primaryKey: { default_sql: "uuid_generate_v4()" },
+      fieldviews: {
+        show: { isEdit: false, run: (v) => v || "" },
+        editHTML: {
+          isEdit: true,
+          run: (nm, v, attrs, cls, required, field) =>
+            input({
+              type: "text",
+              disabled: attrs.disabled,
+              class: ["form-control", cls],
+              "data-fieldname": field.name,
+              name: nm,
+              id: `input${nm}`,
+              value: v || "",
+            }),
+        },
+      },
+      read: (v) => {
+        switch (typeof v) {
+          case "string":
+            return v;
+          default:
+            return undefined;
+        }
+      },
+    },
+  ],
   actions: {
     incrementCounter: {
       run: () => {
