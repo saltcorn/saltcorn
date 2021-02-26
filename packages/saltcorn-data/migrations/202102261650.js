8dfb3d249a1b0eff093a207c68cd6d0d88afa5df
Tom Nielsen <tanielsen@gmail.com>
2021-02-26 22:56:46 +0000

    migration sqlite-friendly

--- packages/saltcorn-data/migrations/202102261650.js
+++ packages/saltcorn-data/migrations/202102261650.js
@@ -1,5 +1,6 @@
-
-const sql = 'alter table users add column "verification_token" text, add column "verified_on" timestamp;';
+const sql = [
+  'alter table users add column "verification_token" text',
+  'alter table users add column "verified_on" timestamp;',
+];
 
 module.exports = { sql };
-    
