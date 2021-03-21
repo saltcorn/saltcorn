086e14d202743ccd92ed10bf0f5164669c089fd7
Tom Nielsen <tanielsen@gmail.com>
2021-03-21 11:09:32 +0000

    set role from cli for create user

--- packages/saltcorn-cli/src/commands/create-user.js
+++ packages/saltcorn-cli/src/commands/create-user.js
@@ -6,11 +6,26 @@ class CreateUserCommand extends Command {
   async run() {
     const User = require("@saltcorn/data/models/user");
     const { flags } = this.parse(CreateUserCommand);
+    if (flags.admin && flags.role && flags.role !== "admin") {
+      console.error("Error: specify at most one of admin and role");
+      this.exit(1);
+    }
+
     await maybe_as_tenant(flags.tenant, async () => {
+      let role_id = flags.admin ? 1 : 8;
+      if (flags.role) {
+        const roles = await User.get_roles();
+        const role = roles.find((r) => r.role === flags.role);
+        if (!role) {
+          console.error(`Error: role ${flags.role} not found`);
+          this.exit(1);
+        }
+        role_id = role.id;
+      }
       const email = flags.email || (await cli.prompt("Email address"));
       const password =
         flags.password || (await cli.prompt("Password", { type: "hide" }));
-      await User.create({ email, password, role_id: flags.admin ? 1 : 8 });
+      await User.create({ email, password, role_id });
     });
     this.exit(0);
   }
@@ -27,6 +42,10 @@ CreateUserCommand.flags = {
   email: flags.string({
     char: "e",
     description: "email",
+  }),
+  role: flags.string({
+    char: "r",
+    description: "role",
   }),
   password: flags.string({
     char: "p",
