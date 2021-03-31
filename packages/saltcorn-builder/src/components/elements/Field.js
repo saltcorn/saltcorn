890e458ca01d4d9a560f0b0fcbc8f3e1118d8876
Tom Nielsen <tanielsen@gmail.com>
2021-03-31 11:27:41 +0100

    fv preview works

--- packages/saltcorn-builder/src/components/elements/Field.js
+++ packages/saltcorn-builder/src/components/elements/Field.js
@@ -1,6 +1,7 @@
 import React, { useContext, Fragment } from "react";
 import { useNode } from "@craftjs/core";
 import optionsCtx from "../context";
+import previewCtx from "../preview_context";
 import {
   blockProps,
   BlockSetting,
@@ -12,15 +13,25 @@ import {
 export const Field = ({ name, fieldview, block, textStyle }) => {
   const {
     selected,
+    node_id,
     connectors: { connect, drag },
-  } = useNode((node) => ({ selected: node.events.selected }));
+  } = useNode((node) => ({ selected: node.events.selected, node_id: node.id }));
+  const { previews, setPreviews } = useContext(previewCtx);
+  const myPreview = previews[node_id];
   return (
     <span
       className={`${textStyle} ${selected ? "selected-node" : ""}`}
       {...blockProps(block)}
       ref={(dom) => connect(drag(dom))}
     >
-      [{fieldview} {name}]
+      {myPreview ? (
+        <div
+          className="d-inline"
+          dangerouslySetInnerHTML={{ __html: myPreview }}
+        ></div>
+      ) : (
+        `[${fieldview} ${name}]`
+      )}
     </span>
   );
 };
@@ -32,6 +43,7 @@ export const FieldSettings = () => {
     fieldview,
     block,
     configuration,
+    node_id,
     textStyle,
   } = useNode((node) => ({
     name: node.data.props.name,
@@ -39,13 +51,32 @@ export const FieldSettings = () => {
     block: node.data.props.block,
     textStyle: node.data.props.textStyle,
     configuration: node.data.props.configuration,
+    node_id: node.id,
   }));
   const options = useContext(optionsCtx);
+  const { previews, setPreviews } = useContext(previewCtx);
+
   const fvs = options.field_view_options[name];
   const handlesTextStyle = (options.handlesTextStyle || {})[name];
   const getCfgFields = (fv) =>
     ((options.fieldViewConfigForms || {})[name] || {})[fv];
   const cfgFields = getCfgFields(fieldview);
+  const refetchPreview = () => {
+    fetch(`/field/preview/${options.tableName}/${name}/${fieldview}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "CSRF-Token": options.csrfToken,
+      },
+    })
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (html) {
+        setPreviews((prevState) => ({ ...prevState, [node_id]: html }));
+      });
+  };
+
   return (
     <Fragment>
       <table className="w-100">
@@ -63,6 +94,7 @@ export const FieldSettings = () => {
                   const newfvs = options.field_view_options[e.target.value];
                   if (newfvs && newfvs.length > 0)
                     setProp((prop) => (prop.fieldview = newfvs[0]));
+                  refetchPreview();
                 }}
               >
                 {options.fields.map((f, ix) => (
@@ -90,6 +122,7 @@ export const FieldSettings = () => {
                       e.target.value,
                       getCfgFields(e.target.value)
                     );
+                    refetchPreview();
                   }}
                 >
                   {(fvs || []).map((fvnm, ix) => (
