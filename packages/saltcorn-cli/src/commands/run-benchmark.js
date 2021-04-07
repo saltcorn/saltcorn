e1ca5c4676db44b15823690b2505d05ce7f7f2e0
Tom Nielsen <tanielsen@gmail.com>
2021-04-07 11:20:52 +0100

    run benchmark

---
+++ packages/saltcorn-cli/src/commands/run-benchmark.js
@@ -0,0 +1,42 @@
+const { Command, flags } = require("@oclif/command");
+const wrk = require("wrk");
+
+class RunBenchmarkCommand extends Command {
+  async run() {
+    const that = this;
+    const {
+      args: { baseurl },
+      flags: { token },
+    } = this.parse(RunBenchmarkCommand);
+    wrk(
+      {
+        threads: 1,
+        connections: 50,
+        duration: "10s",
+        url: baseurl || "http://localhost:3000/",
+      },
+      function (err, out) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+        console.log(out);
+        process.exit(0);
+      }
+    );
+  }
+}
+
+RunBenchmarkCommand.args = [
+  { name: "baseurl", required: false, description: "Base URL" },
+];
+
+RunBenchmarkCommand.description = `Run benchmark`;
+
+RunBenchmarkCommand.flags = {
+  token: flags.string({
+    char: "t",
+    description: "API Token for reporting results",
+  }),
+};
+module.exports = RunBenchmarkCommand;
