8d428f6fa44310d17f0af0bfd27ab82791c0e96a
Tom Nielsen <tanielsen@gmail.com>
2021-04-27 13:13:57 +0100

    path to saltcorn bin

--- infosec-scan/py-sectest/scsession.py
+++ infosec-scan/py-sectest/scsession.py
@@ -5,7 +5,7 @@ import time
 
 class SaltcornSession(Session):
   def __init__(self, port=3000):
-    self.salcorn_process = subprocess.Popen(["saltcorn", "serve", "-p", str(port)])
+    self.salcorn_process = subprocess.Popen(["packages/saltcorn-cli/bin/saltcorn", "serve", "-p", str(port)])
     Session.__init__(self, 'http://localhost:%d/' % port)
     self.wait_for_port_open()
 
