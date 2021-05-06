242ae23c3725164d22d26c80e662eae156b4caaf
Tom Nielsen <tanielsen@gmail.com>
2021-05-06 17:26:38 +0100

    check multitenancy in create tenant

--- packages/saltcorn-cli/src/commands/create-tenant.js
+++ packages/saltcorn-cli/src/commands/create-tenant.js
@@ -3,6 +3,12 @@ const { Command, flags } = require("@oclif/command");
 class CreateTenantCommand extends Command {
   async run() {
     const { args } = this.parse(CreateTenantCommand);
+    const db = require("@saltcorn/data/db");
+    if (!db.is_it_multi_tenant()) {
+      console.error("Multitenancy not enabled");
+      this.exit(0);
+      return;
+    }
     const { createTenant } = require("@saltcorn/data/models/tenant");
     await createTenant(args.tenant);
     this.exit(0);
