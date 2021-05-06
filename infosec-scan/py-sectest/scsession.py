511cf4eee5afac7976accd4adb2c0e555f23cddc
Tom Nielsen <tanielsen@gmail.com>
2021-05-06 14:52:14 +0100

    asset path

--- infosec-scan/py-sectest/scsession.py
+++ infosec-scan/py-sectest/scsession.py
@@ -1,9 +1,10 @@
 from sectest import Session
 import re
 import subprocess
+import os 
 
 class SaltcornSession(Session):
-  def __init__(self, port=3000):
+  def __init__(self, port=3001):
     self.salcorn_process = subprocess.Popen(["packages/saltcorn-cli/bin/saltcorn", "serve", "-p", str(port)])
     Session.__init__(self, 'http://localhost:%d/' % port)
     self.wait_for_port_open()
@@ -17,4 +18,12 @@ class SaltcornSession(Session):
 
   @staticmethod
   def reset_to_fixtures():
-    subprocess.run(["packages/saltcorn-cli/bin/saltcorn", "fixtures", "-r"], check=True)
+    SaltcornSession.cli("fixtures", "-r")
+
+  @staticmethod
+  def cli(*args):
+    subprocess.run(["packages/saltcorn-cli/bin/saltcorn"]+list(args), check=True)
+
+  @staticmethod
+  def asset_path(name):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets", name)
