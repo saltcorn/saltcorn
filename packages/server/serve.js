9b94aa660ee17fca9c2bea09912db1b30b1a6b3c
Tom Nielsen <tanielsen@gmail.com>
2021-05-20 15:46:31 +0100

    dont crash

--- packages/server/serve.js
+++ packages/server/serve.js
@@ -53,9 +53,9 @@ module.exports = async ({ port = 3000, disableScheduler, ...appargs } = {}) => {
         })
         .ready((s) => {
           console.log("greenlock all", s);
-          s && s.withServers && s.withServers(({_httpsServer})=>{
+          s.withServers(({_httpsServer})=>{
             console.log("serv", _httpsServer);
-            _httpsServer.setTimeout(240*1000)
+            _httpsServer && _httpsServer.setTimeout(240*1000)
           })
         })
         .serve(app);
