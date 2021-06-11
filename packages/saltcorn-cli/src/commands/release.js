e3e4f62a549df42c4844ac6838c65ae5b0d17206
Tom Nielsen <tanielsen@gmail.com>
2021-06-11 21:02:34 +0100

    write versions

--- packages/saltcorn-cli/src/commands/release.js
+++ packages/saltcorn-cli/src/commands/release.js
@@ -1,23 +1,46 @@
 const { Command, flags } = require("@oclif/command");
-const { cli } = require("cli-ux");
+const fs = require("fs");
+
 
 class ReleaseCommand extends Command {
   async run() {
     const {
       args: { version },
     } = this.parse(ReleaseCommand);
-    const pkgDirs = [
-      "e2e",
-      "saltcorn-builder",
-      "saltcorn-data",
-      "saltcorn-random-tests",
-      "server",
-      "saltcorn-base-plugin",
-      "saltcorn-cli",
-      "saltcorn-markup",
-      "saltcorn-sbadmin2",
-    ];
+    const pkgs = {
+      "@saltcorn/e2e": { dir: "e2e" },
+      "@saltcorn/builder": { dir: "saltcorn-builder", publish: true },
+      "@saltcorn/data": { dir: "saltcorn-data", publish: true },
+      "@saltcorn/random-tests": { dir: "saltcorn-random-tests" },
+      "@saltcorn/server": { dir: "server", publish: true },
+      "@saltcorn/base-plugin": { dir: "saltcorn-base-plugin", publish: true },
+      //"saltcorn-cli", publish: true},
+      "@saltcorn/markup": { dir: "saltcorn-markup", publish: true },
+      "@saltcorn/sbadmin": { dir: "saltcorn-sbadmin2", publish: true },
+    };
+    for (const [nm, p] of Object.entries(pkgs)) {
+      const json = require(`../../../${p.dir}/package.json`);
+      json.version = version;
+      if (json.dependencies)
+        Object.keys(pkgs).forEach((dpkgnm) => {
+          if (json.dependencies[dpkgnm])
+            json.dependencies[dpkgnm] = version;
+        });
+      fs.writeFileSync(`packages/${p.dir}/package.json`, JSON.stringify(json, null, 2));
+    }
+    //for each package:
+    // 1. update version
+    // 2. update dependencies for other packages
+    // 3. publish
 
+    // for cli:
+    // 1. update version
+    // 2. update dependencies for other pkgs
+    // 3. run npm update
+    // 3. publish
+
+    // commit
+    // tag
     this.exit(0);
   }
 }
