53ae8a675a8f1716d4cdb3fcc108f291663529f6
Tom Nielsen <tanielsen@gmail.com>
2021-06-11 21:16:11 +0100

    publish

--- packages/saltcorn-cli/src/commands/release.js
+++ packages/saltcorn-cli/src/commands/release.js
@@ -1,12 +1,13 @@
 const { Command, flags } = require("@oclif/command");
 const fs = require("fs");
-
+const { spawnSync } = require("child_process");
 
 class ReleaseCommand extends Command {
   async run() {
     const {
       args: { version },
     } = this.parse(ReleaseCommand);
+
     const pkgs = {
       "@saltcorn/e2e": { dir: "e2e" },
       "@saltcorn/builder": { dir: "saltcorn-builder", publish: true },
@@ -18,27 +19,44 @@ class ReleaseCommand extends Command {
       "@saltcorn/markup": { dir: "saltcorn-markup", publish: true },
       "@saltcorn/sbadmin": { dir: "saltcorn-sbadmin2", publish: true },
     };
-    for (const [nm, p] of Object.entries(pkgs)) {
-      const json = require(`../../../${p.dir}/package.json`);
+
+    const updatePkgJson = (dir) => {
+      const json = require(`../../../${dir}/package.json`);
       json.version = version;
       if (json.dependencies)
         Object.keys(pkgs).forEach((dpkgnm) => {
-          if (json.dependencies[dpkgnm])
-            json.dependencies[dpkgnm] = version;
+          if (json.dependencies[dpkgnm]) json.dependencies[dpkgnm] = version;
         });
-      fs.writeFileSync(`packages/${p.dir}/package.json`, JSON.stringify(json, null, 2));
-    }
+      fs.writeFileSync(
+        `packages/${dir}/package.json`,
+        JSON.stringify(json, null, 2)
+      );
+    };
+    const publish = (dir) => {
+        console.log("publish", dir);
+      spawnSync("npm", ["publish", "--dry-run"], {
+        stdio: "inherit",
+        cwd: `packages/${dir}/package.json`,
+      });
+    };
     //for each package:
     // 1. update version
     // 2. update dependencies for other packages
     // 3. publish
+
+    for (const p of Object.values(pkgs)) {
+      updatePkgJson(p.dir);
+      if (p.publish) publish(p.dir);
+    }
 
     // for cli:
     // 1. update version
     // 2. update dependencies for other pkgs
     // 3. run npm update
     // 3. publish
+    updatePkgJson("saltcorn-cli");
 
+    publish("saltcorn-cli");
     // commit
     // tag
     this.exit(0);
