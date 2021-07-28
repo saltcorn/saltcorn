de0facec122fd6827f7f88c557900083a5cf96ca
Tom Nielsen <tanielsen@gmail.com>
2021-07-28 22:50:30 +0100

    channel in trigger table

--- packages/saltcorn-data/models/trigger.js
+++ packages/saltcorn-data/models/trigger.js
@@ -76,7 +76,7 @@ class Trigger {
   static async findAllWithTableName() {
     const schema = db.getTenantSchemaPrefix();
 
-    const sql = `select a.id, a.name, a.action, t.name as table_name, a. when_trigger 
+    const sql = `select a.id, a.name, a.action, t.name as table_name, a. when_trigger, a.channel 
     from ${schema}_sc_triggers a left join ${schema}_sc_tables t on t.id=table_id order by a.id`;
     const { rows } = await db.query(sql);
     return rows.map((dbf) => new Trigger(dbf));
