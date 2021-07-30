2153415429758179c68bb27d3da4f3aa6b0936b2
Tom Nielsen <tanielsen@gmail.com>
2021-07-30 22:34:14 +0100

    event log model

---
+++ packages/saltcorn-data/migrations/202107302158.js
@@ -0,0 +1,11 @@
+const sql = `create table _sc_event_log (
+    id serial primary key,
+    event_type text NOT NULL,
+    channel text,
+    occur_at timestamp not null,
+    user_id int,
+    payload jsonb,
+);`;
+
+module.exports = { sql };
+    
---
+++ packages/saltcorn-data/models/eventlog.js
@@ -0,0 +1,56 @@
+const db = require("../db");
+const { contract, is } = require("contractis");
+
+class EventLog {
+  constructor(o) {
+    this.id = o.id;
+    this.event_type = o.event_type;
+    this.channel = o.channel;
+    this.occur_at = ["string", "number"].includes(typeof o.occur_at)
+      ? new Date(o.occur_at)
+      : o.occur_at;
+    this.user_id = o.user_id;
+    this.payload =
+      typeof o.payload === "string" ? JSON.parse(o.payload) : o.payload;
+    contract.class(this);
+  }
+  static async find(where, selopts) {
+    const us = await db.select("_sc_event_log", where, selopts);
+    return us.map((u) => new EventLog(u));
+  }
+  static async findOne(where) {
+    const u = await db.selectOne("_sc_event_log", where);
+    return new EventLog(u);
+  }
+
+  static async count(where) {
+    return await db.count("_sc_event_log", where || {});
+  }
+
+  static async create(o) {
+    const ev = new EventLog(o);
+    const { id, ...rest } = ev;
+
+    ev.id = await db.insert("_sc_event_log", rest);
+    return ev;
+  }
+}
+
+EventLog.contract = {
+  variables: {
+    id: is.maybe(is.posint),
+    event_type: is.str,
+    channel: is.str,
+    occur_at: is.class("Date"),
+    user_id: is.maybe(is.posint),
+    payload: is.obj(),
+  },
+  methods: {},
+  static_methods: {
+    find: is.fun(is.obj(), is.promise(is.array(is.class("Crash")))),
+    findOne: is.fun(is.obj(), is.promise(is.class("Crash"))),
+    create: is.fun([is.obj(), is.obj()], is.promise(is.any)),
+  },
+};
+
+module.exports = EventLog;
