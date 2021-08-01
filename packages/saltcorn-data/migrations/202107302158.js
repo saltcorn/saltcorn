9cc02f98e85f0b83488726a6b7fcb0fb92a430d9
Tom Nielsen <tanielsen@gmail.com>
2021-08-01 15:50:51 +0100

    sqlite

--- packages/saltcorn-data/migrations/202107302158.js
+++ packages/saltcorn-data/migrations/202107302158.js
@@ -1,4 +1,4 @@
-const sql = `CREATE UNLOGGED TABLE _sc_event_log (
+const sql_pg = `CREATE UNLOGGED TABLE _sc_event_log (
     id serial primary key,
     event_type text NOT NULL,
     channel text,
@@ -7,5 +7,14 @@ const sql = `CREATE UNLOGGED TABLE _sc_event_log (
     payload jsonb
 );`;
 
-module.exports = { sql };
+const sql_sqlite = `CREATE TABLE _sc_event_log (
+    id integer primary key,
+    event_type text NOT NULL,
+    channel text,
+    occur_at timestamp not null,
+    user_id int,
+    payload json
+);`;
+
+module.exports = { sql_pg, sql_sqlite };
     
