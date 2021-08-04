8bd3d9396b6a7c7a5413cdff4072aee9b7b03882
Tom Nielsen <tanielsen@gmail.com>
2021-08-04 23:10:36 +0100

    direct to verifier if active

--- packages/server/auth/routes.js
+++ packages/server/auth/routes.js
@@ -450,7 +450,8 @@ const signup_login_with_user = (u, req, res) =>
     function (err) {
       if (!err) {
         Trigger.emitEvent("Login", null, u);
-        res.redirect("/");
+        if (getState().verifier) res.redirect("/auth/verification-flow");
+        else res.redirect("/");
       } else {
         req.flash("danger", err);
         res.redirect("/auth/signup");
@@ -1018,6 +1019,7 @@ router.post(
       },
       function (err) {
         if (!err) {
+          Trigger.emitEvent("Login", null, u);
           req.flash("success", req.__("Welcome, %s!", u.email));
           res.redirect("/");
         } else {
