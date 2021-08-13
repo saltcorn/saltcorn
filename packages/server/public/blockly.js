b94e3b47a23c34ed664b3621d4b9354b3c4bcd62
Tom Nielsen <tanielsen@gmail.com>
2021-08-13 22:38:21 +0100

    library url

--- packages/server/public/blockly.js
+++ packages/server/public/blockly.js
@@ -1,4 +1,7 @@
 function activate_blockly({ events, actions, tables }) {
+
+  // https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#qykfhx
+
   Blockly.Blocks["console"] = {
     init: function () {
       this.appendValueInput("STRING")
