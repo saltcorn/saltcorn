711847c0493ddd3fd15bf3930f866dd435e5b882
Tom Nielsen <tanielsen@gmail.com>
2021-09-06 09:00:52 +0100

    drop constraints with dropped field

--- packages/saltcorn-data/models/field.js
+++ packages/saltcorn-data/models/field.js
@@ -413,15 +413,17 @@ class Field {
   async delete() {
     const Table = require("./table");
     const table = await Table.findOne({ id: this.table_id });
-
+    const TableConstraint = require("./table_constraints");
+    await TableConstraint.delete_field_constraints(table, this);
     if (table.ownership_field_id === this.id) {
       throw new InvalidAdminAction(
         `Cannot delete field ${this.name} as it sets ownership for table ${table.name}`
       );
     }
-    await db.deleteWhere("_sc_fields", { id: this.id });
 
     const schema = db.getTenantSchemaPrefix();
+
+    await db.deleteWhere("_sc_fields", { id: this.id });
 
     if (!db.isSQLite && (!this.calculated || this.stored)) {
       await db.query(
