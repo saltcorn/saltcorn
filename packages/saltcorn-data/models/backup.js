72c9f8c02335163b9b3503c4caf5a3d1d1a7b0bb
Tom Nielsen <tanielsen@gmail.com>
2021-09-22 17:05:50 +0100

    only restore file user_id if not null

--- packages/saltcorn-data/models/backup.js
+++ packages/saltcorn-data/models/backup.js
@@ -193,7 +193,7 @@ const restore_file_users = contract(
   is.fun(is.obj({}), is.promise(is.undefined)),
   async (file_users) => {
     for (const [id, user_id] of Object.entries(file_users)) {
-      await db.update("_sc_files", { user_id }, id);
+      if (user_id) await db.update("_sc_files", { user_id }, id);
     }
   }
 );
