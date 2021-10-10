bbbfae6c7bc2059554bb75b9ee14fc12729f687e
Tom Nielsen <tanielsen@gmail.com>
2021-10-10 09:16:09 +0100

    min height

--- packages/e2e/table.test.js
+++ packages/e2e/table.test.js
@@ -100,7 +100,7 @@ describe("Table create", () => {
     await browser.page.select("#inputtable_name", "Persons");
     await browser.clickNav("button[type=submit]");
     expect(await browser.content()).toContain("Action button");
-    await browser.page.click("div.is-text .d-inline");
+    await browser.page.click("div.is-text");
     await browser.page.waitForSelector("div.cke_editable");
     await browser.erase_input("div.cke_editable", 15);
     await browser.page.waitForSelector("div.cke_editable");
@@ -155,7 +155,7 @@ describe("Table create", () => {
     await browser.page.select("#inputtable_name", "Persons");
     await browser.clickNav("button[type=submit]");
     expect(await browser.content()).toContain("Join field");
-    await browser.page.click("div.is-text .d-inline");
+    await browser.page.click("div.is-text");
     await browser.page.waitForSelector("div.cke_editable");
     await browser.page.waitFor(100);
     await browser.erase_input("div.cke_editable", 15);
