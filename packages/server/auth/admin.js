bfe4b23da4e6788726f14c9acaa9da9a71c2cae7
Tom Nielsen <tanielsen@gmail.com>
2021-11-02 09:11:55 +0000

    set cookie expration

--- packages/server/auth/admin.js
+++ packages/server/auth/admin.js
@@ -288,6 +288,8 @@ const user_settings_form = (req) =>
       "timeout",
       "email_mask",
       "allow_forgot",
+      "cookie_duration",
+      "cookie_duration_remember",
       "cookie_sessions",
       "custom_http_headers",
     ],
