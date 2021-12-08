89df2b9b9d24b2f8ab5c8d6018369fcbcdd2177a
Tom Nielsen <tanielsen@gmail.com>
2021-12-08 08:58:11 +0000

    ckeditor for offline builder

--- packages/saltcorn-markup/builder.js
+++ packages/saltcorn-markup/builder.js
@@ -22,8 +22,8 @@ const {
 const { contract, is } = require("contractis");
 
 /**
- * @param {object} rec 
- * @param {object} csrf 
+ * @param {object} rec
+ * @param {object} csrf
  * @returns {object}
  */
 const addCsrf = (rec, csrf) => {
@@ -32,64 +32,69 @@ const addCsrf = (rec, csrf) => {
 };
 
 /**
- * @param {object} x 
+ * @param {object} x
  * @returns {string}
  */
 const encode = (x) => encodeURIComponent(JSON.stringify(x));
 
-module.exports = 
-/**
- * @param {object} opts
- * @param {object} opts.options
- * @param {object} opts.context
- * @param {object} opts.action
- * @param {string} opts.stepName
- * @param {object} opts.layout
- * @param {string} [opts.mode = "show"]
- * @param {object} opts
- * @param {object} csrfToken 
- * @returns {div}
- */
-(
-  { options, context, action, stepName, layout, mode = "show", version_tag },
-  csrfToken
-) =>
-  div(
-    script({
-      src: version_tag
-        ? `/static_assets/${version_tag}/builder_bundle.js`
-        : "/builder_bundle.js",
-    }),
-    link({
-      rel: "stylesheet",
-      type: "text/css",
-      media: "screen",
-      href: version_tag
-        ? `/static_assets/${version_tag}/fonticonpicker.react.css`
-        : "/fonticonpicker.react.css",
-    }),
-    link({
-      rel: "stylesheet",
-      type: "text/css",
-      media: "screen",
-      href: version_tag
-        ? `/static_assets/${version_tag}/saltcorn-builder.css`
-        : "/saltcorn-builder.css",
-    }),
-    div({ id: "saltcorn-builder" }),
-    form(
-      { action, method: "post", id: "scbuildform" },
-      input({
-        type: "hidden",
-        name: "contextEnc",
-        value: encodeURIComponent(JSON.stringify(context)),
+module.exports =
+  /**
+   * @param {object} opts
+   * @param {object} opts.options
+   * @param {object} opts.context
+   * @param {object} opts.action
+   * @param {string} opts.stepName
+   * @param {object} opts.layout
+   * @param {string} [opts.mode = "show"]
+   * @param {object} opts
+   * @param {object} csrfToken
+   * @returns {div}
+   */
+  (
+    { options, context, action, stepName, layout, mode = "show", version_tag },
+    csrfToken
+  ) =>
+    div(
+      script({
+        src: version_tag
+          ? `/static_assets/${version_tag}/builder_bundle.js`
+          : "/builder_bundle.js",
       }),
-      input({ type: "hidden", name: "stepName", value: stepName }),
-      input({ type: "hidden", name: "columns", value: "" }),
-      input({ type: "hidden", name: "layout", value: "" }),
-      input({ type: "hidden", name: "_csrf", value: csrfToken })
-    ),
-    script(`builder.renderBuilder(
+      script({
+        src: version_tag
+          ? `/static_assets/${version_tag}/ckeditor/ckeditor.js`
+          : "/ckeditor/ckeditor.js",
+      }),
+      link({
+        rel: "stylesheet",
+        type: "text/css",
+        media: "screen",
+        href: version_tag
+          ? `/static_assets/${version_tag}/fonticonpicker.react.css`
+          : "/fonticonpicker.react.css",
+      }),
+      link({
+        rel: "stylesheet",
+        type: "text/css",
+        media: "screen",
+        href: version_tag
+          ? `/static_assets/${version_tag}/saltcorn-builder.css`
+          : "/saltcorn-builder.css",
+      }),
+      div({ id: "saltcorn-builder" }),
+      form(
+        { action, method: "post", id: "scbuildform" },
+        input({
+          type: "hidden",
+          name: "contextEnc",
+          value: encodeURIComponent(JSON.stringify(context)),
+        }),
+        input({ type: "hidden", name: "stepName", value: stepName }),
+        input({ type: "hidden", name: "columns", value: "" }),
+        input({ type: "hidden", name: "layout", value: "" }),
+        input({ type: "hidden", name: "_csrf", value: csrfToken })
+      ),
+      script(`builder.renderBuilder(
       "saltcorn-builder", 
       "${encode(addCsrf(options, csrfToken))}", 
       "${encode(layout || {})}",
@@ -98,4 +103,4 @@ module.exports =
     document.addEventListener('DOMContentLoaded',
       function(){window.onerror=globalErrorCatcher},false);
     ;`)
-  );
+    );
