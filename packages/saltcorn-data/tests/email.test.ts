84d36578aa4ca6379b10e06808c674a71ec79e6f
Tom Nielsen <tanielsen@gmail.com>
2022-02-16 15:29:52 +0000

    complex view test

--- packages/saltcorn-data/tests/email.test.ts
+++ packages/saltcorn-data/tests/email.test.ts
@@ -3,11 +3,11 @@
   from 'bootstrap-email'.
  */
 
-import { dirname, join, normalize } from "path";
 import { writeFileSync } from "fs";
 import email from "../models/email";
 import { describe, it, expect, jest } from "@jest/globals";
 import View from "../models/view";
+import Table from "../models/table";
 
 function removeBreaks(str: string): string {
   return str.replace(/(\r\n|\r|\n)/gm, "").toLowerCase();
@@ -26,13 +26,13 @@ beforeAll(async () => {
 
 jest.setTimeout(60 * 1000);
 
-describe("Bootstrap Mail Transformations", () => {
-  it("transform to mjml", async () => {
+describe("MJML Mail Transformations", () => {
+  it("transform simple to mjml", async () => {
     const v = await View.findOne({ name: "authorshow" });
     const ml = await email.viewToMjml(v, { id: 1 });
     expect(ml).toBe(`<mjml><mj-body>Herman Melville</mj-body></mjml>`);
   });
-  it("transform to html", async () => {
+  it("transform simple to html", async () => {
     const v = await View.findOne({ name: "authorshow" });
     const html = await email.viewToEmailHtml(v, { id: 1 });
     //writeFileSync("emailout", html);
@@ -55,21 +55,91 @@ describe("Bootstrap Mail Transformations", () => {
   <![endif]--><style type="text/css"></style></head><body><div></div></body></html>`)
     );
   });
-
-  /* const examplesDir = normalize(join(dirname(BootstrapEmail), "../examples"));
-  const inputsDir = join(examplesDir, "input");
-  const outputsDir = join(examplesDir, "output");
-  const inputFiles = readdirSync(inputsDir).map((file: string) => {
-    return readFileSync(join(inputsDir, file)).toString();
+  it("transform complex to html", async () => {
+    const v = await mkView({
+      name: "emailview1",
+      layout: {
+        widths: [6, 6],
+        besides: [
+          {
+            above: [
+              null,
+              {
+                type: "card",
+                contents: {
+                  type: "view_link",
+                  view: "Own:authorshow",
+                  minRole: 10,
+                  in_modal: true,
+                  view_label: "foo it",
+                },
+                isFormula: {},
+              },
+            ],
+          },
+          {
+            type: "container",
+            bgType: "Color",
+            hAlign: "left",
+            vAlign: "top",
+            bgColor: "#a9a7a7",
+            bgFileId: 1,
+            contents: {
+              url: "'https://countto.com/'+pages",
+              text: "author",
+              type: "link",
+              isFormula: { url: true, text: true },
+            },
+            imageSize: "contain",
+            minHeight: "100",
+            textColor: "#ffffff",
+            borderStyle: "solid",
+            borderWidth: "1",
+          },
+        ],
+      },
+      columns: [
+        {
+          type: "ViewLink",
+          view: "Own:authorshow",
+          minRole: 10,
+          in_modal: true,
+        },
+      ],
+    });
+    const html = await email.viewToEmailHtml(v, { id: 1 });
+    //writeFileSync("emailout", html);
+    expect(trimLines(html)).toBe(
+      trimLines(`<!doctype html><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head><title></title><!--[if !mso]><!-- --><meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]--><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1"><style type="text/css">#outlook a { padding:0; }
+      body { margin:0;padding:0;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%; }
+      table, td { border-collapse:collapse;mso-table-lspace:0pt;mso-table-rspace:0pt; }
+      img { border:0;height:auto;line-height:100%; outline:none;text-decoration:none;-ms-interpolation-mode:bicubic; }
+      p { display:block;margin:13px 0; }</style><!--[if mso]>
+    <xml>
+    <o:OfficeDocumentSettings>
+      <o:AllowPNG/>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+    </xml>
+    <![endif]--><!--[if lte mso 11]>
+    <style type="text/css">
+      .mj-outlook-group-fix { width:100% !important; }
+    </style>
+    <![endif]--><style type="text/css">@media only screen and (min-width:480px) {
+    .mj-column-px-50 { width:50px !important; max-width: 50px; }
+  }</style><style type="text/css"></style></head><body><div><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:600px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:50px;" ><![endif]--><div class="mj-column-px-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"></table></div><!--[if mso | IE]></td><td class="" style="vertical-align:top;width:50px;" ><![endif]--><div class="mj-column-px-50 mj-outlook-group-fix" style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;"><table border="0" cellpadding="0" cellspacing="0" role="presentation" style="vertical-align:top;" width="100%"><tr><td style="font-size:0px;padding:20px 0;word-break:break-word;"><!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:50px;" width="50" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]--><div style="margin:0px auto;max-width:50px;"><table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;"><tbody><tr><td style="direction:ltr;font-size:0px;padding:20px 0;text-align:center;"><!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></table></div><!--[if mso | IE]></td></tr></table><![endif]--></td></tr></tbody></table></div><!--[if mso | IE]></td></tr></table><![endif]--></div></body></html>`)
+    );
   });
-  const expectedFiles = readdirSync(outputsDir).map((file: string) => {
-    return readFileSync(join(outputsDir, file)).toString();
-  });
-  it("transform input files and compare with expected files", async () => {
-    expect(inputFiles.length).toBe(expectedFiles.length);
-    for (let i = 0; i < inputFiles.length; i++) {
-      //const mail = await transformBootstrapEmail(inputFiles[i], false);
-      //expect(removeBreaks(mail)).toBe(removeBreaks(expectedFiles[i]));
-    }
-  });*/
 });
+
+const mkView = async ({ name, ...rest }: any) => {
+  const tbl = await Table.findOne({ name: "books" });
+  if (!tbl) throw new Error("no such table");
+  return await View.create({
+    table_id: tbl.id,
+    name,
+    viewtemplate: "Show",
+    configuration: rest,
+    min_role: 10,
+  });
+};
