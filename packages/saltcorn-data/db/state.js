ffa4086af24ee9325fe14032df5033a0323a3781
Tom Nielsen <tanielsen@gmail.com>
2022-03-01 17:28:37 +0000

    fonts in state

--- packages/saltcorn-data/db/state.js
+++ packages/saltcorn-data/db/state.js
@@ -41,10 +41,57 @@ const process_send = (v) => {
   if (process.send) process.send(v);
 };
 
+const standard_fonts = {
+  Arial: "Arial, Helvetica Neue, Helvetica, sans-serif",
+  Baskerville:
+    "Baskerville, Baskerville Old Face, Garamond, Times New Roman, serif",
+  "Bodoni MT":
+    "Bodoni MT, Bodoni 72, Didot, Didot LT STD, Hoefler Text, Garamond, Times New Roman, serif",
+  Calibri: "Calibri, Candara, Segoe, Segoe UI, Optima, Arial, sans-serif",
+  "Calisto MT":
+    "Calisto MT, Bookman Old Style, Bookman, Goudy Old Style, Garamond, Hoefler Text, Bitstream Charter, Georgia, serif",
+  Cambria: "Cambria, Georgia, serif",
+  Candara: "Candara, Calibri, Segoe, Segoe UI, Optima, Arial, sans-serif",
+  "Century Gothic": "Century Gothic, CenturyGothic, AppleGothic, sans-serif",
+  Consolas: "Consolas, monaco, monospace",
+  "Copperplate Gothic": "Copperplate, Copperplate Gothic Light, fantasy",
+  "Courier New":
+    "Courier New, Courier, Lucida Sans Typewriter, Lucida Typewriter, monospace",
+  "Dejavu Sans": "Dejavu Sans, Arial, Verdana, sans-serif",
+  Didot:
+    "Didot, Didot LT STD, Hoefler Text, Garamond, Calisto MT, Times New Roman, serif",
+  "Franklin Gothic": "Franklin Gothic, Arial Bold",
+  Garamond:
+    "Garamond, Baskerville, Baskerville Old Face, Hoefler Text, Times New Roman, serif",
+  Georgia: "Georgia, Times, Times New Roman, serif",
+  "Gill Sans": "Gill Sans, Gill Sans MT, Calibri, sans-serif",
+  "Goudy Old Style":
+    "Goudy Old Style, Garamond, Big Caslon, Times New Roman, serif",
+  Helvetica: "Helvetica Neue, Helvetica, Arial, sans-serif",
+  Impact:
+    "Impact, Charcoal, Helvetica Inserat, Bitstream Vera Sans Bold, Arial Black, sans serif",
+  "Lucida Bright": "Lucida Bright, Georgia, serif",
+  "Lucida Sans": "Lucida Sans, Helvetica, Arial, sans-serif",
+  Optima: "Optima, Segoe, Segoe UI, Candara, Calibri, Arial, sans-serif",
+  Palatino:
+    "Palatino, Palatino Linotype, Palatino LT STD, Book Antiqua, Georgia, serif",
+  Perpetua:
+    "Perpetua, Baskerville, Big Caslon, Palatino Linotype, Palatino, serif",
+  Rockwell:
+    "Rockwell, Courier Bold, Courier, Georgia, Times, Times New Roman, serif",
+  "Segoe UI":
+    "Segoe UI, Frutiger, Dejavu Sans, Helvetica Neue, Arial, sans-serif",
+  Tahoma: "Tahoma, Verdana, Segoe, sans-serif",
+  "Trebuchet MS":
+    "Trebuchet MS, Lucida Grande, Lucida Sans Unicode, Lucida Sans, sans-serif",
+  Verdana: "Verdana, Geneva, sans-serif",
+};
+
 /**
  * State Class
  * @category saltcorn-data
  */
+
 class State {
   /**
    * State constructor
@@ -71,6 +118,7 @@ class State {
     this.plugin_locations = {};
     this.plugin_module_names = {};
     this.eventTypes = {};
+    this.fonts = standard_fonts;
     this.layouts = { emergency: { wrap: emergency_layout } };
     this.headers = {};
     this.function_context = { moment, slugify: db.slugify };
@@ -470,6 +518,8 @@ class State {
     this.external_tables = {};
     this.eventTypes = {};
     this.verifier = null;
+    this.fonts = standard_fonts;
+
     Object.entries(this.plugins).forEach(([k, v]) => {
       this.registerPlugin(k, v, this.plugin_cfgs[k]);
     });
