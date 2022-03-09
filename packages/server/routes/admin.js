d96913b021c42da26096eb598f5a6a373ab1ad42
Tom Nielsen <tanielsen@gmail.com>
2022-03-09 15:02:43 +0000

    hide multitenant cfg option if no cfg file

--- packages/server/routes/admin.js
+++ packages/server/routes/admin.js
@@ -71,6 +71,7 @@ const {
 } = require("../markup/admin");
 const moment = require("moment");
 const View = require("@saltcorn/data/models/view");
+const { getConfigFile } = require("@saltcorn/data/db/connect");
 
 /**
  * @type {object}
@@ -99,7 +100,7 @@ const site_id_form = (req) =>
       "page_custom_html",
       "development_mode",
       "log_sql",
-      "multitenancy_enabled",
+      ...(getConfigFile() ? ["multitenancy_enabled"] : []),
     ],
     action: "/admin",
     submitLabel: req.__("Save"),
