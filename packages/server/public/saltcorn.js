20020aaa4ef747e1c5b18e0b3f6736b9e2541e70
Tom Nielsen <tanielsen@gmail.com>
2022-05-10 11:13:19 +0200

    range filter

--- packages/server/public/saltcorn.js
+++ packages/server/public/saltcorn.js
@@ -313,6 +313,34 @@ function initialize_page() {
   });
   $('a[data-bs-toggle="tab"].deeplink').historyTabs();
   init_bs5_dropdowns();
+
+  // Initialize Sliders - https://stackoverflow.com/a/31083391
+  var sliderSections = document.getElementsByClassName("range-slider");
+  for (var x = 0; x < sliderSections.length; x++) {
+    var sliders = sliderSections[x].getElementsByTagName("input");
+    for (var y = 0; y < sliders.length; y++) {
+      if (sliders[y].type === "range") {
+        sliders[y].oninput = function () {
+          // Get slider values
+          var parent = this.parentNode;
+          var slides = parent.getElementsByTagName("input");
+          var slide1 = parseFloat(slides[0].value);
+          var slide2 = parseFloat(slides[1].value);
+          // Neither slider will clip the other, so make sure we determine which is larger
+          if (slide1 > slide2) {
+            var tmp = slide2;
+            slide2 = slide1;
+            slide1 = tmp;
+          }
+
+          var displayElement = parent.getElementsByClassName("rangeValues")[0];
+          displayElement.innerHTML = slide1 + " - " + slide2;
+        };
+        // Manually trigger event first time to display values
+        sliders[y].oninput();
+      }
+    }
+  }
 }
 
 $(initialize_page);
