7031c2cca87c0f6475973addc23399a0e3fd9607
Tom Nielsen <tanielsen@gmail.com>
2022-05-18 13:53:26 +0200

    get-cfg

---
+++ packages/saltcorn-cli/src/commands/get-cfg.js
@@ -0,0 +1,61 @@
+/**
+ * @category saltcorn-cli
+ * @module commands/set-cfg
+ */
+const { Command, flags } = require("@oclif/command");
+const { cli } = require("cli-ux");
+const { maybe_as_tenant } = require("../common");
+
+/**
+ * SetCfgCommand Class
+ * @extends oclif.Command
+ * @category saltcorn-cli
+ */
+class SetCfgCommand extends Command {
+  /**
+   * @returns {Promise<void>}
+   */
+  async run() {
+    const { args, flags } = this.parse(SetCfgCommand);
+    await maybe_as_tenant(flags.tenant, async () => {
+      if (flags.plugin) {
+        const Plugin = require("@saltcorn/data/models/plugin");
+        const plugin = await Plugin.findOne({ name: flags.plugin });
+        console.log(JSON.stringify(plugin.configuration[args.key]));
+        await plugin.upsert();
+      } else {
+        const { getState } = require("@saltcorn/data/db/state");
+        console.log(JSON.stringify(getState().getConfig(args.key)));
+      }
+    });
+    this.exit(0);
+  }
+}
+
+/**
+ * @type {string}
+ */
+SetCfgCommand.description = `Get a configuration value`;
+
+/**
+ * @type {object[]}
+ */
+SetCfgCommand.args = [
+  { name: "key", required: true, description: "Configuration key" },
+];
+
+/**
+ * @type {object}
+ */
+SetCfgCommand.flags = {
+  tenant: flags.string({
+    char: "t",
+    description: "tenant",
+  }),
+  plugin: flags.string({
+    char: "p",
+    description: "plugin",
+  }),
+};
+
+module.exports = SetCfgCommand;
