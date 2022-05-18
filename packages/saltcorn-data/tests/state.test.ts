fca847018d84010bcb64ecaebf03da13f14478cf
Tom Nielsen <tanielsen@gmail.com>
2022-05-18 10:15:37 +0200

    state test

--- packages/saltcorn-data/tests/state.test.ts
+++ packages/saltcorn-data/tests/state.test.ts
@@ -1,11 +1,13 @@
 import db from "../db";
 const state = require("../db/state");
+const {getState } = state
+import { afterAll, beforeAll, describe, it, expect } from "@jest/globals";
 
 afterAll(db.close);
 
 describe("State constants", () => {
   it("should have fonts", async () => {
-    expect(state.standard_fonts.Arial).toBe(
+    expect(getState().fonts.Arial).toBe(
       "Arial, Helvetica Neue, Helvetica, sans-serif"
     );
   });
