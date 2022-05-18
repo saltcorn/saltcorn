db350d5544d8a02ab0dc8d2ae7c9ec041279ecfb
Tom Nielsen <tanielsen@gmail.com>
2022-05-18 10:22:28 +0200

    more state

--- packages/saltcorn-data/tests/state.test.ts
+++ packages/saltcorn-data/tests/state.test.ts
@@ -1,6 +1,6 @@
 import db from "../db";
 const state = require("../db/state");
-const {getState } = state
+const { getState } = state;
 import { afterAll, beforeAll, describe, it, expect } from "@jest/globals";
 
 afterAll(db.close);
@@ -10,5 +10,19 @@ describe("State constants", () => {
     expect(getState().fonts.Arial).toBe(
       "Arial, Helvetica Neue, Helvetica, sans-serif"
     );
+    Object.values(getState().fonts).forEach((v) => {
+      expect(typeof v).toBe("string");
+      expect(!!v).toBe(true);
+    });
+  });
+  it("should have fonts", async () => {
+    expect(state.features.fieldrepeats_in_field_attributes).toBe(true);
+    Object.values(state.features).forEach((v) => {
+      expect(typeof v).toBe("boolean");
+      expect(!!v).toBe(true);
+    });
+  });
+  it("should have process_init_time", async () => {
+    expect(state.get_process_init_time() instanceof Date).toBe(true);
   });
 });
