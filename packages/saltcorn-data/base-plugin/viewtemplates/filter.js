182a731c38613940312b4ceda0d0a5cee65d08c5
Tom Nielsen <tanielsen@gmail.com>
2022-05-26 23:08:47 +0200

    rm log

--- packages/saltcorn-data/base-plugin/viewtemplates/filter.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/filter.js
@@ -159,7 +159,6 @@ const run = async (
   await traverse(layout, {
     field: async (segment) => {
       const { field_name, fieldview, configuration } = segment;
-      console.log(segment);
       let field = fields.find((fld) => fld.name === field_name);
       if (!field) return;
       field.fieldview = fieldview;
