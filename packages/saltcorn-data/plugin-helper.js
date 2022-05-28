f1fd11f2c4439bace1f46c91163a72e0da8fba84
Tom Nielsen <tanielsen@gmail.com>
2022-05-28 21:44:46 +0100

    Misc fixes: show action, upload in popup (#1215)
    * avoid crash in stateFieldsToWhere
    
    * rm log
    
    * file upload from popup
    
    * show run action fix body

--- packages/saltcorn-data/plugin-helper.js
+++ packages/saltcorn-data/plugin-helper.js
@@ -1053,7 +1053,7 @@ const addOrCreateList = (container, key, x) => {
  */
 const stateFieldsToWhere = ({ fields, state, approximate = true }) => {
   var qstate = {};
-  Object.entries(state).forEach(([k, v]) => {
+  Object.entries(state || {}).forEach(([k, v]) => {
     if (k === "_fts") {
       qstate[k] = {
         searchTerm: v.replace(/\0/g, ""),
@@ -1094,7 +1094,7 @@ const stateFieldsToWhere = ({ fields, state, approximate = true }) => {
       // omit
     } else if (typeof v === "object" && v && field?.type?.name === "JSON") {
       let json = {};
-      if(Object.values(v).length ===1 && Object.values(v)[0]==="") return
+      if (Object.values(v).length === 1 && Object.values(v)[0] === "") return;
       Object.entries(v).forEach(([kj, vj]) => {
         if (vj === "") return;
         if (kj.endsWith("__lte")) {
