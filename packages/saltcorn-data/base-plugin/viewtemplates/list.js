cf5a593a49c3058030d0e9fa94327e470b8ab269
Tom Nielsen <tanielsen@gmail.com>
2022-06-28 12:05:31 +0100

    list joinfield

--- packages/saltcorn-data/base-plugin/viewtemplates/list.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/list.js
@@ -361,31 +361,15 @@ const get_state_fields = async (table_id, viewname, { columns }) => {
  * @param {object[]} opts.fields
  * @returns {Promise<void>}
  */
-const set_join_fieldviews = async ({ columns, fields }) => {
+const set_join_fieldviews = async ({ table, columns, fields }) => {
   for (const segment of columns) {
     const { join_field, join_fieldview } = segment;
     if (!join_fieldview) continue;
-    const keypath = join_field.split(".");
-    if (keypath.length === 2) {
-      const [refNm, targetNm] = keypath;
-      const ref = fields.find((f) => f.name === refNm);
-      if (!ref) continue;
-      const table = await Table.findOne({ name: ref.reftable_name });
-      if (!table) continue;
-      const reffields = await table.getFields();
-      const field = reffields.find((f) => f.name === targetNm);
-      if (field && field.type === "File") segment.field_type = "File";
-      else if (
-        field &&
-        field.type &&
-        field.type.name &&
-        field.type.fieldviews &&
-        field.type.fieldviews[join_fieldview]
-      )
-        segment.field_type = field.type.name;
-    } else {
-      //const [refNm, through, targetNm] = keypath;
-    }
+
+    const field = await table.getField(join_field);
+    if (field && field.type === "File") segment.field_type = "File";
+    else if (field?.type.name && field?.type?.fieldviews[join_fieldview])
+      segment.field_type = field.type.name;
   }
 };
 /** @type {function} */
@@ -452,7 +436,7 @@ const run = async (
     extraOpts && extraOpts.req && extraOpts.req.user
       ? extraOpts.req.user.role_id
       : 10;
-  await set_join_fieldviews({ columns, fields });
+  await set_join_fieldviews({ table, columns, fields });
 
   const tfields = get_viewable_fields(
     viewname,
