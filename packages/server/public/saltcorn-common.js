9fc74a48ab4f03c954688ab4718e55b000fe004b
Tom Nielsen <tanielsen@gmail.com>
2022-07-06 14:33:06 +0100

    Fix list fieldview edit (#1289)
    * calc opts selected from val
    
    * set selected on selects
    
    * isoDateFormatter accept format

--- packages/server/public/saltcorn-common.js
+++ packages/server/public/saltcorn-common.js
@@ -63,8 +63,8 @@ function apply_showif() {
       .val();
 
     var options = data[1][val];
-    var current = e.attr("data-selected");
-    //console.log({ val, options, current, data });
+    var current = e.attr("data-selected") || e.val();
+    //console.log({ field: e.attr("name"), target: data[0], val, current });
     e.empty();
     (options || []).forEach((o) => {
       if (
