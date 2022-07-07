bc70581c962b70a50f3d2709df246f59073a6949
Christian Hugo <hugo.christian@web.de>
2022-07-07 16:56:43 +0200

    include mobile-builder in the typdedoc docu

--- packages/saltcorn-mobile-builder/index.ts
+++ packages/saltcorn-mobile-builder/index.ts
@@ -1,1 +1,11 @@
-export default {};
+// For the typedoc documentation
+
+/**
+ * This is the mobile builder package
+ * @module
+ */
+export * as common_build_utils from "./utils/common-build-utils";
+export * as cordova_build_utils from "./utils/cordova-build-utils";
+export * as package_bundle_utils from "./utils/package-bundle-utils";
+
+export * from "./mobile-builder";
