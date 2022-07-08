8d0ba3610b94faafc6d8447a8643c0bf5b86a0b6
Tom Nielsen <tanielsen@gmail.com>
2022-07-08 15:47:02 +0100

    eval extra state fml in filter view (#1295)
    * eval extra state fml in filter view
    
    * embed any view in filter

--- packages/saltcorn-data/base-plugin/viewtemplates/filter.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/filter.js
@@ -39,7 +39,10 @@ const { InvalidConfiguration } = require("../../utils");
 const { jsexprToWhere } = require("../../models/expression");
 const Library = require("../../models/library");
 const { getState } = require("../../db/state");
-
+const {
+  get_expression_function,
+  eval_expression,
+} = require("../../models/expression");
 /**
  * @returns {Workflow}
  */
@@ -74,11 +77,25 @@ const configuration_workflow = () =>
             context.table_id || context.exttable_name,
             ({ viewrow }) => viewrow.name !== context.viewname
           );
-          const views = own_link_views.map((v) => ({
-            label: v.name,
-            name: v.name,
-            viewtemplate: v.viewtemplate,
-          }));
+          const own_view_names = new Set();
+          const views = own_link_views.map((v) => {
+            own_view_names.add(v.name);
+            return {
+              label: v.name,
+              name: v.name,
+              viewtemplate: v.viewtemplate,
+            };
+          });
+          const all_views = await View.find();
+          for (const v of all_views) {
+            if (!own_view_names.has(v.name)) {
+              views.push({
+                label: v.name,
+                name: v.name,
+                viewtemplate: v.viewtemplate,
+              });
+            }
+          }
           for (const field of fields) {
             const presets = field.presets;
             field.preset_options = presets ? Object.keys(presets) : [];
@@ -173,11 +190,15 @@ const run = async (
     },
     view: async (segment) => {
       const view = await View.findOne({ name: segment.view });
+      const extra_state = segment.extra_state_fml
+        ? eval_expression(segment.extra_state_fml, {}, extra.req.user)
+        : {};
+      const state1 = { ...state, ...extra_state };
       if (!view)
         throw new InvalidConfiguration(
           `View ${viewname} incorrectly configured: cannot find view ${segment.view}`
         );
-      else segment.contents = await view.run(state, extra);
+      else segment.contents = await view.run(state1, extra);
     },
     link: (segment) => {
       if (segment.transfer_state) {
