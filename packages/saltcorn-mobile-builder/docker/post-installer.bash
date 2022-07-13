d977ff73f0f7f68f5eb2aa1664f513ad039dc02e
Christian Hugo <hugo.christian@web.de>
2022-07-13 21:36:37 +0200

    include files in npm pck, skip build env var

--- packages/saltcorn-mobile-builder/docker/post-installer.bash
+++ packages/saltcorn-mobile-builder/docker/post-installer.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker build . -f Dockerfile -t saltcorn/cordova-builder
+dir=`dirname "$0"`
+if [ "$SKIP_DOCKER_IMAGE_INSTALL" != "true" ]; then
+  docker build "$dir" -f "$dir"/Dockerfile -t saltcorn/cordova-builder
+fi
 
 exit 0
