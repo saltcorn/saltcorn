605ccd6df1d1f0caef87e4d8ff596905dcc6f7e5
christianhugoch <hugo.christian@web.de>
2022-07-14 17:44:42 +0200

    platform independent js script instead of bash (#1305)
    * platform independent js script instead of bash
    
    * fixed path

---
+++ packages/saltcorn-mobile-builder/docker/post-installer.js
@@ -0,0 +1,20 @@
+const { spawnSync } = require("child_process");
+const { join } = require("path");
+
+if (process.env.SKIP_DOCKER_IMAGE_INSTALL === "true") {
+  console.log("skipping build 'saltcorn/cordova-builder' docker image");
+} else {
+  const result = spawnSync(
+    "docker",
+    [
+      "build",
+      __dirname,
+      "-f",
+      join(__dirname, "Dockerfile"),
+      "-t",
+      "saltcorn/cordova-builder",
+    ],
+    { cwd: ".", stdio: "inherit" }
+  );
+  if (result.error) console.log(result.error.toString());
+}
