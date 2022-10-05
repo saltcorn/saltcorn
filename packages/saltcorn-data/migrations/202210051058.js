ac2792ff54e1faf3f3aa0e82e0c4e3300e0e5b93
Tom Nielsen <tanielsen@gmail.com>
2022-10-05 12:12:51 +0100

    log file fields

--- packages/saltcorn-data/migrations/202210051058.js
+++ packages/saltcorn-data/migrations/202210051058.js
@@ -1,5 +1,6 @@
 const js = async () => {
     const Table = require("../models/table");
+    const Field = require("../models/field");
     const File = require("../models/file");
     const db = require("../db");
     const tables = await Table.find({});
@@ -10,7 +11,7 @@ const js = async () => {
 
     //TODO bail out if S3
 
-    // rename all files, move to tenant dir
+    // rename all files, move to tenant dir with new name
     const db_files = await File.find({ inDB: true })
     const newLocations = {}
     for (const file of db_files) {
@@ -30,6 +31,10 @@ const js = async () => {
             }
         }
     }
-    //
+    // migrate file fields to text fields
+    const fileFields = await Fields.find({ type: "File" })
+    console.log(fileFields[0]);
+
+
 }
 module.exports = { js };
