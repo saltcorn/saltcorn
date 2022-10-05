6e3d6a743996121d35770e172afb7b43133a339f
Tom Nielsen <tanielsen@gmail.com>
2022-10-05 11:04:47 +0100

    log files

--- packages/saltcorn-data/migrations/202210051058.js
+++ packages/saltcorn-data/migrations/202210051058.js
@@ -1,9 +1,11 @@
 const js = async () => {
     const Table = require("../models/table");
+    const File = require("../models/file");
     const db = require("../db");
     const tables = await Table.find({});
 
     // rename all files, move to tenant dir
-
+    const db_files = await File.find({ inDB: true })
+    console.log(db_files[0]);
 }
 module.exports = { js };
