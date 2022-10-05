c145accd89894dfc2f6d5b2810129b35dae7adae
Tom Nielsen <tanielsen@gmail.com>
2022-10-05 12:58:52 +0100

    update rows

--- packages/saltcorn-data/migrations/202210051058.js
+++ packages/saltcorn-data/migrations/202210051058.js
@@ -1,3 +1,4 @@
+
 const js = async () => {
     const Table = require("../models/table");
     const Field = require("../models/field");
@@ -7,8 +8,11 @@ const js = async () => {
     const fsp = require("fs").promises
     const fs = require("fs")
     const path = require("path")
+    const { getState } = require("../db/state");
+
     await File.ensure_file_store(db.getTenantSchema())
 
+    await getState().refresh(false)
     //TODO bail out if S3
 
     // rename all files, move to tenant dir with new name
@@ -31,16 +35,28 @@ const js = async () => {
             }
         }
     }
+    //console.log(newLocations);
     // migrate file fields to text fields
     const fileFields = await Field.find({ type: "File" })
     const schema = db.getTenantSchemaPrefix()
     for (const field of fileFields) {
         const table = Table.findOne({ id: field.table_id })
-        console.log({ table });
+        //console.log({ table });
+        //db.set_sql_logging(true)
+        await db.query(`alter table ${schema}"${table.name}" drop constraint "${table.name}_${field.name}_fkey"`)
         await db.query(`alter table ${schema}"${table.name}" alter column "${field.name}" type text;`)
+        const rows = await table.getRows({})
+        for (const row of rows) {
+            if (row[field.name]) {
+                await table.updateRow({ [field.name]: newLocations[row[field.name]] }, row[table.pk_name])
+            }
+
+            //   throw new Error(`row value: ${table.name}.${field.name} ${JSON.stringify(row, null, 2)} new ${newLocations[row[field.name]]}`)
+        }
 
     }
-    //console.log(fileFields[0]);
+
+    // pages etc, other layout items
 
 
 }
