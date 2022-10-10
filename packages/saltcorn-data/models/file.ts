4fe25d2ae03e1ea629ec555aa64296eab356ea7c
Tom Nielsen <tanielsen@gmail.com>
2022-10-10 14:13:21 +0100

    serve legacy files by id

--- packages/saltcorn-data/models/file.ts
+++ packages/saltcorn-data/models/file.ts
@@ -218,6 +218,21 @@ class File {
       const { getState } = require("../db/state");
       const state = getState();
       const useS3 = state?.getConfig("storage_s3_enabled");
+      //legacy serving ids
+      if (/^\d+$/.test(where)) {
+        const legacy_file_id_locations = state?.getConfig(
+          "legacy_file_id_locations"
+        );
+        //console.log("lfil", legacy_file_id_locations);
+
+        if (legacy_file_id_locations?.[where]) {
+          const legacy_file = await File.findOne(
+            legacy_file_id_locations?.[where]
+          );
+          return legacy_file;
+        }
+      }
+
       if (useS3) {
         const files = await File.find({ id: +where });
         return files[0];
@@ -489,7 +504,7 @@ class File {
       size: statSync(fullPath).size,
     };
     const created = await File.from_req_files(file, userId);
-    if(!created.location.endsWith(name)) {
+    if (!created.location.endsWith(name)) {
       created.filename = parse(created.location).base;
     }
     return created;
