f09dadbef98774e78128239f07336c63cfa62039
Christian Hugo <hugo.christian@web.de>
2022-12-04 22:03:40 +0100

    docker image with saltcorn and mobile dependencies - the image has all depencies to build a mobile app for android   and the '@saltcorn/cli' version from npm (like the release image) - is needed because the image from 'saltcorn-mobile-builder' only has the build dependencies   and it's complicated to run the build image from another container - added NODE_ENV=development for the cordova commands   it fails with NODE_ENV=production

--- deploy/docker_build_push.sh
+++ deploy/docker_build_push.sh
@@ -9,6 +9,10 @@ docker build -t saltcorn/saltcorn:latest -f Dockerfile.release .
 docker push saltcorn/saltcorn:latest
 docker build -t saltcorn/saltcorn:$VERSION -f Dockerfile.release .
 docker push saltcorn/saltcorn:$VERSION
+docker build -t saltcorn/saltcorn-with-mobile:latest -f Dockerfile.mobile.release .
+docker push saltcorn/saltcorn-with-mobile:latest
+docker build -t saltcorn/saltcorn-with-mobile:$VERSION -f Dockerfile.mobile.release .
+docker push saltcorn/saltcorn-with-mobile:$VERSION
 docker build -t saltcorn/saltcorn:dev -f Dockerfile.dev .
 docker push saltcorn/saltcorn:dev
 docker build -t saltcorn/saltcorn:pyml -f Dockerfile.pyml .
