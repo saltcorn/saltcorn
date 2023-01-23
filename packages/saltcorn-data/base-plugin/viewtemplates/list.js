7528b78e800c1688d27e434cdd1f3df5a8cbd1a5
Tom Nielsen <tanielsen@gmail.com>
2023-01-23 17:50:47 +0000

    rm ownership logic from list

--- packages/saltcorn-data/base-plugin/viewtemplates/list.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/list.js
@@ -772,18 +772,6 @@ module.exports = {
         q.orderDesc = default_state && default_state._descending;
 
       const role = req && req.user ? req.user.role_id : 10;
-      if (table.ownership_field_id && role > table.min_role_read && req) {
-        const owner_field = fields.find(
-          (f) => f.id === table.ownership_field_id
-        );
-        mergeIntoWhere(where, {
-          [owner_field.name]: req.user ? req.user.id : -1,
-        });
-      }
-      if (table.ownership_formula && role > table.min_role_read) {
-        const freeVars = freeVariables(table.ownership_formula);
-        add_free_variables_to_joinfields(freeVars, joinFields, fields);
-      }
 
       //console.log({ i: default_state.include_fml });
       if (default_state?.include_fml) {
@@ -800,10 +788,6 @@ module.exports = {
         forUser: req.user,
       });
 
-      //TODO this will mean that limit is not respected. change filter to jsexprToWhere
-      if (table.ownership_formula && role > table.min_role_read && req) {
-        rows = rows.filter((row) => table.is_owner(req.user, row));
-      }
       const rowCount = await table.countRows(where);
       return { rows, rowCount };
     },
