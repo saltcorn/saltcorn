1a116e14ceb147a950c3dde1cd85ebb2a592015d
Tom Nielsen <tanielsen@gmail.com>
2023-02-11 15:15:01 +0000

    2 server redirects

--- packages/server/auth/routes.js
+++ packages/server/auth/routes.js
@@ -17,6 +17,7 @@ const {
   loggedIn,
   csrfField,
   setTenant,
+  is_absolute_url,
 } = require("../routes/utils.js");
 const { getState } = require("@saltcorn/data/db/state");
 const { send_reset_email } = require("./resetpw");
@@ -1078,7 +1079,7 @@ router.post(
     }
     if (getState().get2FApolicy(req.user) === "Mandatory") {
       res.redirect("/auth/twofa/setup/totp");
-    } else if (req.body.dest) {
+    } else if (req.body.dest && !is_absolute_url(req.body.dest)) {
       res.redirect(decodeURIComponent(req.body.dest));
     } else res.redirect("/");
   })
