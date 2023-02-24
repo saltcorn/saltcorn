832e4dd19b91f8c1878f6afb3794acb739f2e7ef
Tom Nielsen <tanielsen@gmail.com>
2023-02-24 22:55:22 +0000

    path to dir

--- deploy/pupchecks/run.sh
+++ deploy/pupchecks/run.sh
@@ -3,20 +3,20 @@
 set -e
 
 PGDATABASE=saltcorn_test
-saltcorn reset-schema -f
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
+../../packages/saltcorn-cli/bin/saltcorn reset-schema -f
 
 echo Restoring test application backup...
-saltcorn restore ../../packages/saltcorn-random-tests/backup-files/sc-backup-sub2-2023-01-06-21-49.zip
+../../packages/saltcorn-cli/bin/saltcorn restore ../../packages/saltcorn-random-tests/backup-files/sc-backup-sub2-2023-01-06-21-49.zip
 
 echo Starting background Saltcorn server...
-saltcorn serve -p 3012 &
+../../packages/saltcorn-cli/bin/saltcorn serve -p 3012 &
 SCPID=$!
 trap "kill $SCPID" EXIT
 
 while ! nc -z localhost 3012; do   
-  sleep 0.1 
+  sleep 0.2 
 done
 
 npx pupcheck e2e.pch
