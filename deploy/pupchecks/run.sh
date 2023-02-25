b10b5118a19a34a172eb8b19128c64bce267da32
Tom Nielsen <tanielsen@gmail.com>
2023-02-25 15:34:06 +0000

    blog test

--- deploy/pupchecks/run.sh
+++ deploy/pupchecks/run.sh
@@ -15,12 +15,30 @@ for filename in with_fixtures/*.pch; do
 
   echo Starting background Saltcorn server...
   saltcorn serve -p 3012 &
-  SCPID=$!
-  trap "kill $SCPID" EXIT
+  SCPID1=$!
+  trap "kill $SCPID1" EXIT
 
   while ! nc -z localhost 3012; do   
     sleep 0.2 
   done
 
   PUPCHECK_BASE_URL="http://localhost:3012" npx pupcheck -H $filename
+done
+
+
+for filename in no_fixtures/*.pch; do
+  saltcorn reset-schema -f
+
+  echo Restoring test application backup...
+
+  echo Starting background Saltcorn server...
+  saltcorn serve -p 3013 &
+  SCPID2=$!
+  trap "kill $SCPID2" EXIT
+
+  while ! nc -z localhost 3013; do   
+    sleep 0.2 
+  done
+
+  PUPCHECK_BASE_URL="http://localhost:3013" npx pupcheck -H $filename
 done
