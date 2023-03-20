026aa1f92d82c665e6653fe82d13db2c44c979d5
Tom Nielsen <tanielsen@gmail.com>
2023-03-20 11:55:59 +0000

    model

---
+++ packages/saltcorn-data/migrations/202303201126.js
@@ -0,0 +1,22 @@
+const sql_pg = `CREATE UNLOGGED TABLE _sc_notifications (
+  id serial primary key,
+  created timestamptz NOT NULL,
+  title text not null,
+  body text not null,
+  link text not null,
+  user_id int references users(id) not null,
+  read boolean not null
+);`;
+//CREATE INDEX ON _sc_notifications (user_id);
+
+const sql_sqlite = `CREATE TABLE _sc_notifications (
+  id integer primary key,
+  created timestamptz NOT NULL,
+  title text not null,
+  body text not null,
+  link text not null,
+  user_id int references users(id) not null,
+  read boolean not null
+);`;
+
+module.exports = { sql_pg, sql_sqlite };
---
+++ packages/saltcorn-data/models/notification.ts
@@ -0,0 +1,86 @@
+/**
+ * Notification Database Access Layer
+ * @category saltcorn-data
+ * @module models/notification
+ * @subcategory models
+ */
+import db from "../db";
+import type { Where, SelectOptions, Row } from "@saltcorn/db-common/internal";
+
+/**
+ * Notification Class
+ * @category saltcorn-data
+ */
+
+class Notification {
+  id?: number;
+  created: Date;
+  title: string;
+  body: string;
+  link: string;
+  user_id: number;
+  read: boolean;
+
+  /**
+   * Notification constructor
+   * @param {object} o
+   */
+  constructor(o: NotificationCfg | Notification) {
+    this.id = o.id;
+    this.created = o.created;
+    this.title = o.title;
+    this.body = o.body;
+    this.link = o.link;
+    this.user_id = o.user_id;
+    this.read = !!o.read;
+  }
+  /**
+   * @param {*} where
+   * @param {*} selectopts
+   * @returns {Notificatio[]}
+   */
+  static async find(
+    where: Where,
+    selectopts?: SelectOptions
+  ): Promise<Notification[]> {
+    const us = await db.select("_sc_notifications", where, selectopts);
+    return us.map((u: any) => new Notification(u));
+  }
+
+  /**
+   * @param {*} where
+   * @returns {Notification}
+   */
+  static async findOne(where: Where): Promise<Notification> {
+    const u = await db.selectMaybeOne("_sc_notifications", where);
+    return u ? new Notification(u) : u;
+  }
+
+  static async create(notin: NotificationCfg): Promise<void> {
+    const o = new Notification(notin);
+    await db.insert("_sc_notifications", {
+      created: o.created,
+      title: o.title,
+      body: o.body,
+      link: o.link,
+      user_id: o.user_id,
+      read: o.read,
+    });
+  }
+
+  async mark_as_read() {
+    await db.update("_sc_notifications", { read: true }, this.id);
+  }
+}
+
+type NotificationCfg = {
+  id?: number;
+  created: Date;
+  title: string;
+  body: string;
+  link: string;
+  user_id: number;
+  read: boolean;
+};
+
+export = Notification;
