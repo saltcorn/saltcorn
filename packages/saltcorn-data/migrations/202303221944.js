da16c15345b47a4e8dd623682183cb783e07ab09
Tom Nielsen <tanielsen@gmail.com>
2023-03-23 12:59:55 +0000

    sqlite is array

--- packages/saltcorn-data/migrations/202303221944.js
+++ packages/saltcorn-data/migrations/202303221944.js
@@ -1,5 +1,7 @@
 const sql_pg = `alter table _sc_tables add column "provider_cfg" jsonb, add column "provider_name" text`;
-const sql_sqlite = `alter table _sc_tables add column provider_cfg json;
-alter table _sc_tables add column provider_name text default '';`;
+const sql_sqlite = [
+  `alter table _sc_tables add column "provider_cfg" json;`,
+  `alter table _sc_tables add column "provider_name" text default '';`,
+];
 
 module.exports = { sql_pg, sql_sqlite };
