0405649c1b6c58d9911226e0544a7e8e55a08fad
Tom Nielsen <tanielsen@gmail.com>
2023-05-28 18:07:21 +0100

    attach docker build during DO image build

--- packages/saltcorn-mobile-builder/docker/post-installer.js
+++ packages/saltcorn-mobile-builder/docker/post-installer.js
@@ -1,27 +1,32 @@
-const { spawn } = require("child_process");
+const { spawn, spawnSync } = require("child_process");
 const { join } = require("path");
 
 if (process.env.SKIP_DOCKER_IMAGE_INSTALL === "true") {
   console.log("skipping build 'saltcorn/cordova-builder' docker image");
 } else {
-  console.log("spawn docker");
-  const child = spawn(
-    "docker",
-    [
-      "build",
-      __dirname,
-      "--network",
-      "host",
-      "--no-cache",
-      "-f",
-      join(__dirname, "Dockerfile"),
-      "-t",
-      "saltcorn/cordova-builder",
-    ],
-    { cwd: ".", stdio: "ignore", detached: true }
-  );
-  child.unref();
-  child.on("error", () => {
-    // console not available
-  });
+  const dArgs = [
+    "build",
+    __dirname,
+    "--network",
+    "host",
+    "--no-cache",
+    "-f",
+    join(__dirname, "Dockerfile"),
+    "-t",
+    "saltcorn/cordova-builder",
+  ];
+  if (process.env.ATTTACH_DOCKER_IMAGE_BUILD === "true") {
+    const result = spawnSync("docker", dArgs, { cwd: ".", stdio: "inherit" });
+    if (result.error) console.log(result.error.toString());
+  } else {
+    const child = spawn("docker", dArgs, {
+      cwd: ".",
+      stdio: "ignore",
+      detached: true,
+    });
+    child.unref();
+    child.on("error", () => {
+      // console not available
+    });
+  }
 }
