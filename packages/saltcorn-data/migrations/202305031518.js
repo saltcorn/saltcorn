ebc813c03176fce06d49816774477eb43b5f8af1
Tom Nielsen <tanielsen@gmail.com>
2023-06-12 18:01:07 +0100

    better unique constraints

--- packages/saltcorn-data/migrations/202305031518.js
+++ packages/saltcorn-data/migrations/202305031518.js
@@ -1,14 +1,15 @@
 const sql_pg = `
 create table _sc_models (
   id serial primary key,
-  name text not null unique,
+  name text not null,
   table_id integer references _sc_tables(id),
   modeltemplate text not null,
-  configuration jsonb
+  configuration jsonb,
+  UNIQUE (table_id, name)
 );
 create table _sc_model_instances (
   id serial primary key,
-  name text not null unique,
+  name text not null,
   model_id integer references _sc_models(id),
   state jsonb,
   hyperparameters jsonb,
@@ -17,22 +18,24 @@ create table _sc_model_instances (
   metric_values jsonb,
   parameters jsonb,
   fit_object bytea, 
-  is_default boolean
+  is_default boolean,
+  unique (model_id, name)
 );`;
 
 const sql_sqlite = [
   `
 create table _sc_models (
   id integer primary key,
-  name text not null unique,
+  name text not null,
   table_id integer references _sc_tables(id),
   modeltemplate text not null,
-  configuration json
+  configuration json,
+  UNIQUE(table_id, name) ON CONFLICT FAIL
 );`,
   `
 create table _sc_model_instances (
   id integer primary key,
-  name text not null unique,
+  name text not null,
   model_id integer references _sc_models(id),
   state json,
   hyperparameters json,
@@ -41,7 +44,8 @@ create table _sc_model_instances (
   metric_values json,
   parameters json,
   fit_object blob, 
-  is_default boolean
+  is_default boolean,
+  UNIQUE(model_id, name) ON CONFLICT FAIL
 );`,
 ];
 
