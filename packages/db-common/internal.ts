f939a653eb33cba1047721ca449600eb87dc807a
Michael Dubner <pywebmail@list.ru>
2023-08-01 20:37:31 +0300

    Rewrite jsonWhere (closes #1941, closes #1896)

--- packages/db-common/internal.ts
+++ packages/db-common/internal.ts
@@ -121,6 +121,8 @@ const whereFTS = (
 };
 
 export type Value = string | number | boolean | Date | Value[];
+export type JsonPathElem = string | number;
+export type JsonPath = JsonPathElem | JsonPathElem[];
 
 export type Where = {
   _fts?: { fields: any[]; table?: string; searchTerm: string; schema?: string };
@@ -372,14 +374,21 @@ function jsonWhere(
   v: any[] | Object,
   phs: PlaceHolderStack
 ): string {
-  const lhs = (f: string, sf: string, convText: boolean) =>
+  const jsonpathElemEscape = (sf: JsonPathElem): string =>
+    typeof sf == 'number' ? `[${sf}]` :
+    `.${ /[\x00-\x08\x0A-\x1F\x22\x27\x7F.[\]]/.test(String(sf)) ? JSON.stringify(String(sf)) : sf }`;
+  const jsonpathPrepare = (sf: JsonPath): string =>
+    ((/^\$[[.]/.test(String(sf)) && !/[\n\r\v\0]/.test(String(sf))) ? String(sf) :
+    `\$${Array.isArray(sf) ? sf.map(jsonpathElemEscape).join('') : jsonpathElemEscape(sf)}`
+    ).replace(/'/g, "''");
+  const lhs = (f: string, sf: JsonPath, convText: boolean): string =>
     phs.is_sqlite
-      ? `json_extract(${quote(
-          sqlsanitizeAllowDots(f)
-        )}, '$.${sqlsanitizeAllowDots(sf)}')`
-      : `${quote(sqlsanitizeAllowDots(f))}${
-          convText ? "->>" : "->"
-        }'${sqlsanitizeAllowDots(sf)}'`;
+      ? `json_extract(${quote(sqlsanitizeAllowDots(f))}, '${jsonpathPrepare(sf)}')`
+      : `${
+          convText ? "jsonb_build_array(" : ""
+        }jsonb_path_query_first(${quote(sqlsanitizeAllowDots(f))}, '${jsonpathPrepare(sf)}')${
+          convText ? ")->>0" : ""
+        }`;
 
   if (Array.isArray(v)) return `${lhs(k, v[0], true)}=${phs.push(v[1])}`;
   else {
