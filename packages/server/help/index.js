66aff30a54e0a16ce70eaadd5a5dfdec94c47d45
Tom Nielsen <tanielsen@gmail.com>
2023-10-01 20:37:11 +0100

    eval

--- packages/server/help/index.js
+++ packages/server/help/index.js
@@ -6,13 +6,24 @@ const MarkdownIt = require("markdown-it"),
   md = new MarkdownIt();
 const get_md_file = async (topic) => {
   try {
-    const fp = require.resolve(`./${File.normalise(topic)}.md`);
+    const fp = require.resolve(`./${File.normalise(topic)}.hmd`);
     const fileBuf = await fs.readFile(fp);
     return fileBuf.toString();
   } catch (e) {
     return false;
   }
 };
+
+//https://gist.github.com/asselin/2762936
+Handlebars.registerHelper("$", function (expr) {
+  console.log(expr);
+  if (arguments.length < 2)
+    throw new Error("Handlerbars Helper 'arrayaccess' needs 1 parameter");
+  // eslint-disable-next-line
+  with (this) {
+    return eval(expr);
+  }
+});
 
 const get_help_markup = async (topic, query) => {
   const context = {};
@@ -21,7 +32,9 @@ const get_help_markup = async (topic, query) => {
   }
   const mdTemplate = await get_md_file(topic);
   if (!mdTemplate) return { markup: "Topic not found" };
-  const template = Handlebars.compile(mdTemplate || "");
+  const template = Handlebars.compile(mdTemplate, {
+    allowProtoMethodsByDefault: true,
+  });
   const mdTopic = template(context);
   const markup = md.render(mdTopic);
   return { markup };
