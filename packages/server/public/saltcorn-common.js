c662ee6b04f86ed5094d31f2f93ab86a7f6c1460
Tom Nielsen <tanielsen@gmail.com>
2023-11-22 10:52:02 +0000

    fix radio group in get form rec

--- packages/server/public/saltcorn-common.js
+++ packages/server/public/saltcorn-common.js
@@ -397,12 +397,15 @@ function get_form_record(e_in, select_labels) {
     }
 
   e.find("input[name],select[name],textarea[name]").each(function () {
-    const name = $(this).attr("data-fieldname") || $(this).attr("name");
-    if (select_labels && $(this).prop("tagName").toLowerCase() === "select")
-      rec[name] = $(this).find("option:selected").text();
-    else if ($(this).prop("type") === "checkbox")
-      rec[name] = $(this).prop("checked");
-    else rec[name] = $(this).val();
+    const $this = $(this);
+    const name = $this.attr("data-fieldname") || $this.attr("name");
+    if (select_labels && $this.prop("tagName").toLowerCase() === "select")
+      rec[name] = $this.find("option:selected").text();
+    else if ($this.prop("type") === "checkbox")
+      rec[name] = $this.prop("checked");
+    else if ($this.prop("type") === "radio" && !$this.prop("checked")) {
+      //do nothing
+    } else rec[name] = $this.val();
   });
   return rec;
 }
