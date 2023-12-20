e2a3131be5b9fcd6da3d2de5f1a77cea1b83f7f4
Tom Nielsen <tanielsen@gmail.com>
2023-12-20 12:26:17 +0000

    run-sql: catch errors

--- packages/saltcorn-cli/src/commands/run-sql.js
+++ packages/saltcorn-cli/src/commands/run-sql.js
@@ -4,7 +4,11 @@
  */
 const { Command, flags } = require("@oclif/command");
 const { cli } = require("cli-ux");
-const { maybe_as_tenant, init_some_tenants, readFileSync } = require("../common");
+const {
+  maybe_as_tenant,
+  init_some_tenants,
+  readFileSync,
+} = require("../common");
 const db = require("@saltcorn/data/db");
 
 /**
@@ -13,53 +17,55 @@ const db = require("@saltcorn/data/db");
  * @category saltcorn-cli
  */
 class RunSQLCommand extends Command {
-
   /**
    * @returns {Promise<void>}
    */
   async run() {
     const { flags, args } = this.parse(RunSQLCommand);
 
-    if(!flags.sql&&!flags.file){
-      console.log("One of arguments is required: --sql or --file.\nRun with --help to get more info");
+    if (!flags.sql && !flags.file) {
+      console.log(
+        "One of arguments is required: --sql or --file.\nRun with --help to get more info"
+      );
       this.exit(1);
     }
 
     await init_some_tenants(flags.tenant);
 
-
     //const { mockReqRes } = require("@saltcorn/data/tests/mocks");
 
     const that = this;
     await maybe_as_tenant(flags.tenant, async () => {
-
       const schema = db.getTenantSchema();
-      if(flags.tenant)
-        if(db.isSQLite) {
-          console.warn("SQLite is used as datasource. Tenants are unsupported")
-        }
-      else {
-        // https://www.commandprompt.com/education/how-do-i-setchange-the-default-schema-in-postgresql/
-        // life hack to set default schema for tables in PG
-        await db.query("SET SEARCH_PATH='"+schema+"'");
+      if (flags.tenant)
+        if (db.isSQLite) {
+          console.warn("SQLite is used as datasource. Tenants are unsupported");
+        } else {
+          // https://www.commandprompt.com/education/how-do-i-setchange-the-default-schema-in-postgresql/
+          // life hack to set default schema for tables in PG
+          await db.query("SET SEARCH_PATH='" + schema + "'");
         }
 
       console.log("current tenant:", schema);
       //if(flags.sql){
-      const sql_str = flags.sql? flags.sql  : readFileSync(flags.file);
+      const sql_str = flags.sql ? flags.sql : readFileSync(flags.file);
       // check that file not find (not directly)
-      if (sql_str === null ){
+      if (sql_str === null) {
         this.exit(1);
       }
-      const query = await db.query(sql_str);
+      try {
+        const query = await db.query(sql_str);
 
-      if (!query) {
-        console.error(`Cannot execute Query ${flags.sql}`);
-        this.exit(1);
+        if (!query) {
+          console.error(`Cannot execute Query ${flags.sql}`);
+          this.exit(1);
+        }
+        // print sql statement
+        console.log(sql_str);
+        console.table(query.rows);
+      } catch (e) {
+        console.error(e);
       }
-      // print sql statement
-      console.log(sql_str);
-      console.table (query.rows);
     });
     this.exit(0);
   }
