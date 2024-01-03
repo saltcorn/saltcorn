1b65006fa5286d7f3b9070d646b712810fb324db
Tom Nielsen <tanielsen@gmail.com>
2024-01-03 12:46:52 +0000

    fix plugin test error detect

--- packages/server/tests/plugins.test.js
+++ packages/server/tests/plugins.test.js
@@ -233,7 +233,7 @@ describe("Pack Endpoints", () => {
       .send(
         "pack=les%22%3A+%5B%5D%2C+%22views%22%3A+%5B%5D%2C+%22plugins%22%3A+%5B%5D%2C+%22pages%22%3A+%5B%5D+%7D"
       )
-      .expect(toInclude("alert-danger"));
+      .expect(toInclude("Unexpected token l in JSON at position 0"));
   });
   it("should install named", async () => {
     const loginCookie = await getAdminLoginCookie();
