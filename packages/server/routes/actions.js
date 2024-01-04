e1260885c5862fb8374a9a73f70cf9fa4892fdd7
Tom Nielsen <tanielsen@gmail.com>
2024-01-04 22:14:50 +0000

    trigger cfg route accept name

--- packages/server/routes/actions.js
+++ packages/server/routes/actions.js
@@ -380,11 +380,15 @@ router.post(
  * @memberof module:routes/actions~actionsRouter
  */
 router.get(
-  "/configure/:id",
+  "/configure/:idorname",
   isAdmin,
   error_catcher(async (req, res) => {
-    const { id } = req.params;
-    const trigger = await Trigger.findOne({ id });
+    const { idorname } = req.params;
+    let trigger;
+    let id = parseInt(idorname);
+    if (id) trigger = await Trigger.findOne({ id });
+    else trigger = await Trigger.findOne({ name: idorname });
+
     if (!trigger) {
       req.flash("warning", req.__("Action not found"));
       res.redirect(`/actions/`);
@@ -396,11 +400,16 @@ router.get(
       ? Table.findOne({ id: trigger.table_id })
       : null;
 
-    const subtitle = span(
-      { class: "ms-3" },
-      trigger.action,
-      table ? ` on ` + a({ href: `/table/${table.name}` }, table.name) : ""
-    ) + a({href: `/actions/testrun/${id}`, class: "ms-2" }, req.__("Test run")+"&nbsp;&raquo;");
+    const subtitle =
+      span(
+        { class: "ms-3" },
+        trigger.action,
+        table ? ` on ` + a({ href: `/table/${table.name}` }, table.name) : ""
+      ) +
+      a(
+        { href: `/actions/testrun/${id}`, class: "ms-2" },
+        req.__("Test run") + "&nbsp;&raquo;"
+      );
     if (!action) {
       req.flash("warning", req.__("Action not found"));
       res.redirect(`/actions/`);
@@ -618,7 +627,7 @@ router.get(
     let table, row;
     if (trigger.table_id) {
       table = Table.findOne({ id: trigger.table_id });
-      row = await table.getRow({}, {orderBy: "RANDOM()"});
+      row = await table.getRow({}, { orderBy: "RANDOM()" });
     }
     let runres;
 
@@ -666,7 +675,10 @@ router.get(
               "&laquo;&nbsp;" + req.__("back to actions")
             ),
             a(
-              { href: `/actions/configure/${trigger.id}`, class: "mt-4 btn btn-primary me-1" },
+              {
+                href: `/actions/configure/${trigger.id}`,
+                class: "mt-4 btn btn-primary me-1",
+              },
               req.__("Configure action")
             ),
             a(
