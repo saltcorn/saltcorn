200545bbba61eac59c813e27e84d3b128f02d4f9
Tom Nielsen <tanielsen@gmail.com>
2024-01-17 12:02:56 +0000

    semi colon in migration

--- packages/saltcorn-data/migrations/202311231355.js
+++ packages/saltcorn-data/migrations/202311231355.js
@@ -4,7 +4,7 @@ const sql = [
   name text NOT NULL,
   description text,
   min_role integer references _sc_roles(id) NOT NULL
-)`,
+);`,
   `create table _sc_page_group_members (
   id serial primary key,
   page_id integer references _sc_pages(id) NOT NULL,
@@ -12,7 +12,7 @@ const sql = [
   sequence integer NOT NULL,
   eligible_formula text,
   description text
-)`,
+);`,
 ];
 
 module.exports = { sql };
