52dfb589461ae6d5a6d3298c9bfab992a2e99b23
Tom Nielsen <tanielsen@gmail.com>
2024-02-06 21:28:16 +0000

    Cache files cfg (#2270)
    * file cache setting
    
    * in serve
    
    * search_or_create: fix sorting
    
    * SO link for attribution

--- packages/saltcorn-data/base-plugin/fieldviews.js
+++ packages/saltcorn-data/base-plugin/fieldviews.js
@@ -633,9 +633,16 @@ const search_or_create = {
               attrs.summary_field
             }+'</option>').join("")
             ${reqd ? "" : `opts = '<option></option>'+opts`}
-            $(elem).prev().html(opts).prop('selectedIndex', res.success.length${
+            const sel = $(elem).prev().html(opts);
+            sel.html(opts).prop('selectedIndex', res.success.length${
               reqd ? "-1" : ""
             }); 
+            // https://stackoverflow.com/a/26232541
+            var selected = sel.val(); // cache selected value, before reordering
+            var opts_list = sel.find('option');
+            opts_list.sort(function(a, b) { return $(a).text().toLowerCase() > $(b).text().toLowerCase() ? 1 : -1; });
+            sel.html('').append(opts_list);
+            sel.val(selected);
           }
         })
       }`)
