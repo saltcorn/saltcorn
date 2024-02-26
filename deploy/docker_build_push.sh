f507823f86e08ecf3543e7709e330d5173319858
Tom Nielsen <tanielsen@gmail.com>
2024-02-26 14:29:39 +0000

    build docker main first

--- deploy/docker_build_push.sh
+++ deploy/docker_build_push.sh
@@ -4,18 +4,17 @@ set -xe
 
 VERSION=`jq -r .version packages/saltcorn-data/package.json`
 echo $VERSION
+docker build --no-cache -t saltcorn/saltcorn:latest -f Dockerfile.release .
+docker push saltcorn/saltcorn:latest
+
+docker build -t saltcorn/saltcorn:$VERSION -f Dockerfile.release .
+docker push saltcorn/saltcorn:$VERSION
 
 docker build --no-cache -t saltcorn/saltcorn-with-mobile:latest -f Dockerfile.mobile.release .
 docker push saltcorn/saltcorn-with-mobile:latest
 
 docker build -t saltcorn/saltcorn-with-mobile:$VERSION -f Dockerfile.mobile.release .
 docker push saltcorn/saltcorn-with-mobile:$VERSION
-
-docker build -t saltcorn/saltcorn:latest -f Dockerfile.release .
-docker push saltcorn/saltcorn:latest
-
-docker build -t saltcorn/saltcorn:$VERSION -f Dockerfile.release .
-docker push saltcorn/saltcorn:$VERSION
 
 
 docker build -t saltcorn/saltcorn:dev -f Dockerfile.dev .
