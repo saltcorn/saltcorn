a5781de10903b0346695804feae2b6bd1fbfafd5
Tom Nielsen <tanielsen@gmail.com>
2024-03-08 15:45:04 +0000

    agg fv tests

--- packages/saltcorn-data/tests/list.test.ts
+++ packages/saltcorn-data/tests/list.test.ts
@@ -573,6 +573,96 @@ describe("Misc List views", () => {
     const vres1 = await view.run({}, mockReqRes);
     expect(vres1).toContain("<td>pub:ak press</td>");
   });
+  it("aggregation with int fieldview config", async () => {
+    const view = await mkViewWithCfg({
+      configuration: {
+        layout: {
+          besides: [
+            {
+              contents: {
+                stat: "Max",
+                type: "aggregation",
+                block: false,
+                aggwhere: "",
+                agg_field: "id",
+                textStyle: "",
+                agg_relation: "patients.favbook",
+                agg_fieldview: "show_with_html",
+                configuration: {
+                  code: "MAX:{{it}}",
+                },
+              },
+              alignment: "Default",
+              col_width_units: "px",
+            },
+          ],
+          list_columns: true,
+        },
+        columns: [
+          {
+            stat: "Max",
+            type: "Aggregation",
+            block: false,
+            aggwhere: "",
+            agg_field: "id",
+            textStyle: "",
+            agg_relation: "patients.favbook",
+            agg_fieldview: "show_with_html",
+            configuration: {
+              code: "MAX:{{it}}",
+            },
+          },
+        ],
+      },
+    });
+    const vres1 = await view.run({}, mockReqRes);
+    expect(vres1).toContain("<td>MAX:2</td>");
+  });
+  it("aggregation with int fieldview config", async () => {
+    const view = await mkViewWithCfg({
+      configuration: {
+        layout: {
+          besides: [
+            {
+              contents: {
+                stat: "Max",
+                type: "aggregation",
+                block: false,
+                aggwhere: "",
+                agg_field: "name",
+                textStyle: "",
+                agg_relation: "patients.favbook",
+                agg_fieldview: "code",
+                configuration: {
+                  code: "MAX:{{it}}",
+                },
+              },
+              alignment: "Default",
+              col_width_units: "px",
+            },
+          ],
+          list_columns: true,
+        },
+        columns: [
+          {
+            stat: "Max",
+            type: "Aggregation",
+            block: false,
+            aggwhere: "",
+            agg_field: "name",
+            textStyle: "",
+            agg_relation: "patients.favbook",
+            agg_fieldview: "code",
+            configuration: {
+              code: "MAX:{{it}}",
+            },
+          },
+        ],
+      },
+    });
+    const vres1 = await view.run({}, mockReqRes);
+    expect(vres1).toContain("<code>Michael Douglas</code>");
+  });
   it("runs button action", async () => {
     const view = await mkViewWithCfg({
       configuration: {
