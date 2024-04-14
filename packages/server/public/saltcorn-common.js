c37c777c2ce6bc3141a4e4d9849f5544e6fc4ced
Christian Hugo <hugo.christian@web.de>
2024-04-14 08:39:40 +0200

    added sc-image attribute for mobile images in fixed html - set this to true and the app tries to load the src via ajax

--- packages/server/public/saltcorn-common.js
+++ packages/server/public/saltcorn-common.js
@@ -751,22 +751,33 @@ function initialize_page() {
       </form>`
       );
   });
-  $("[mobile-img-path]").each(async function () {
-    if (parent.loadEncodedFile) {
-      const fileId = $(this).attr("mobile-img-path");
-      const base64Encoded = await parent.loadEncodedFile(fileId);
-      this.src = base64Encoded;
-    }
-  });
-  $("[mobile-bg-img-path]").each(async function () {
-    if (parent.loadEncodedFile) {
-      const fileId = $(this).attr("mobile-bg-img-path");
-      if (fileId) {
+  if (!isNode) {
+    $("[mobile-img-path]").each(async function () {
+      if (parent.loadEncodedFile) {
+        const fileId = $(this).attr("mobile-img-path");
         const base64Encoded = await parent.loadEncodedFile(fileId);
-        this.style.backgroundImage = `url("${base64Encoded}")`;
+        this.src = base64Encoded;
       }
-    }
-  });
+    });
+    $("[sc-image]").each(async function () {
+      if (parent.loadEncodedFile) {
+        const src = $(this).attr("src");
+        const fileId = src.replace("/files/serve/", "");
+        const base64Encoded = await parent.loadEncodedFile(fileId);
+        this.src = base64Encoded;
+      }
+    });
+
+    $("[mobile-bg-img-path]").each(async function () {
+      if (parent.loadEncodedFile) {
+        const fileId = $(this).attr("mobile-bg-img-path");
+        if (fileId) {
+          const base64Encoded = await parent.loadEncodedFile(fileId);
+          this.style.backgroundImage = `url("${base64Encoded}")`;
+        }
+      }
+    });
+  }
 
   function setExplainer(that) {
     var id = $(that).attr("id") + "_explainer";
