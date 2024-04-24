dd7fb5ee2f7d36ab887d7ad9370d8d3f5b94a465
Christian Hugo <hugo.christian@web.de>
2024-04-24 11:02:43 +0200

    load spinner always centered, even with scroll

--- packages/saltcorn-mobile-app/www/js/utils/iframe_view_utils.js
+++ packages/saltcorn-mobile-app/www/js/utils/iframe_view_utils.js
@@ -889,10 +889,10 @@ function showLoadSpinner() {
     $("body").append(`
     <div 
       id="scspinner" 
-      style="position: absolute;
-        top: 0px;
-        width: 100%;
-        height: 100%;
+      style="position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
         z-index: 9999;"
     >
       <div 
