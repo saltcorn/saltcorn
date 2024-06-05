001106c22795c9867dcdf9f40c230b5437f82750
Tom Nielsen <tanielsen@gmail.com>
2024-06-05 17:17:34 +0100

    container click action using this

--- packages/saltcorn-data/base-plugin/viewtemplates/edit.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/edit.js
@@ -601,7 +601,7 @@ const transformForm = async ({
   await traverse(form.layout, {
     container(segment) {
       if (segment.click_action) {
-        segment.url = `javascript:view_post('${viewname}', 'run_action', {click_action: '${segment.click_action}', ...get_form_record({viewname: '${viewname}'}) })`;
+        segment.url = `javascript:view_post(this, 'run_action', {click_action: '${segment.click_action}', ...get_form_record(this) })`;
       }
     },
     async action(segment) {
