7f26d3237883284ecd7bc0a0224947ad29b3e6d1
Tom Nielsen <tanielsen@gmail.com>
2024-06-20 19:12:01 +0100

    working tree

--- packages/server/routes/registry.js
+++ packages/server/routes/registry.js
@@ -5,6 +5,7 @@ const { mkTable, link, post_btn } = require("@saltcorn/markup");
 const {
   script,
   domReady,
+  a,
   div,
   i,
   text,
@@ -12,12 +13,17 @@ const {
   input,
   label,
   form,
+  ul,
+  li,
+  details,
+  summary,
 } = require("@saltcorn/markup/tags");
 const Table = require("@saltcorn/data/models/table");
 const { isAdmin, error_catcher } = require("./utils");
 const moment = require("moment");
 const { send_infoarch_page } = require("../markup/admin.js");
-
+const View = require("@saltcorn/data/models/view");
+const Page = require("@saltcorn/data/models/page");
 /**
  * @type {object}
  * @const
@@ -34,6 +40,23 @@ router.get(
   "/",
   isAdmin,
   error_catcher(async (req, res) => {
+    const { etype, ename } = req.query;
+    let edContents = "";
+    const views = await View.find({}, { orderBy: "name", nocase: true });
+    const pages = await Page.find({}, { orderBy: "name", nocase: true });
+    const li_link = (etype1, ename1) =>
+      li(
+        a(
+          {
+            href: `/registry-editor?etype=${etype1}&ename=${encodeURIComponent(
+              ename1
+            )}`,
+            class: etype1 === etype && ename1 === ename ? "fw-bold" : undefined,
+          },
+          ename1
+        )
+      );
+    console.log({ p: etype === "page", v: etype === "view", etype });
     send_infoarch_page({
       res,
       req,
@@ -41,7 +64,32 @@ router.get(
       contents: [
         {
           type: "card",
-          contents: [],
+          contents: {
+            besides: [
+              {
+                type: "blank",
+                contents: ul(
+                  { class: "katetree" },
+                  li(
+                    details(
+                      { open: etype === "page" }, //
+                      summary("Pages"),
+                      ul(pages.map((p) => li_link("page", p.name)))
+                    )
+                  ),
+                  li(
+                    details(
+                      { open: etype === "view" },
+                      summary("Views"),
+                      ul(views.map((v) => li_link("view", v.name)))
+                    )
+                  )
+                ),
+              },
+              { type: "blank", contents: "editor here" },
+            ],
+            widths: etype && ename ? [2, 10] : [3, 9],
+          },
         },
       ],
     });
