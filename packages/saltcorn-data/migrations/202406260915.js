5f8f5fa6ae38e506d4a02571deb4800cecb16721
Tom Nielsen <tanielsen@gmail.com>
2024-06-26 10:21:49 +0100

    not required

--- packages/saltcorn-data/migrations/202406260915.js
+++ packages/saltcorn-data/migrations/202406260915.js
@@ -1,4 +1,4 @@
 const sql = `insert into _sc_fields(table_id, name, label, type, attributes, required, is_unique)
-       values((select id from _sc_tables where name='users'),'role_id','Role','Integer', '{}', true, false)`;
+       values((select id from _sc_tables where name='users'),'role_id','Role','Integer', '{}', false, false)`;
 
 module.exports = { sql };
