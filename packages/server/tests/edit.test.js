8ba44be8187cd88593d67def5ba4e037a97582f8
Tom Nielsen <tanielsen@gmail.com>
2024-07-22 11:05:47 +0100

    test for showif without joinfield

--- packages/server/tests/edit.test.js
+++ packages/server/tests/edit.test.js
@@ -38,171 +38,130 @@ beforeAll(async () => {
 
 jest.setTimeout(30000);
 
-describe("JSDOM-E2E edit test", () => {
-  it("join select should set dynamic where and show if with joinfield", async () => {
-    await View.create({
-      viewtemplate: "Edit",
-      description: "",
-      min_role: 100,
-      name: `AuthorEditForTest`,
-      table_id: Table.findOne("books")?.id,
-      default_render_page: "",
-      slug: {},
-      attributes: {},
-      configuration: {
-        layout: {
-          above: [
-            {
-              gx: null,
-              gy: null,
-              style: {
-                "margin-bottom": "1.5rem",
-              },
-              aligns: ["end", "start"],
-              widths: [2, 10],
-              besides: [
-                {
-                  font: "",
-                  type: "blank",
-                  block: false,
-                  style: {},
-                  inline: false,
-                  contents: "Publisher",
-                  labelFor: "publisher",
-                  isFormula: {},
-                  textStyle: "",
-                },
-                {
-                  above: [
-                    {
-                      type: "field",
-                      block: false,
-                      fieldview: "select",
-                      textStyle: "",
-                      field_name: "publisher",
-                      configuration: {},
-                    },
-                    {
-                      type: "container",
-                      style: {},
-                      bgType: "None",
-                      hAlign: "left",
-                      margin: [0, 0, 0, 0],
-                      rotate: 0,
-                      vAlign: "top",
-                      bgColor: "#ffffff",
-                      display: "block",
-                      padding: [0, 0, 0, 0],
-                      bgFileId: 0,
-                      contents: {
-                        font: "",
-                        icon: "",
-                        type: "blank",
-                        block: false,
-                        style: {},
-                        inline: false,
-                        contents: "Warning",
-                        labelFor: "",
-                        isFormula: {},
-                        textStyle: "",
-                      },
-                      imageSize: "contain",
-                      isFormula: {},
-                      minHeight: 0,
-                      textColor: "#ffffff",
-                      htmlElement: "div",
-                      showForRole: [],
-                      gradEndColor: "#88ff88",
-                      customClass: "pubwarn",
-                      setTextColor: false,
-                      fullPageWidth: false,
-                      gradDirection: "0",
-                      showIfFormula: 'publisher?.name == "AK Press"',
-                      gradStartColor: "#ff8888",
-                      minScreenWidth: "",
-                      show_for_owner: false,
-                    },
-                  ],
-                },
-              ],
-              breakpoints: ["", ""],
+const makeJoinSelectView = async ({ name, showIfFormula }) => {
+  await View.create({
+    viewtemplate: "Edit",
+    description: "",
+    min_role: 100,
+    name,
+    table_id: Table.findOne("books")?.id,
+    default_render_page: "",
+    slug: {},
+    attributes: {},
+    configuration: {
+      layout: {
+        above: [
+          {
+            gx: null,
+            gy: null,
+            style: {
+              "margin-bottom": "1.5rem",
             },
-            {
-              gx: null,
-              gy: null,
-              style: {
-                "margin-bottom": "1.5rem",
+            aligns: ["end", "start"],
+            widths: [2, 10],
+            besides: [
+              {
+                font: "",
+                type: "blank",
+                block: false,
+                style: {},
+                inline: false,
+                contents: "Publisher",
+                labelFor: "publisher",
+                isFormula: {},
+                textStyle: "",
               },
-              aligns: ["end", "start"],
-              widths: [2, 10],
-              besides: [
-                {
-                  font: "",
-                  type: "blank",
-                  block: false,
-                  style: {},
-                  inline: false,
-                  contents: "sequel_to",
-                  labelFor: "sequel_to",
-                  isFormula: {},
-                  textStyle: "",
-                },
-                {
-                  type: "field",
-                  block: false,
-                  fieldview: "select",
-                  textStyle: "",
-                  field_name: "sequel_to",
-                  configuration: {
-                    where: "publisher == $publisher",
+              {
+                above: [
+                  {
+                    type: "field",
+                    block: false,
+                    fieldview: "select",
+                    textStyle: "",
+                    field_name: "publisher",
+                    configuration: {},
                   },
+                  {
+                    type: "container",
+                    style: {},
+                    bgType: "None",
+                    hAlign: "left",
+                    margin: [0, 0, 0, 0],
+                    rotate: 0,
+                    vAlign: "top",
+                    bgColor: "#ffffff",
+                    display: "block",
+                    padding: [0, 0, 0, 0],
+                    bgFileId: 0,
+                    contents: {
+                      font: "",
+                      icon: "",
+                      type: "blank",
+                      block: false,
+                      style: {},
+                      inline: false,
+                      contents: "Warning",
+                      labelFor: "",
+                      isFormula: {},
+                      textStyle: "",
+                    },
+                    imageSize: "contain",
+                    isFormula: {},
+                    minHeight: 0,
+                    textColor: "#ffffff",
+                    htmlElement: "div",
+                    showForRole: [],
+                    gradEndColor: "#88ff88",
+                    customClass: "pubwarn",
+                    setTextColor: false,
+                    fullPageWidth: false,
+                    gradDirection: "0",
+                    showIfFormula,
+                    gradStartColor: "#ff8888",
+                    minScreenWidth: "",
+                    show_for_owner: false,
+                  },
+                ],
+              },
+            ],
+            breakpoints: ["", ""],
+          },
+          {
+            gx: null,
+            gy: null,
+            style: {
+              "margin-bottom": "1.5rem",
+            },
+            aligns: ["end", "start"],
+            widths: [2, 10],
+            besides: [
+              {
+                font: "",
+                type: "blank",
+                block: false,
+                style: {},
+                inline: false,
+                contents: "sequel_to",
+                labelFor: "sequel_to",
+                isFormula: {},
+                textStyle: "",
+              },
+              {
+                type: "field",
+                block: false,
+                fieldview: "select",
+                textStyle: "",
+                field_name: "sequel_to",
+                configuration: {
+                  where: "publisher == $publisher",
                 },
-              ],
-              breakpoints: ["", ""],
-            },
-            {
-              type: "action",
-              block: false,
-              rndid: "cb94bd",
-              nsteps: "",
-              minRole: 100,
-              isFormula: {},
-              action_icon: "",
-              action_name: "Save",
-              action_size: "",
-              action_bgcol: "",
-              action_label: "",
-              action_style: "btn-primary",
-              action_title: "",
-              configuration: {},
-              step_only_ifs: "",
-              action_textcol: "",
-              action_bordercol: "",
-              step_action_names: "",
-            },
-          ],
-        },
-        columns: [
-          {
-            type: "Field",
-            block: false,
-            fieldview: "select",
-            textStyle: "",
-            field_name: "publisher",
-            configuration: {},
+              },
+            ],
+            breakpoints: ["", ""],
           },
           {
-            type: "Field",
+            type: "action",
             block: false,
-            fieldview: "select",
-            textStyle: "",
-            field_name: "sequel_to",
-            configuration: {
-              where: "publisher == $publisher",
-            },
-          },
-          {
-            type: "Action",
             rndid: "cb94bd",
             nsteps: "",
             minRole: 100,
@@ -221,17 +180,71 @@ describe("JSDOM-E2E edit test", () => {
             step_action_names: "",
           },
         ],
-        viewname: "AuthorEditForTest",
-        auto_save: false,
-        split_paste: false,
-        exttable_name: null,
-        page_when_done: null,
-        view_when_done: "authorlist",
-        dest_url_formula: null,
-        destination_type: "View",
-        formula_destinations: [],
-        page_group_when_done: null,
       },
+      columns: [
+        {
+          type: "Field",
+          block: false,
+          fieldview: "select",
+          textStyle: "",
+          field_name: "publisher",
+          configuration: {},
+        },
+        {
+          type: "Field",
+          block: false,
+          fieldview: "select",
+          textStyle: "",
+          field_name: "sequel_to",
+          configuration: {
+            where: "publisher == $publisher",
+          },
+        },
+        {
+          type: "Action",
+          rndid: "cb94bd",
+          nsteps: "",
+          minRole: 100,
+          isFormula: {},
+          action_icon: "",
+          action_name: "Save",
+          action_size: "",
+          action_bgcol: "",
+          action_label: "",
+          action_style: "btn-primary",
+          action_title: "",
+          configuration: {},
+          step_only_ifs: "",
+          action_textcol: "",
+          action_bordercol: "",
+          step_action_names: "",
+        },
+      ],
+      viewname: "AuthorEditForTest",
+      auto_save: false,
+      split_paste: false,
+      exttable_name: null,
+      page_when_done: null,
+      view_when_done: "authorlist",
+      dest_url_formula: null,
+      destination_type: "View",
+      formula_destinations: [],
+      page_group_when_done: null,
+    },
+  });
+};
+
+const newEvent = (dom, type) =>
+  new dom.window.CustomEvent(type, {
+    bubbles: true,
+    cancelable: true,
+  });
+
+describe("JSDOM-E2E edit test", () => {
+  it("join select should set dynamic where and show if with joinfield", async () => {
+    await makeJoinSelectView({
+      name: "AuthorEditForTest",
+      showIfFormula: 'publisher?.name == "AK Press"',
     });
     const dom = await load_url_dom("/view/AuthorEditForTest");
     await sleep(1000);
@@ -248,12 +261,7 @@ describe("JSDOM-E2E edit test", () => {
     ]);
     const select = dom.window.document.querySelector("select[name=publisher]");
     select.value = "1";
-    select.dispatchEvent(
-      new dom.window.CustomEvent("change", {
-        bubbles: true,
-        cancelable: true,
-      })
-    );
+    select.dispatchEvent(newEvent(dom, "change"));
 
     await sleep(2000);
     expect([...select_seq.options].map((o) => o.text)).toStrictEqual([
@@ -263,14 +271,36 @@ describe("JSDOM-E2E edit test", () => {
     ]);
 
     expect(pubwarn.style.display).toBe("");
+  });
+  it("join select should set dynamic where and show if with no joinfield", async () => {
+    await makeJoinSelectView({
+      name: "AuthorEditForTest1",
+      showIfFormula: "publisher == 1",
+    });
+    const dom = await load_url_dom("/view/AuthorEditForTest1");
+    await sleep(1000);
+    const pubwarn = dom.window.document.querySelector("div.pubwarn");
 
-    /*input.value = "Leo";
-    input.dispatchEvent(new dom.window.Event("change"));
-    await sleep(2000);
-    expect(dom.window.location.href).toBe(
-      "http://localhost/view/authorfilter1?author=Leo"
+    expect(pubwarn.style.display).toBe("none");
+
+    const select_seq = dom.window.document.querySelector(
+      "select[name=sequel_to]"
     );
-*/
-    //console.log("dom", dom);
+    expect([...select_seq.options].map((o) => o.text)).toStrictEqual([
+      "",
+      "Herman Melville",
+    ]);
+    const select = dom.window.document.querySelector("select[name=publisher]");
+    select.value = "1";
+    select.dispatchEvent(newEvent(dom, "change"));
+
+    await sleep(2000);
+    expect([...select_seq.options].map((o) => o.text)).toStrictEqual([
+      "",
+      "Leo Tolstoy",
+      "Peter Kropotkin",
+    ]);
+
+    expect(pubwarn.style.display).toBe("");
   });
 });
