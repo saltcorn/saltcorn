f5b0f7faf017370d2f5166535ed48c21c272c989
Tom Nielsen <tanielsen@gmail.com>
2024-08-09 17:14:00 +0100

    finish role help

--- packages/server/auth/roleadmin.js
+++ packages/server/auth/roleadmin.js
@@ -200,7 +200,9 @@ router.get(
       sub2_page: "New",
       contents: {
         type: "card",
-        title: req.__("Roles"),
+        title:
+          req.__("Roles") +
+          `<a href="javascript:ajax_modal('/admin/help/User%20roles?')"><i class="fas fa-question-circle ms-1"></i></a>`,
         contents: [renderForm(form, req.csrfToken())],
       },
     });
