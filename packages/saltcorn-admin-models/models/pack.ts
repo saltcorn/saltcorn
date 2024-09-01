709e8e12cd9e15621eadbf606f547f7dd42377f0
Tom Nielsen <tanielsen@gmail.com>
2024-09-01 18:50:30 +0100

    create pack

--- packages/saltcorn-admin-models/models/pack.ts
+++ packages/saltcorn-admin-models/models/pack.ts
@@ -24,7 +24,7 @@ import ModelInstance from "@saltcorn/data/models/model_instance";
 import EventLog, { EventLogCfg } from "@saltcorn/data/models/eventlog";
 import User from "@saltcorn/data/models/user";
 import config from "@saltcorn/data/models/config";
-import type { Pack } from "@saltcorn/types/base_types";
+import type { CodePagePack, Pack } from "@saltcorn/types/base_types";
 import type { PagePack } from "@saltcorn/types/model-abstracts/abstract_page";
 const { save_menu_items } = config;
 import type Plugin from "@saltcorn/data/models/plugin";
@@ -830,6 +830,22 @@ const fetch_pack_by_name = async (
   else return null;
 };
 
+const code_pages_from_tag = async (tag: Tag): Promise<Array<CodePagePack>> => {
+  const function_code_pages_tags: { string: [string] } =
+    getState().getConfigCopy("function_code_pages_tags", {});
+  const function_code_pages = getState().getConfigCopy(
+    "function_code_pages",
+    {}
+  );
+
+  const cps: Array<CodePagePack> = [];
+  Object.entries(function_code_pages_tags || {}).forEach(([cpname, tags]) => {
+    const code = function_code_pages[cpname];
+    if (tags.includes(tag.name) && code) cps.push({ name: cpname, code, tags });
+  });
+  return cps;
+};
+
 const create_pack_from_tag = async (tag: Tag): Promise<any> => {
   const pack: Pack = {
     tables: [],
@@ -844,6 +860,7 @@ const create_pack_from_tag = async (tag: Tag): Promise<any> => {
     models: [],
     model_instances: [],
     event_logs: [],
+    code_pages: [],
   };
   const tables = await tag.getTables();
   for (const t of tables) pack.tables.push(await table_pack(t));
@@ -854,6 +871,7 @@ const create_pack_from_tag = async (tag: Tag): Promise<any> => {
   const triggers = await tag.getTriggers();
   for (const t of triggers) pack.triggers.push(await trigger_pack(t));
   pack.tags.push(await tag_pack(tag));
+  pack.code_pages = await code_pages_from_tag(tag);
   return pack;
 
   //TODO add models, plugins
