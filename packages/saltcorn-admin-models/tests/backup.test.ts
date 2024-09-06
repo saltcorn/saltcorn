c89d9d83edf5cdd7cf724f8df0402b1703007412
Tom Nielsen <tanielsen@gmail.com>
2024-09-06 09:34:06 +0100

    test system zip

--- packages/saltcorn-admin-models/tests/backup.test.ts
+++ packages/saltcorn-admin-models/tests/backup.test.ts
@@ -149,4 +149,127 @@ describe("Backup and restore", () => {
 
     expect(staff.checkPassword("ghrarhr54hg")).toBe(true);
   });
+  it("should create and restore backup with system zip", async () => {
+    await require("@saltcorn/data/db/reset_schema")();
+    await require("@saltcorn/data/db/fixtures")();
+    await setConfig("site_name", "backups rule!");
+    await setConfig("backup_with_system_zip", true);
+    await setConfig("backup_system_zip_level", 1);
+    await setConfig("menu_items", [
+      {
+        type: "Page",
+        label: "a_page",
+        min_role: 100,
+        pagename: "a_page",
+      },
+      {
+        url: "https://www.bbc.co.uk/news",
+        icon: "undefined",
+        text: "BBC",
+        type: "Link",
+        label: "BBC",
+        style: "",
+        location: "Standard",
+        min_role: 100,
+      },
+    ]);
+    const sn1 = await getConfig("site_name");
+    expect(sn1).toBe("backups rule!");
+    await Role.create({ role: "paid", id: 60 });
+    await Table.create("myblanktable", { min_role_read: 60 });
+    const vtbl = await Table.create("myversionedtable", {
+      min_role_read: 80,
+      versioned: true,
+    });
+    await Field.create({
+      name: "name",
+      type: "String",
+      table: vtbl,
+    });
+    await vtbl.insertRow({ name: "Fred" });
+    await vtbl.updateRow({ name: "Sam" }, 1);
+
+    await Trigger.create({
+      name: "footrig",
+      table_id: 1,
+      when_trigger: "Insert",
+      action: "run_js_code",
+      configuration: { code: "console.log('new user')" },
+    });
+    await Trigger.create({
+      name: "hourtrig",
+      when_trigger: "Hourly",
+      action: "run_js_code",
+      configuration: { code: "console.log('cuckoo')" },
+    });
+    await Library.create({
+      name: "foo",
+      icon: "fa-bar",
+      layout: { baz: "bar" },
+    });
+
+    const fnm = await create_backup();
+    const t1 = Table.findOne({ name: "books" });
+    assertIsSet(t1);
+    const t1c = await t1.countRows();
+    const v1 = await View.find();
+    expect(!!t1).toBe(true);
+
+    await reset();
+    const admu = await User.create({
+      email: "admin@foo.com",
+      password: "AhGGr6rhu45",
+      role_id: 1,
+    });
+    assertsObjectIsUser(admu);
+    expect(typeof admu.password).toBe("string");
+
+    const t2 = Table.findOne({ name: "books" });
+    expect(t2).toBe(null);
+    const sn0 = await getConfig("site_name");
+    expect(sn0).toBe("Saltcorn");
+    const menus0 = await getConfig("menu_items", []);
+    expect(menus0.length).toBe(0);
+    const restore_res = await restore(fnm, (p) => {});
+    await unlink(fnm);
+    expect(restore_res).toBe(undefined);
+    const t3 = Table.findOne({ name: "books" });
+    assertIsSet(t3);
+    expect(!!t3).toBe(true);
+    const t5 = Table.findOne({ name: "myblanktable" });
+    assertIsSet(t5);
+    const t6 = Table.findOne({ name: "myversionedtable" });
+    assertIsSet(t6);
+    const vhist = await t6.get_history();
+    expect(vhist.length).toBe(2);
+
+    expect(!!t5).toBe(true);
+    expect(t5.min_role_read).toBe(60);
+    const t3c = await t3.countRows();
+    expect(t1c).toBe(t3c);
+    const v2 = await View.find();
+    expect(v1.length).toBe(v2.length);
+    const sn = await getConfig("site_name");
+    expect(sn).toBe("backups rule!");
+    const menus = await getConfig("menu_items");
+    expect(menus.length).toBe(2);
+    expect(menus[0].type).toBe("Page");
+    expect(menus[0].pagename).toBe("a_page");
+    expect(menus[1].type).toBe("Link");
+    expect(menus[1].url).toBe("https://www.bbc.co.uk/news");
+
+    await t3.insertRow({ author: "Marcus Rediker", pages: 224 });
+    const staff = await User.findOne({ email: "staff@foo.com" });
+    expect(!!staff).toBe(true);
+    assertsObjectIsUser(staff);
+    expect(typeof staff.password).toBe("string");
+    const trig = await Trigger.findOne({ name: "footrig" });
+    expect(!!trig).toBe(true);
+    const htrig = await Trigger.findOne({ name: "hourtrig" });
+    expect(!!htrig).toBe(true);
+    const lib = await Library.findOne({ name: "foo" });
+    expect(!!lib).toBe(true);
+
+    expect(staff.checkPassword("ghrarhr54hg")).toBe(true);
+  });
 });
