56e31c9af839c02c32ae24aa90fd840a61f4c3c9
christianhugoch <hugo.christian@web.de>
2024-09-24 21:28:24 +0200

    Tags in install dialog (#2667)
    * tags selector in the plugin_versions dialog
    - when a tag gets selected, it sets the value of the versions selector
    - when the version selector changes, it resets the tag selector
    
    * check version supported for latest plugins upgrade
    - adds one more npm registry fetch
    
    * use cache for isVersionSupported() otherwise, search a supported version with forceFetch
    
    ---------
    
    Co-authored-by: Tom Nielsen <tanielsen@gmail.com>

--- packages/plugins-loader/stable_versioning.js
+++ packages/plugins-loader/stable_versioning.js
@@ -25,7 +25,7 @@ const doCheck = (pluginVersion, versionInfos, scVersion) => {
  * @param pluginVersion - wanted version
  * @param versionInfos - version infos from the npm registry (resembles the package.json version).
  *                            Here you'll find the engines.saltcorn property.
- * @param scVersion - saltcorn version
+ * @param scVersion - optional saltcorn version (if not set it will be taken from the state)
  * @returns
  */
 const supportedVersion = (
@@ -35,14 +35,15 @@ const supportedVersion = (
 ) => {
   const resolved =
     pluginVersion === "latest" ? resolveLatest(versionInfos) : pluginVersion;
-  if (doCheck(resolved, versionInfos, scVersion)) return resolved;
+  const safeScVersion = scVersion || getState().scVersion;
+  if (doCheck(resolved, versionInfos, safeScVersion)) return resolved;
   else {
     const keys = Object.keys(versionInfos);
     keys.sort((a, b) => semver.rcompare(b, a));
     // iterate in reverse order to get the latest version
     for (let i = keys.length - 1; i >= 0; i--) {
       const version = keys[i];
-      if (doCheck(version, versionInfos, scVersion)) return version;
+      if (doCheck(version, versionInfos, safeScVersion)) return version;
     }
     return null;
   }
@@ -54,21 +55,21 @@ const supportedVersion = (
  * @param scEngine - version infos from the npm registry (resembles the package.json version)
  *                        Here you'll find the engines.saltcorn property.
  *                        If versionInfos is a string, it will be treated as the engines.saltcorn property.
- * @param scVersion - optional saltcorn version (if not set it will be taken from the package.json)
+ * @param scVersion - optional saltcorn version (if not set it will be taken from the state)
  */
 const isVersionSupported = (pluginVersion, scEngine, scVersion) => {
   const safeInfos =
     typeof scEngine === "string"
       ? { [pluginVersion]: { engines: scEngine } }
       : scEngine;
-  const safeScVersion = scVersion || require("package.json").version;
+  const safeScVersion = scVersion || getState().scVersion;
   return doCheck(pluginVersion, safeInfos, safeScVersion);
 };
 
 /**
  * check if 'scVersion' fullfilles 'scEngine'
  * @param scEngine fixed version or range of saltcorn versions (e.g. ">=1.0.0")
- * @param scVersion optional saltcorn version (if not set it will be taken from the package.json)
+ * @param scVersion optional saltcorn version (if not set it will be taken from the state)
  * @returns true if the saltcorn version fullfilles scEngine
  */
 const isEngineSatisfied = (scEngine, scVersion) => {
@@ -77,7 +78,7 @@ const isEngineSatisfied = (scEngine, scVersion) => {
     getState().log(4, `invalid engine property: ${scEngine}`);
     return true;
   }
-  const safeScVersion = scVersion || require("package.json").version;
+  const safeScVersion = scVersion || getState().scVersion;
   if (semver.valid(safeScVersion) === null) {
     getState().log(4, `invalid saltcorn version: ${scVersion}`);
     return true;
@@ -87,7 +88,7 @@ const isEngineSatisfied = (scEngine, scVersion) => {
 
 /**
  * change latest to the actual version
- * @param versionInfos - version infos from the npm registry (resembles the package.json version)
+ * @param versionInfos - version infos from the npm registry
  */
 const resolveLatest = (versionInfos) => {
   const keys = Object.keys(versionInfos);
