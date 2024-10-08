66fa9c75ee80364254823f7f87d26e7f8f92515a
Tom Nielsen <tanielsen@gmail.com>
2024-10-08 21:51:39 +0100

    Provided table filtering: full text search and equality on bounds (#2706)
    * provided table filtering: fix gt/lt with equality
    
    * full text search

--- packages/saltcorn-data/plugin-helper.js
+++ packages/saltcorn-data/plugin-helper.js
@@ -2333,7 +2333,13 @@ const json_list_to_external_table = (get_json_list, fields0) => {
       (x) =>
       ([k, v]) => {
         if (Array.isArray(v)) return v.every((v1) => sat(x)([k, v1]));
+        else if (k === "_fts")
+          return JSON.stringify(x)
+            .toLowerCase()
+            .includes((v.searchTerm || "").toLowerCase());
+        else if (v?.lt && v?.equal) return x[k] <= +v.lt;
         else if (v?.lt) return x[k] < +v.lt;
+        else if (v?.gt && v?.equal) return x[k] >= +v.gt;
         else if (v?.gt) return x[k] > +v.gt;
         else if (v?.ilike) return (x[k] || "").includes(v.ilike);
         else return x[k] == v;
