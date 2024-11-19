40ba1356a6847284e1743c6a111e941a984a3f09
Tom Nielsen <tanielsen@gmail.com>
2024-11-19 16:02:50 +0000

    class on columns in builder

--- deploy/playwright/pageobject/locators.js
+++ deploy/playwright/pageobject/locators.js
@@ -382,8 +382,8 @@ class PageLocators {
     this.searchIconLocator = 'input.rfipsearch__input[placeholder="Search Icons"]';
     this.editIconLocator = 'i.far.fa-edit';
     this.showeditLink = 'a.btn.btn-primary:has(i.far.fa-edit)'
-    this.secondrowcolumn = '.row:nth-of-type(2) .split-col:nth-of-type(2) .canvas';
-    this.secondrowcolumn1 = '.row:nth-of-type(2) .split-col:nth-of-type(1) .canvas';
+    this.secondrowcolumn = '.builder-columns.row:nth-of-type(2) .split-col:nth-of-type(2) .canvas';
+    this.secondrowcolumn1 = '.builder-columns.row:nth-of-type(2) .split-col:nth-of-type(1) .canvas';
     this.optioninput = 'input[name="options"]';
     this.column5 = '.d-flex.justify-content-between.h-100 >> text=Column 5';
     this.headerlabel = 'input.form-control[value=""]';
