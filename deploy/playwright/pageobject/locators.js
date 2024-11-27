a6ed4bd9476f75a4107246bb434ea7ecc6fab5fc
Tom Nielsen <tanielsen@gmail.com>
2024-11-27 13:59:56 +0000

    table url

--- deploy/playwright/pageobject/locators.js
+++ deploy/playwright/pageobject/locators.js
@@ -609,7 +609,7 @@ class PageLocators {
     this.tableclick = baseURL+'/table';
     this.viewclick = baseURL+'/viewedit';
     this.pageclick = baseURL+'pageedit';
-    this.create_CSV =baseURL+'/create-from-csv';
+    this.create_CSV =baseURL+'/table/create-from-csv';
     this.admin_Clear_All ='admin/clear-all';
     this.admin_Build_Mobile_App = 'Page url should be /admin/build-mobile-app';
     this.plugins_Set_All ='Page url should be /plugins?set=all';
