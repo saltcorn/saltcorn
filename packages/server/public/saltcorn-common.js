d55c2dcf3ce1249bdf2f475783eb2b9c7dee74d0
Tom Nielsen <tanielsen@gmail.com>
2024-12-01 20:35:49 +0000

    fix codemirror when not dark

--- packages/server/public/saltcorn-common.js
+++ packages/server/public/saltcorn-common.js
@@ -1064,12 +1064,12 @@ function initialize_page() {
         codes.forEach((el) => {
           //console.log($(el).attr("mode"), el);
           if ($(el).hasClass("codemirror-enabled")) return;
-
-          const cm = CodeMirror.fromTextArea(el, {
+          const cmOpts = {
             lineNumbers: true,
             mode: $(el).attr("mode"),
-            theme: _sc_lightmode === "dark" ? "blackboard" : undefined,
-          });
+          };
+          if (_sc_lightmode === "dark") cmOpts.theme = "blackboard";
+          const cm = CodeMirror.fromTextArea(el, cmOpts);
           $(el).addClass("codemirror-enabled");
           cm.on(
             "change",
