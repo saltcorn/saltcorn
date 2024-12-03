0b12fd047da49f9256f75fb1a50386a18ff46f3f
Tom Nielsen <tanielsen@gmail.com>
2024-12-03 16:51:46 +0000

    reduce imports in config route

--- packages/server/routes/config.js
+++ packages/server/routes/config.js
@@ -5,30 +5,11 @@
  */
 const Router = require("express-promise-router");
 
-const Field = require("@saltcorn/data/models/field");
-const File = require("@saltcorn/data/models/file");
-const Table = require("@saltcorn/data/models/table");
-const View = require("@saltcorn/data/models/view");
-const Form = require("@saltcorn/data/models/form");
-const { isAdmin, setTenant, error_catcher } = require("./utils.js");
+
+const { isAdmin, error_catcher } = require("./utils.js");
 const { getState } = require("@saltcorn/data/db/state");
 
-const {
-  mkTable,
-  renderForm,
-  link,
-  post_btn,
-  post_delete_btn,
-} = require("@saltcorn/markup");
-const {
-  getConfig,
-  setConfig,
-  getAllConfigOrDefaults,
-  deleteConfig,
-  configTypes,
-  isFixedConfig,
-} = require("@saltcorn/data/models/config");
-const { table, tbody, tr, th, td, div } = require("@saltcorn/markup/tags");
+
 
 /**
  * @type {object}
