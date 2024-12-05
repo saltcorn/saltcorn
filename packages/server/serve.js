17be034f279a2d55e0135a0ccb1cd47d01cf99cb
Christian Hugo <hugo.christian@web.de>
2024-12-05 19:37:13 +0100

    no force in tenants - could overwrite the version of another tenant or the admin - and causes sometimes 403 codes whild downloading the tarball

--- packages/server/serve.js
+++ packages/server/serve.js
@@ -121,11 +121,7 @@ const initMaster = async ({ disableMigrate }, useClusterAdaptor = true) => {
   if (getState().getConfig("log_sql", false)) db.set_sql_logging();
   if (db.is_it_multi_tenant()) {
     const tenants = await getAllTenants();
-    await init_multi_tenant(
-      async () => await loadAllPlugins(true),
-      disableMigrate,
-      tenants
-    );
+    await init_multi_tenant(loadAllPlugins, disableMigrate, tenants);
   }
   eachTenant(async () => {
     const state = getState();
