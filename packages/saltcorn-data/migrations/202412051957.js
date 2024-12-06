9ceacc93814261cce1b184b7cd22a9c10c109141
Tom Nielsen <tanielsen@gmail.com>
2024-12-06 13:51:37 +0000

    runs in migration

--- packages/saltcorn-data/migrations/202412051957.js
+++ packages/saltcorn-data/migrations/202412051957.js
@@ -1,4 +1,4 @@
-const sql_pg = `CREATE TABLE IF NOT EXISTS  _sc_workflow_steps (
+const sql_pg = `CREATE TABLE IF NOT EXISTS _sc_workflow_steps (
     id serial primary key,
     name text NOT NULL,
     trigger_id integer references _sc_triggers(id),
@@ -6,6 +6,17 @@ const sql_pg = `CREATE TABLE IF NOT EXISTS  _sc_workflow_steps (
     action_name text NOT NULL,
     initial_step boolean,
     configuration jsonb
+);
+
+CREATE TABLE IF NOT EXISTS _sc_workflow_runs (
+    id serial primary key,  
+    trigger_id integer references _sc_triggers(id),
+    context jsonb NOT NULL,
+    wait_info jsonb,
+    started_at timestamp not null,
+    started_by_id int references users(id) not null,
+    error text,
+    status text,
 );`;
 
 const sql_sqlite = `CREATE TABLE _sc_workflow_steps (
@@ -16,6 +27,17 @@ const sql_sqlite = `CREATE TABLE _sc_workflow_steps (
     action_name text NOT NULL,
     initial_step boolean,
     configuration json
+);
+
+CREATE TABLE IF NOT EXISTS _sc_workflow_runs (
+    id serial primary key,  
+    trigger_id integer references _sc_triggers(id),
+    context json NOT NULL,
+    wait_info json,
+    started_at timestamp not null,
+    started_by_id int references users(id) not null,
+    error text,
+    status text,
 );`;
 
 module.exports = { sql_pg, sql_sqlite };
