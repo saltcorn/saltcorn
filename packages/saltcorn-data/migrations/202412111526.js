ab821084d5acc1d759270c8c77f0c66afb670ff2
Tom Nielsen <tanielsen@gmail.com>
2024-12-11 19:35:28 +0000

    Workflows: trace, status_updated_at, name unique (#2910)
    * migration
    
    * status_updated_at implement
    
    * session id variable
    
    * trace model
    
    * setting
    
    * tracing basics
    
    * tracing works
    
    * label
    
    * test run redirects to run page
    
    * traces test
    
    * fix sqlite migration
    
    * session in sqlite
    
    * user_id in sqlite migration

---
+++ packages/saltcorn-data/migrations/202412111526.js
@@ -0,0 +1,38 @@
+const sql_pg = `
+alter table _sc_workflow_runs add column status_updated_at timestamp;
+alter table _sc_workflow_runs add column session_id text;
+
+ALTER TABLE _sc_workflow_steps ADD CONSTRAINT workflow_steps_name_uniq UNIQUE (trigger_id, name);
+
+CREATE TABLE IF NOT EXISTS _sc_workflow_trace (
+    id serial primary key,  
+    run_id integer references _sc_workflow_runs(id) on delete cascade,
+    user_id integer references users(id) on delete cascade,
+    step_name_run text NOT NULL,    
+    context jsonb NOT NULL,
+    status text,    
+    wait_info jsonb,
+    step_started_at timestamp not null,
+    elapsed double precision,
+    error text
+);`;
+
+const sql_sqlite = [
+  `alter table _sc_workflow_runs add column status_updated_at timestamp;`,
+  `alter table _sc_workflow_runs add column session_id text;`,
+  `create unique index workflow_steps_name_uniq on _sc_workflow_steps(trigger_id, name);`,
+  `CREATE TABLE IF NOT EXISTS _sc_workflow_trace (
+    id serial primary key,  
+    run_id integer references _sc_workflow_runs(id) on delete cascade,
+    user_id integer references users(id) on delete cascade,
+    step_name_run text NOT NULL,    
+    context json NOT NULL,
+    status text,    
+    wait_info json,
+    step_started_at timestamp not null,
+    elapsed double precision,
+    error text
+);`,
+];
+
+module.exports = { sql_pg, sql_sqlite };
--- packages/saltcorn-types/model-abstracts/abstract_workflow_run.ts
+++ packages/saltcorn-types/model-abstracts/abstract_workflow_run.ts
@@ -4,8 +4,10 @@ export type WorkflowRunCfg = {
   context?: any;
   wait_info?: any;
   started_at?: Date;
+  status_updated_at?: Date;
   started_by?: number;
   error?: string;
+  session_id?: string;
   status?: "Pending" | "Running" | "Finished" | "Waiting" | "Error";
   current_step?: string;
 };
