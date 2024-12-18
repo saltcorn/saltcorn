bc4203d6444d1fd656b5c50040c7cf9f9803d3d3
Christian Hugo <hugo.christian@web.de>
2024-12-18 18:41:33 +0100

    option to pull it from docker hub

--- packages/saltcorn-cli/src/commands/build-capacitor-builder.js
+++ packages/saltcorn-cli/src/commands/build-capacitor-builder.js
@@ -10,31 +10,40 @@ const { spawnSync } = require("child_process");
 class BuildCapacitorBuilder extends Command {
   async run() {
     const { flags } = await this.parse(BuildCapacitorBuilder);
-    const dockerDir = join(
-      require.resolve("@saltcorn/mobile-builder"),
-      "..",
-      "..",
-      "docker"
-    );
-    const dArgs = ["build", dockerDir, "--network", "host"];
-    if (flags.buildClean) dArgs.push("--no-cache");
-    dArgs.push(
-      "-f",
-      join(dockerDir, "Dockerfile"),
-      "-t",
-      "saltcorn/capacitor-builder",
-      "--progress=plain"
-    );
-    const result = spawnSync("docker", dArgs, { cwd: ".", stdio: "inherit" });
-    if (result.error) console.log(result.error.toString());
+    if (flags.pullFromHub) {
+      const result = spawnSync(
+        "docker",
+        ["pull", "saltcorn/capacitor-builder"],
+        { cwd: ".", stdio: "inherit" }
+      );
+      if (result.error) console.log(result.error.toString());
+    } else {
+      const dockerDir = join(
+        require.resolve("@saltcorn/mobile-builder"),
+        "..",
+        "..",
+        "docker"
+      );
+      const dArgs = ["build", dockerDir, "--network", "host"];
+      if (flags.buildClean) dArgs.push("--no-cache");
+      dArgs.push(
+        "-f",
+        join(dockerDir, "Dockerfile"),
+        "-t",
+        "saltcorn/capacitor-builder",
+        "--progress=plain"
+      );
+      const result = spawnSync("docker", dArgs, { cwd: ".", stdio: "inherit" });
+      if (result.error) console.log(result.error.toString());
+    }
   }
 }
 
 BuildCapacitorBuilder.description =
-  "Build the 'saltcorn/capacitor-builder' docker image";
+  "Build the 'saltcorn/capacitor-builder' docker image or pull it from docker hub.";
 
 BuildCapacitorBuilder.help =
-  "Build the 'saltcorn/capacitor-builder' docker image. " +
+  "Build or pull the 'saltcorn/capacitor-builder' docker image. " +
   "This image is used in the 'build-app' command to run the capacitor commands. " +
   "Please make sure docker is callable without using root (see rootless mode, or add the user to the docker group).";
 
@@ -43,6 +52,11 @@ BuildCapacitorBuilder.flags = {
     name: "build clean",
     string: "clean",
     description: "run a clean build with --no-cache",
+    default: false,
+  }),
+  pullFromHub: Flags.boolean({
+    name: "pull from docker hub",
+    description: "pull the image from docker hub",
     default: false,
   }),
 };
