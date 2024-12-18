7c08a3f348ac2d3b8df957bf5cc9552b131abcda
Tom Nielsen <tanielsen@gmail.com>
2024-12-18 17:06:33 +0000

    Revert "description in view settings accordion"
    This reverts commit b0c51d701eb12a5aae924417628160d73296af85.

--- packages/server/routes/viewedit.js
+++ packages/server/routes/viewedit.js
@@ -196,7 +196,6 @@ const viewForm = async (req, tableOptions, roles, pages, values) => {
         label: req.__("Description"),
         name: "description",
         type: "String",
-        tab: "View settings",
         sublabel: req.__(
           "Description allows you to give more information about the view."
         ),
