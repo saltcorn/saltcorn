b2eee1395cec4857492d89035f2f13c9aa7c2197
Tom Nielsen <tanielsen@gmail.com>
2025-02-03 16:59:53 +0000

    possibly implement

--- packages/saltcorn-data/models/table.ts
+++ packages/saltcorn-data/models/table.ts
@@ -80,6 +80,7 @@ const { text } = tags;
 
 import type { AbstractTag } from "@saltcorn/types/model-abstracts/abstract_tag";
 import type {
+  FieldLike,
   JoinFieldOption,
   RelationOption,
 } from "@saltcorn/types/base_types";
@@ -3836,6 +3837,31 @@ where table_schema = '${db.getTenantSchema() || "public"}'
         values($1,'id','ID','${pk_type}', '{}', true, true, true) returning id`,
       [this.id]
     );
+  }
+
+  async move_include_fts_to_search_context() {
+    const include_fts_fields = this.fields.filter(
+      (f: Field) => f.attributes?.include_fts
+    );
+    if (!include_fts_fields.length) return;
+    let expressions: string[] = [];
+    for (const ftsfield of include_fts_fields)
+      expressions.push(
+        `${ftsfield.name}?.${ftsfield?.attributes?.summary_field || "id"}`
+      );
+    const field = await Field.create({
+      table: this,
+      label: "Search context",
+      type: "String",
+      calculated: true,
+      expression: expressions.join(" + "),
+      stored: true,
+    });
+    this.fields.push(field);
+    for (const ftsfield of include_fts_fields)
+      await field.update({
+        attributes: { ...ftsfield.attributes, include_fts: false },
+      });
   }
 }
 
