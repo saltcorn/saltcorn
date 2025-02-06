3a0fa1b8476cd34fd7f51b0399a3a77c2ee4cdde
Tom Nielsen <tanielsen@gmail.com>
2025-02-06 21:21:27 +0000

    fix e2e test?

--- deploy/playwright/pageobject/locators.js
+++ deploy/playwright/pageobject/locators.js
@@ -549,7 +549,7 @@ class PageLocators {
     this.textInputLabel = 'input.viewlink-label.form-control[type="text"]';
     this.popupcheckbox = 'input.form-check-input[type="checkbox"][name="block"]';
     this.showTeamLink = 'a[href*="Show_Team?id=1"]';
-    this.showTeamspan = 'span.btn.btn-secondary.btn-sm.is-builder-link:has-text("Show team")';
+    this.showTeamspan = 'span.is-builder-link:has-text("Show team")';
     this.IDDivLocator = 'div.col-2.text-end:has-text("ID")';
     this.view2tasklist = 'text=TaskList [List] Task';
     this.listOfAssignedTasks = 'a:has-text("List of assigned tasks")';
