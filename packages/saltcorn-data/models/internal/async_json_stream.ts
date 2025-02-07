439baa09e45729db869e4ce1920ff72344f4ab5b
Tom Nielsen <tanielsen@gmail.com>
2025-02-07 22:34:17 +0000

    try async stream

---
+++ packages/saltcorn-data/models/internal/async_json_stream.ts
@@ -0,0 +1,35 @@
+// https://stackoverflow.com/a/37041949/19839414
+// https://github.com/uhop/stream-json/issues/82#issuecomment-685289413
+
+const { Writable } = require("stream");
+const { chain } = require("stream-chain");
+const { parser } = require("stream-json");
+const fs = require("fs");
+
+type AsyncConsumer<T> = (subString: T) => Promise<void>;
+
+export async function async_json_stream<T>(
+  filename: string,
+  asyncF: AsyncConsumer<T>
+): Promise<void> {
+  class WriteToDB extends Writable {
+    constructor(options: any) {
+      super({ ...options, objectMode: true });
+    }
+
+    write(chunk: any, encoding: any, callback: any) {
+      // this is the stuff you need:
+      asyncF(chunk)
+        .then(() => callback(null))
+        .catch((error) => callback(error));
+    }
+  }
+  const writerToDB = new WriteToDB({});
+
+  const pipeline = chain([fs.createReadStream(filename), parser(), writerToDB]);
+  return await new Promise((resolve, reject) => {
+    pipeline.on("end", () => {
+      resolve();
+    });
+  });
+}
