4fae8cdaf7ba79787fc1c25b37aa86167a153dc0
Tom Nielsen <tanielsen@gmail.com>
2025-03-07 13:55:20 +0000

    migration

---
+++ packages/saltcorn-data/migrations/202503071353.js
@@ -0,0 +1,8 @@
+const sql_pg =
+  "alter table _sc_workflow_runs drop constraint _sc_workflow_runs_started_by_fkey, add constraint _sc_workflow_runs_started_by_fkey foreign key (started_by) references users (id) on delete set null;";
+
+const sql_sqlite = [
+  "alter table _sc_workflow_runs drop constraint _sc_workflow_runs_started_by_fkey, add constraint _sc_workflow_runs_started_by_fkey foreign key (started_by) references users (id) on delete set null;",
+];
+
+module.exports = { sql_pg, sql_sqlite };
