514d86b1611f8dd9c21824a58817e8d0669b8c85
Tom Nielsen <tanielsen@gmail.com>
2025-03-21 09:35:11 +0000

    List: Fix pagination when using row inclusion and exclusion formulae (#3126)
    * rm some ids from list
    
    * en
    
    * list: take inclusion and exclusion formulae into account for row count for pagination

--- packages/saltcorn-data/base-plugin/viewtemplates/list.js
+++ packages/saltcorn-data/base-plugin/viewtemplates/list.js
@@ -905,6 +905,7 @@ const run = async (
   const table = Table.findOne(
     typeof table_id === "string" ? { name: table_id } : { id: table_id }
   );
+  const pk_name = table.pk_name;
   const fields = table.getFields();
   const appState = getState();
   const locale = extraOpts.req.getLocale();
@@ -937,9 +938,9 @@ const run = async (
   await set_join_fieldviews({ table, columns, fields });
 
   readState(stateWithId, fields, extraOpts.req);
-  const id = stateWithId.id;
+  const id = stateWithId[pk_name];
   let state = { ...stateWithId };
-  if (extraOpts?.removeIdFromstate) delete state.id;
+  if (extraOpts?.removeIdFromstate) delete state[pk_name];
 
   const statehash = hashState(state, viewname);
 
@@ -990,12 +991,12 @@ const run = async (
         case RelationType.OWN:
           stateMany = {
             or: rows.map((row) => ({
-              [table.pk_name]: row[table.pk_name],
+              [pk_name]: row[pk_name],
               ...get_extra_state(row),
             })),
           };
           getRowState = (row) => ({
-            [table.pk_name]: row[table.pk_name],
+            [pk_name]: row[pk_name],
             ...get_extra_state(row),
           });
           break;
@@ -1034,7 +1035,7 @@ const run = async (
     if (this.viewtemplateObj?.runMany) {
       const runs = await view.runMany(stateMany, extraOpts);
       viewResults[segment.view + segment.relation] = (row) =>
-        runs.find((rh) => rh.row[table.pk_name] == row[table.pk_name])?.html;
+        runs.find((rh) => rh.row[pk_name] == row[pk_name])?.html;
     } else if (this.viewtemplateObj?.renderRows) {
       const rendered = await view.viewtemplateObj.renderRows(
         view.table,
@@ -1051,7 +1052,7 @@ const run = async (
             html,
             row: rows[ix],
           }))
-          .find((rh) => rh.row[table.pk_name] == row[table.pk_name])?.html;
+          .find((rh) => rh.row[pk_name] == row[pk_name])?.html;
     } else {
       const results = [];
 
@@ -1063,7 +1064,7 @@ const run = async (
         });
       }
       viewResults[segment.view + segment.relation] = (row) =>
-        results.find((rh) => rh.row[table.pk_name] == row[table.pk_name])?.html;
+        results.find((rh) => rh.row[pk_name] == row[pk_name])?.html;
     }
   });
 
@@ -1484,6 +1485,7 @@ module.exports = {
         const ctx = { ...state, user_id: req.user?.id || null, user: req.user };
         let where1 = jsexprToWhere(default_state.include_fml, ctx, fields);
         mergeIntoWhere(where, where1 || {});
+        mergeIntoWhere(whereForCount, where1 || {});
       }
       if (default_state?.exclusion_relation) {
         const [reltable, relfld] = default_state.exclusion_relation.split(".");
@@ -1499,17 +1501,17 @@ module.exports = {
             )
           : {};
         const relRows = await relTable.getRows(relWhere);
-        if (relRows.length > 0)
-          mergeIntoWhere(
-            where,
-            !db.isSQLite
-              ? {
-                  id: { not: { in: relRows.map((r) => r[relfld]) } },
-                }
-              : {
-                  not: { or: relRows.map((r) => ({ id: r[relfld] })) },
-                }
-          );
+        if (relRows.length > 0) {
+          const mergeObj = !db.isSQLite
+            ? {
+                [table.pk_name]: { not: { in: relRows.map((r) => r[relfld]) } },
+              }
+            : {
+                not: { or: relRows.map((r) => ({ id: r[relfld] })) },
+              };
+          mergeIntoWhere(where, mergeObj);
+          mergeIntoWhere(whereForCount, mergeObj);
+        }
       }
       let rows = await table.getJoinedRows({
         where,
