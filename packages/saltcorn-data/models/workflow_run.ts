683dd3cac0f32dea9f70f787fd569ddc5090381a
Tom Nielsen <tanielsen@gmail.com>
2025-04-04 13:36:49 +0100

    return before break

--- packages/saltcorn-data/models/workflow_run.ts
+++ packages/saltcorn-data/models/workflow_run.ts
@@ -829,8 +829,8 @@ class WorkflowRun {
           }
         }
       ); // try-catch
-      if (do_break) break;
       if (returnVal) return returnVal;
+      if (do_break) break;
     } //while
     return this.context;
   }
