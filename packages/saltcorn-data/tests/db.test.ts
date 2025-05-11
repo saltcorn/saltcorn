32d35178e8206e0aa321407c033a901dc9ef9eac
Christian Hugo <hugo.christian@web.de>
2025-05-11 11:47:05 +0200

    unit tests

--- packages/saltcorn-data/tests/db.test.ts
+++ packages/saltcorn-data/tests/db.test.ts
@@ -13,13 +13,11 @@ jest.setTimeout(30000);
 describe("where", () => {
   it("should support in", async () => {
     await Table.create("myothertable");
-    if (!db.isSQLite) {
-      const tf = await db.selectOne("_sc_tables", {
-        name: { in: ["myothertable", "nosuchtable"] },
-      });
+    const tf = await db.selectOne("_sc_tables", {
+      name: { in: ["myothertable", "nosuchtable"] },
+    });
 
-      expect(tf.name).toStrictEqual("myothertable");
-    }
+    expect(tf.name).toStrictEqual("myothertable");
   });
 
   it("should support ilike", async () => {
@@ -68,4 +66,108 @@ describe("Transaction test", () => {
         expect(b).toBeNull();
       });
     });
+});
+
+describe("delete where test", () => {
+  it("should delete where", async () => {
+    const books = Table.findOne({ name: "books" });
+    assertIsSet(books);
+    await books.insertRow({ author: "Crime and Punishment", pages: 688 });
+    await books.insertRow({ author: "For Whom the Bell Tolls", pages: 401 });
+    let rows = await books.getRows();
+    expect(rows.length).toBe(4);
+    await db.deleteWhere(books.name, { author: "The Gambler" });
+    rows = await books.getRows();
+    expect(rows.length).toBe(4);
+    await db.deleteWhere(books.name, { author: "Crime and Punishment" });
+    rows = await books.getRows();
+    expect(rows.length).toBe(3);
+    await db.deleteWhere(books.name, {
+      not: { author: "For Whom the Bell Tolls" },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(1);
+    await db.deleteWhere(books.name);
+    rows = await books.getRows();
+    expect(rows.length).toBe(0);
+  });
+
+  it("should delete where with not in", async () => {
+    const books = Table.findOne({ name: "books" });
+    assertIsSet(books);
+    await books.insertRow({ author: "Crime and Punishment", pages: 688 });
+    await books.insertRow({ author: "For Whom the Bell Tolls", pages: 401 });
+    await books.insertRow({ author: "The Gambler", pages: 501 });
+    await books.insertRow({ author: "The Idiot", pages: 601 });
+    let rows = await books.getRows();
+    expect(rows.length).toBe(4);
+
+    await db.deleteWhere(books.name, {
+      author: {
+        not: {
+          in: [
+            "Crime and Punishment",
+            "For Whom the Bell Tolls",
+            "The Gambler",
+            "The Idiot",
+          ],
+        },
+      },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(4);
+    await db.deleteWhere(books.name, {
+      author: {
+        not: {
+          and: [
+            {
+              in: ["Crime and Punishment", "For Whom the Bell Tolls"],
+            },
+            { in: ["The Gambler", "The Idiot"] },
+          ],
+        },
+      },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(4);
+    await db.deleteWhere(books.name, {
+      author: { not: { in: ["The Gambler"] } },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(1);
+    await db.deleteWhere(books.name, { pages: { not: { in: [601] } } });
+    rows = await books.getRows();
+    expect(rows.length).toBe(0);
+  });
+
+  it("should delete where with in", async () => {
+    const books = Table.findOne({ name: "books" });
+    assertIsSet(books);
+    await books.insertRow({ author: "Crime and Punishment", pages: 688 });
+    await books.insertRow({ author: "For Whom the Bell Tolls", pages: 401 });
+    await books.insertRow({ author: "The Gambler", pages: 501 });
+    await books.insertRow({ author: "The Idiot", pages: 601 });
+    let rows = await books.getRows();
+    expect(rows.length).toBe(4);
+
+    await db.deleteWhere(books.name, { author: { in: ["David Copperfield"] } });
+    rows = await books.getRows();
+    expect(rows.length).toBe(4);
+    await db.deleteWhere(books.name, {
+      author: { in: ["The Gambler", "The Idiot"] },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(2);
+
+    await db.deleteWhere(books.name, {
+      author: {
+        or: [
+          { in: ["Crime and Punishment", "The Gambler"] },
+          { in: ["For Whom the Bell Tolls", "The Idiot"] },
+        ],
+      },
+    });
+    rows = await books.getRows();
+    expect(rows.length).toBe(0);
+  });
 });
