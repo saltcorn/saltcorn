a5b3fa5f62516fa546824902881432c67005965a
Christian Hugo <hugo.christian@web.de>
2025-05-22 11:03:42 +0200

    instead of putting test_schema.js under source control, generate it

--- deploy/playwright_mobile/run.sh
+++ deploy/playwright_mobile/run.sh
@@ -9,6 +9,11 @@ PGDATABASE=saltcorn_test saltcorn reset-schema -f
 PGDATABASE=saltcorn_test saltcorn restore ./backups/guitars_backup.zip
 PGDATABASE=saltcorn_test saltcorn build-app -p web -e guitar_feed -t view -b $BUILD_DIR -u admin@foo.com -s http://localhost:3000
 
+# put tables.json into test_schema.js like this: var _test_schema_ = [content from tables.json]
+if [ -f $BUILD_DIR/www/data/tables.json ]; then
+  echo "var _test_schema_ = $(cat $BUILD_DIR/www/data/tables.json)" > $BUILD_DIR/www/data/test_schema.js
+fi
+
 echo Starting background Saltcorn server...
 PGDATABASE=saltcorn_test SALTCORN_SERVE_MOBILE_TEST_BUILD=/tmp/saltcorn_build/www saltcorn serve -p 3000 &
 SCPID=$!
