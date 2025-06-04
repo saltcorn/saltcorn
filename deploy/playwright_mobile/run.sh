11b4eb3da93c659511d26ca11e006b3280b5a4c1
christianhugoch <hugo.christian@web.de>
2025-06-04 10:12:05 +0200

    rudimental playwright mobile tests (#3290)
    * rudimental playwright mobile tests
    - run the app browser side with sqlite wasml
    - only one rudimental test, to see it working
    
    * add the jwt and session check again
    - was propably a problem because I tested in a normal browser with a session
    
    * initWebstore only on web
    
    * instead of putting test_schema.js under source control, generate it
    
    * login test

---
+++ deploy/playwright_mobile/run.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -e
+
+BUILD_DIR=/tmp/saltcorn_build
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+PATH=../../packages/saltcorn-cli/bin/:$PATH
+PGDATABASE=saltcorn_test saltcorn reset-schema -f
+PGDATABASE=saltcorn_test saltcorn restore ./backups/guitars_backup.zip
+PGDATABASE=saltcorn_test saltcorn build-app -p web -e guitar_feed -t view -b $BUILD_DIR -u admin@foo.com -s http://localhost:3010
+
+# put tables.json into test_schema.js like this: var _test_schema_ = [content from tables.json]
+if [ -f $BUILD_DIR/www/data/tables.json ]; then
+  echo "var _test_schema_ = $(cat $BUILD_DIR/www/data/tables.json)" > $BUILD_DIR/www/data/test_schema.js
+fi
+
+echo Starting background Saltcorn server...
+PGDATABASE=saltcorn_test SALTCORN_SERVE_MOBILE_TEST_BUILD=/tmp/saltcorn_build/www saltcorn serve -p 3010 &
+SCPID=$!
+trap "kill $SCPID" EXIT
+
+while ! nc -z localhost 3010; do
+  sleep 0.2
+done
+
+npx playwright test
