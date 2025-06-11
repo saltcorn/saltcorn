baa7a25cd5b6d21bc31b041a88d3ced56e320c33
Tom Nielsen <tanielsen@gmail.com>
2025-06-11 21:05:20 +0100

    single migration

--- packages/saltcorn-data/migrations/202506050958.js
+++ packages/saltcorn-data/migrations/202506050958.js
@@ -1,4 +1,4 @@
-const sql_pg = `
+const sql = `
 CREATE TABLE IF NOT EXISTS _sc_metadata (
     id serial primary key,
     type text NOT NULL,
@@ -8,12 +8,4 @@ CREATE TABLE IF NOT EXISTS _sc_metadata (
     body jsonb
 );`;
 
-const sql_sqlite = `CREATE TABLE IF NOT EXISTS _sc_metadata (
-    id serial primary key,
-    type text NOT NULL,
-    name text NOT NULL,
-    user_id integer references users(id) on delete cascade,
-    written_at timestamp not null,
-    body json
-);`;
-module.exports = { sql_pg, sql_sqlite };
+module.exports = { sql };
