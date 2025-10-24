159e875f9bf9959b40beb2c2bf051b559a86e17f
Tom Nielsen <tanielsen@gmail.com>
2025-10-24 14:31:36 +0100

    sticky header

--- packages/saltcorn-markup/table.ts
+++ packages/saltcorn-markup/table.ts
@@ -211,7 +211,7 @@ const mkTable = (
     );
   return div(
     {
-      class: ["table-responsive", opts.tableClass],
+      class: [!opts.sticky_header && "table-responsive", opts.tableClass],
       id: opts.tableId,
     },
     table(
@@ -231,6 +231,7 @@ const mkTable = (
       !opts.noHeader &&
         !opts.transpose &&
         thead(
+          opts.sticky_header ? { class: "sticky-top" } : "",
           tr(
             hdrs.map((hdr: HeadersParams, ix: number) =>
               headerCellWithToggle(hdr, opts, ix === hdrs.length - 1)
