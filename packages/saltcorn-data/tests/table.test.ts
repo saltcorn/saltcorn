ef5d5e5949bdda484f9f20262cf7c90758e773bc
Tom Nielsen <tanielsen@gmail.com>
2025-11-25 18:02:42 +0000

    insert test

--- packages/saltcorn-data/tests/table.test.ts
+++ packages/saltcorn-data/tests/table.test.ts
@@ -2544,16 +2544,16 @@ describe("table providers", () => {
     assertIsSet(table);
     expect(table.min_role_read).toBe(40);
   });
-   it("should make keys to provider table", async () => {
+  it("should make keys to provider table", async () => {
     const tc = await Table.create("key_to_provider");
     await Field.create({
       table: tc,
       label: "Person",
-      name: "Person",
+      name: "person",
       type: "Key to JoeTable",
     });
+    await tc.insertRow({ person: "Robinette" });
   });
-
 });
 
 describe("distance ordering", () => {
