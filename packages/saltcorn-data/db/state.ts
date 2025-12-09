102967edfb7f957467a87922db1efe3ef581000e
Tom Nielsen <tanielsen@gmail.com>
2025-12-09 16:12:42 +0000

    simplify log

--- packages/saltcorn-data/db/state.ts
+++ packages/saltcorn-data/db/state.ts
@@ -471,9 +471,9 @@ class State {
   log(min_level: number, ...msgs: any[]) {
     if (min_level <= this.logLevel) {
       const ten = db.getTenantSchema();
-      const s = ten !== "public" ? `Tenant=${ten} ` : "";
-      if (min_level === 1) console.error(...(s ? [s] : []), ...msgs);
-      else console.log(...(s ? [s] : []), ...msgs);
+      if (ten !== "public") msgs.unshift(`Tenant=${ten}`);
+      if (min_level === 1) console.error(...msgs);
+      else console.log(...msgs);
       const msg = msgs
         .map((m) =>
           typeof m === "string" ? m : m.toString ? m.toString() : `${m}`
