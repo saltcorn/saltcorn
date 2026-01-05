408f0b16b2bd060d1c5a2662d70703a89715b892
christianhugoch <hugo.christian@web.de>
2026-01-05 18:16:18 +0100

    monaco editor for extra-state fml, where and where Agg (#3822)
    * monaco editor for extra-state fml, where and where Agg
    - visible in View, ViewLink, Aggregation and DropDownFilter
    
    * unit tests
    
    * playwright test
    
    * validate_expression_elem for monaco onInput
    
    * use React.ref to get the dom element
    
    * use setProp with propKey

--- packages/server/public/saltcorn-common.js
+++ packages/server/public/saltcorn-common.js
@@ -1038,10 +1038,23 @@ function doMobileTransforms() {
   );
 }
 
-function validate_expression_elem(target) {
+/**
+ * @param {any|string} targetOrVal either the target or the string to validate
+ * @param {any} ref the target when targetOrVal is a string (see builder/MonacoEditor)
+ */
+function validate_expression_elem(targetOrVal, ref = null) {
+  let val = null;
+  let target = null;
+  if (typeof targetOrVal === "string") {
+    val = targetOrVal;
+    target = $(ref);
+  } else {
+    target = targetOrVal;
+    val = target.val();
+  }
+
   const next = target.next();
   if (next.hasClass("expr-error")) next.remove();
-  const val = target.val();
   if (target.hasClass("validate-expression-conditional")) {
     const box = target
       .closest(".form-namespace")
