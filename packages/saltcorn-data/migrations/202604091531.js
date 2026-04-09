12c024888139352fc4a4fdb0a97d3d7de64a07da
Tom Nielsen <tanielsen@gmail.com>
2026-04-09 15:35:49 +0100

    migration

---
+++ packages/saltcorn-data/migrations/202604091531.js
@@ -0,0 +1,8 @@
+const sql = `
+alter table _sc_tables add column updated_at timestamp;
+alter table _sc_views add column updated_at timestamp;
+alter table _sc_pages add column updated_at timestamp;
+alter table _sc_triggers add column updated_at timestamp;
+`;
+
+module.exports = { sql };
