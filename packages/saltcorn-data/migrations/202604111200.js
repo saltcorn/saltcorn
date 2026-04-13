9b3efacf7045b9fc660bbdcfa98db337b865deac
Christian Hugo <hugo.christian@web.de>
2026-04-13 15:14:39 +0200

    quotes around schema

--- packages/saltcorn-data/migrations/202604111200.js
+++ packages/saltcorn-data/migrations/202604111200.js
@@ -4,14 +4,14 @@ DECLARE
     r RECORD;
 BEGIN
     FOR r IN
-        SELECT name FROM ${schema}."_sc_tables" WHERE has_sync_info = true
+        SELECT name FROM "${schema}"."_sc_tables" WHERE has_sync_info = true
     LOOP
         EXECUTE format(
-            'ALTER TABLE ${schema}.%I ADD COLUMN IF NOT EXISTS owner_id integer',
+            'ALTER TABLE "${schema}".%I ADD COLUMN IF NOT EXISTS owner_id integer',
             r.name || '_sync_info'
         );
         EXECUTE format(
-            'ALTER TABLE ${schema}.%I ADD COLUMN IF NOT EXISTS owner_fields jsonb',
+            'ALTER TABLE "${schema}".%I ADD COLUMN IF NOT EXISTS owner_fields jsonb',
             r.name || '_sync_info'
         );
     END LOOP;
