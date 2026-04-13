ccade5bcd8260af56dbd6be85b08f358a6fc9740
Tom Nielsen <tanielsen@gmail.com>
2026-04-13 22:10:33 +0100

    disabled user cant use api token

--- packages/server/app.js
+++ packages/server/app.js
@@ -333,8 +333,10 @@ const getApp = async (opts = {}) => {
     new BearerStrategy(function (token, done) {
       loginAttempt();
       async function loginAttempt() {
-        const mu = (await User.findByApiToken(token)) || (await User.findOne({ api_token: token }));
-        if (mu && token && token.length > 5)
+        const mu =
+          (await User.findByApiToken(token)) ||
+          (await User.findOne({ api_token: token }));
+        if (mu && !mu.disabled && token && token.length > 5)
           return done(
             null,
             {
