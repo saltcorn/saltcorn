da00840e577560ad9d2c3b37972f99d9475a859d
Tom Nielsen <tanielsen@gmail.com>
2026-05-07 21:47:13 +0100

    stringToJSON before getting provided table fields. Fixes #4050

--- packages/saltcorn-data/models/table.ts
+++ packages/saltcorn-data/models/table.ts
@@ -363,6 +363,8 @@ class Table implements AbstractTable {
       //limited refresh if we do not have a client
       if (!db.getRequestContext()?.client) await Table.state_refresh(true);
     };
+    //console.log({tbl});
+
     return t;
   }
 
@@ -447,7 +449,10 @@ class Table implements AbstractTable {
         const { getState } = require("../db/state");
         const provider = getState().table_providers[t.provider_name];
         if (provider)
-          t.fields = await applyAsync(provider.fields, t.provider_cfg);
+          t.fields = await applyAsync(
+            provider.fields,
+            stringToJSON(t.provider_cfg)
+          );
         else t.fields = [];
       } else
         t.fields = flds
