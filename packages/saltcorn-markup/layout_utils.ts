095d94073cba13dceb39c8d67114ff6917af04aa
Tom Nielsen <tanielsen@gmail.com>
2026-06-11 08:16:39 +0100

    Revert "rm mdb class again"
    This reverts commit 98f74f2edb4159a4f7c587c47b7cac303737b23a.

--- packages/saltcorn-markup/layout_utils.ts
+++ packages/saltcorn-markup/layout_utils.ts
@@ -828,10 +828,7 @@ const renderTabs = (
   if (tabsStyle === "Accordion")
     return (
       div(
-        {
-          class: ["accordion", lazyLoadViews && "lazy-accoordion", outerClass],
-          id: `${rndid}top`,
-        },
+        { class: ["accordion", lazyLoadViews && "lazy-accoordion", outerClass], id: `${rndid}top` },
         contents.map((t, ix) =>
           div(
             { class: "accordion-item" },
@@ -871,6 +868,7 @@ const renderTabs = (
                 id: `${rndid}tab${ix}`,
                 "aria-labelledby": `${rndid}head${ix}`,
                 "data-bs-parent": independent ? undefined : `#${rndid}top`,
+                "data-mdb-parent": independent ? undefined : `#${rndid}top`, //TODO fix in material instead, remove here
               },
               div(
                 { class: ["accordion-body", bodyClass || ""] },
