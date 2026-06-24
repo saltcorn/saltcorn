3d7fad6295da2db9b9e40bdda78f6fcb83573ccf
Tom Nielsen <tanielsen@gmail.com>
2026-06-24 20:44:45 +0100

    split extra_tags out of tags

---
+++ packages/saltcorn-markup/extra_tags.ts
@@ -0,0 +1,88 @@
+/**
+ * @category saltcorn-markup
+ * @module tags
+ */
+
+import mkTag = require("./mktag");
+import xss, { whiteList, IWhiteList } from "xss";
+import escape from "escape-html";
+import type {
+  ClassVal,
+  StyleVal,
+  Element,
+  Attributes,
+  AttributeVal,
+  TagFunction,
+  TagExports,
+} from "./types";
+
+//https://stackoverflow.com/a/59220393
+/**
+ * @param {string} js
+ * @returns {string}
+ */
+const domReady = (js: string): string =>
+  `(function(f){if (document.readyState === "complete") f(); else document.addEventListener('DOMContentLoaded',()=>setTimeout(f),false)})(function(){${js}});`;
+
+const with_curScript = (js: string): string =>
+  `((curScript)=>{${js}})(document.currentScript)`;
+
+whiteList.kbd = [];
+whiteList.span = ["style"];
+whiteList.div = ["style"];
+whiteList.td = ["style"];
+
+const mergeWhiteList = (customWhiteList: IWhiteList): IWhiteList => {
+  const res = JSON.parse(JSON.stringify(whiteList));
+  for (const [k, v] of Object.entries(customWhiteList)) {
+    if (!v) continue;
+    if (res[k]) res[k].push(...v);
+    else res[k] = v;
+  }
+  return res;
+};
+
+/**
+ * @param {string|number} t
+ * @returns {string}
+ */
+const text = (t: string | number, customWhiteList?: IWhiteList): string =>
+  t === 0
+    ? "0"
+    : xss(
+        <string>t,
+        customWhiteList
+          ? { whiteList: mergeWhiteList(customWhiteList) }
+          : undefined
+      );
+
+/**
+ * @param {string|number} t
+ * @returns {string}
+ */
+const text_attr = (t: string | number) => (t === 0 ? "0" : escape(<string>t));
+
+
+const tagsExports: TagExports = {
+  /**
+   * @param {string} tagName
+   * @param  {...*} rest
+   * @returns {string}
+   */
+  genericElement: (
+    tagName: string,
+    attributes_or_first_child?: Attributes | Element,
+    ...children: Element[]
+  ) => mkTag(tagName, false)(attributes_or_first_child, ...children),
+  domReady,
+  with_curScript,
+  text,
+  text_attr,
+  /** @type {string} */
+  nbsp: "&nbsp;",
+  /** @type {module:mktag} */
+  mkTag,
+  escape,
+} as TagExports;
+
+export = tagsExports;
--- packages/saltcorn-markup/tags.ts
+++ packages/saltcorn-markup/tags.ts
@@ -4,7 +4,6 @@
  */
 
 import mkTag = require("./mktag");
-import xss, { whiteList, IWhiteList } from "xss";
 import escape from "escape-html";
 import htmlTags from "html-tags";
 import voidHtmlTags from "html-tags/void";
@@ -17,57 +16,16 @@ import type {
   TagFunction,
   TagExports,
 } from "./types";
-
+import {
+  genericElement,
+  domReady,
+  with_curScript,
+  text,
+  text_attr,
+  nbsp,
+} from "./extra_tags";
 const voidHtmlTagsSet = new Set<string>(voidHtmlTags);
 
-//https://stackoverflow.com/a/59220393
-/**
- * @param {string} js
- * @returns {string}
- */
-const domReady = (js: string): string =>
-  `(function(f){if (document.readyState === "complete") f(); else document.addEventListener('DOMContentLoaded',()=>setTimeout(f),false)})(function(){${js}});`;
-
-const with_curScript = (js: string): string =>
-  `((curScript)=>{${js}})(document.currentScript)`;
-
-whiteList.kbd = [];
-whiteList.span = ["style"];
-whiteList.div = ["style"];
-whiteList.td = ["style"];
-
-const mergeWhiteList = (customWhiteList: IWhiteList): IWhiteList => {
-  const res = JSON.parse(JSON.stringify(whiteList));
-  for (const [k, v] of Object.entries(customWhiteList)) {
-    if (!v) continue;
-    if (res[k]) res[k].push(...v);
-    else res[k] = v;
-  }
-  return res;
-};
-
-/**
- * @param {string|number} t
- * @returns {string}
- */
-const text = (t: string | number, customWhiteList?: IWhiteList): string =>
-  t === 0
-    ? "0"
-    : xss(
-        <string>t,
-        customWhiteList
-          ? { whiteList: mergeWhiteList(customWhiteList) }
-          : undefined
-      );
-
-/**
- * @param {string|number} t
- * @returns {string}
- */
-const text_attr = (t: string | number) => (t === 0 ? "0" : escape(<string>t));
-
-/**
- */
 const allTags: { [k: string]: TagFunction } = Object.fromEntries(
   htmlTags.map((tag) => [tag, mkTag(tag, voidHtmlTagsSet.has(tag))])
 );
@@ -79,17 +37,13 @@ const tagsExports: TagExports = {
    * @param  {...*} rest
    * @returns {string}
    */
-  genericElement: (
-    tagName: string,
-    attributes_or_first_child?: Attributes | Element,
-    ...children: Element[]
-  ) => mkTag(tagName, false)(attributes_or_first_child, ...children),
+  genericElement,
   domReady,
   with_curScript,
   text,
   text_attr,
   /** @type {string} */
-  nbsp: "&nbsp;",
+  nbsp,
   /** @type {module:mktag} */
   mkTag,
   escape,
