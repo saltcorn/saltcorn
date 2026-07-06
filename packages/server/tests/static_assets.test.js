0dafd5306e6a665dc8d3358bf699f6c3eeae6d26
Tom Nielsen <tanielsen@gmail.com>
2026-07-06 11:00:02 +0100

    workflow bundle test

--- packages/server/tests/static_assets.test.js
+++ packages/server/tests/static_assets.test.js
@@ -25,4 +25,19 @@ describe("static assets", () => {
           throw new Error("builder bundle body is empty or too small");
       });
   });
+  it("serves the workflow bundle", async () => {
+    const app = await getApp({ disableCsrf: true });
+    const version_tag = db.connectObj.version_tag;
+    await request(app)
+      .get(`/static_assets/${version_tag}/workflow_bundle.js`)
+      .expect(200)
+      .expect((res) => {
+        if (!/javascript/.test(res.headers["content-type"]))
+          throw new Error(
+            `Expected a javascript Content-Type, received "${res.headers["content-type"]}"`
+          );
+        if (!res.text || res.text.length < 1000)
+          throw new Error("workflow bundle body is empty or too small");
+      });
+  });
 });
