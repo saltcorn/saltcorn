36405fa0c10a8dbec4605397b3a0eb8f3eca968e
gerard1001 <rutayisiregerard2002@gmail.com>
2026-07-23 14:29:16 +0200

    exclude validate-expression elements for 'edit with ai'

--- packages/saltcorn-builder/src/components/elements/utils.js
+++ packages/saltcorn-builder/src/components/elements/utils.js
@@ -1565,9 +1565,10 @@ const ConfigField = ({
           label={field?.label || field?.name || "Code"}
           placeholder={field?.attributes?.placeholder}
           nojoins={field?.attributes?.nojoins}
-          isExpression={["row", "query"].includes(
-            field?.attributes?.expression_type
-          )}
+          isExpression={
+            !!field?.attributes?.expression_type ||
+            !!field?.class?.includes?.("validate-expression")
+          }
         />
       );
     },
