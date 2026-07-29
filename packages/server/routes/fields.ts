39cc131f6155dfae1d67e3d3e08cf24ec33886a3
Tom Nielsen <tanielsen@gmail.com>
2026-07-29 17:34:40 +0100

    in server

--- packages/server/routes/fields.ts
+++ packages/server/routes/fields.ts
@@ -1679,20 +1679,26 @@ router.post(
     let val =
       typeof field.type === "object" && field.type?.read
         ? field.type?.read(req.body[field_name], field.attributes)
-        : req.body[field_name];
+        : field.type === "Key" && req.body[field_name] === ""
+          ? null
+          : req.body[field_name];
     await db.withTransaction(async () => {
       await table.updateRow({ [field_name]: val }, pk, req.user);
     });
     let fv: any;
+    let blank = false;
     if (field.is_fkey) {
       if (join_field) {
         const refTable = Table.findOne({ name: field.reftable_name })!;
         const refRow = (await refTable.getRow({ [refTable.pk_name]: val }))!;
-        val = refRow[join_field];
-        const targetField = refTable.getField(join_field)!;
-        const fieldviews = targetField.fieldviews;
+        if (!refRow) blank = true;
+        else {
+          val = refRow[join_field];
+          const targetField = refTable.getField(join_field)!;
+          const fieldviews = targetField.fieldviews;
 
-        fv = fieldviews[fieldview];
+          fv = fieldviews[fieldview];
+        }
       } else fv = { run: (v: any) => `${v}` };
     } else {
       const fieldviews = field.fieldviews;
@@ -1715,10 +1721,12 @@ router.post(
           "data-inline-edit-dest-url": `/api/${table.name}/${pk}`,
           class: !isWeb(req) ? "mobile-data-inline-edit" : "",
         },
-        fv.run(val, req, {
-          ...field.attributes,
-          ...configuration,
-        })
+        blank
+          ? ""
+          : fv.run(val, req, {
+              ...field.attributes,
+              ...configuration,
+            })
       )
     );
   })
