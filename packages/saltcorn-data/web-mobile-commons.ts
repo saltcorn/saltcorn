c416dd7325c80729e669b2e30ed0b4829eec7536
Tom Nielsen <tanielsen@gmail.com>
2026-09-03 18:24:09 +0100

    delete drawer labels  in menu gen

--- packages/saltcorn-data/web-mobile-commons.ts
+++ packages/saltcorn-data/web-mobile-commons.ts
@@ -165,7 +165,6 @@ const get_extra_menu = (
 
         return {
           label: translated_label,
-          drawer_label: item.drawer_text,
           icon: item.icon,
           isUser: item.user_menu_header,
           location: item.location,
